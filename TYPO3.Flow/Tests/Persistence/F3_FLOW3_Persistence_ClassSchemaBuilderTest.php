511fa57cdfc4a90b64b18f25ff5d61e9f5bd1f59
Robert Lemke <robert@typo3.org>
2008-06-03 15:22:45 +0000

    * FLOW3: (Persistence) Some update: new tests for the persistence framework. Original-Commit-Hash: 14c29d32b2af52b7c2fb0c54ac0b7b5e97223e1f

---
+++ TYPO3.Flow/Tests/Persistence/F3_FLOW3_Persistence_ClassSchemaBuilderTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(ENCODING = 'utf-8');
+
+/*                                                                        *
+ * This script is part of the TYPO3 project - inspiring people to share!  *
+ *                                                                        *
+ * TYPO3 is free software; you can redistribute it and/or modify it under *
+ * the terms of the GNU General Public License version 2 as published by  *
+ * the Free Software Foundation.                                          *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
+ * Public License for more details.                                       *
+ *                                                                        */
+
+/**
+ * @package FLOW3
+ * @subpackage Persistence
+ * @version $Id$
+ */
+
+/**
+ * Testcase for the Class Schema Builder
+ *
+ * @package FLOW3
+ * @subpackage Persistence
+ * @version $Id$
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
+ */
+class F3_FLOW3_Persistence_ClassSchemaBuilderTest extends F3_Testing_BaseTestCase {
+ 
+	/**
+	 * @test
+	 * @author Robert Lemke <robert@typo3.org>
+	 */
+	public function buildDetects() {
+		require_once('Fixture/F3_FLOW3_Test_Persistence_Fixture_Model1.php');
+	}
+}
+
+?>
