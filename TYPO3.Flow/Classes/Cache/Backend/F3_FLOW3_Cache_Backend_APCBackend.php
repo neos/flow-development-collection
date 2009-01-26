84d373fea7ab875dc3144912a7f121e7ac26cd3b
Karsten Dambekalns <karsten@typo3.org>
2009-01-26 20:59:46 +0000

    FLOW3: * added cache backend for using the "Alternative PHP Cache" (APC) Original-Commit-Hash: 02b45452f90b51ca3ef42b54359770cc82ea753e

---
+++ TYPO3.Flow/Classes/Cache/Backend/F3_FLOW3_Cache_Backend_APCBackend.php
@@ -0,0 +1,398 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\FLOW3\Cache\Backend;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 framework.                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * @package FLOW3
+ * @subpackage Cache
+ * @version $Id$
+ */
+
+/**
+ * A caching backend which stores cache entries by using APC.
+ *
+ * This backend uses the following types of keys:
+ * - tag_xxx
+ *   xxx is tag name, value is array of associated identifiers identifier. This
+ *   is "forward" tag index. It is mainly used for obtaining content by tag
+ *   (get identifier by tag -> get content by identifier)
+ * - ident_xxx
+ *   xxx is identifier, value is array of associated tags. This is "reverse" tag
+ *   index. It provides quick access for all tags associated with this identifier
+ *   and used when removing the identifier
+ * - tagIndex
+ *   Value is a List of all tags (array)
+
+ * Each key is prepended with a prefix. By default prefix consists from two parts
+ * separated by underscore character and ends in yet another underscore character:
+ * - "FLOW3"
+ * - MD5 of script path and filename and SAPI name
+ * This prefix makes sure that keys from the different installations do not
+ * conflict.
+ *
+ * @package FLOW3
+ * @subpackage Cache
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @scope prototype
+ */
+class APCBackend extends \F3\FLOW3\Cache\Backend\AbstractBackend {
+
+	/**
+	 * A prefix to seperate stored data from other data possible stored in the APC
+	 *
+	 * @var string
+	 */
+	protected $identifierPrefix;
+
+	/**
+	 * @var \F3\FLOW3\Utility\Environment
+	 */
+	protected $environment;
+
+	/**
+	 * @var \F3\FLOW3\Log\SystemLoggerInterface
+	 */
+	protected $systemLogger;
+
+	/**
+	 * Constructs this backend
+	 *
+	 * @param string $context FLOW3's application context
+	 * @param mixed $options Configuration options - unused here
+	 * @author Robert Lemke <robert@typo3.org>
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 *
+	 */
+	public function __construct($context, $options = array()) {
+		if (!extension_loaded('apc')) throw new \F3\FLOW3\Cache\Exception('The PHP extension "apc" must be installed and loaded in order to use the APC backend.', 1232985414);
+		parent::__construct($context, $options);
+	}
+
+	/**
+	 * Injects the environment utility
+	 *
+	 * @param \F3\FLOW3\Utility\Environment $environment
+	 * @return void
+	 * @author Robert Lemke <robert@typo3.org>
+	 */
+	public function injectEnvironment(\F3\FLOW3\Utility\Environment $environment) {
+		$this->environment = $environment;
+	}
+
+	/**
+	 * Injects the system logger
+	 *
+	 * @param \F3\FLOW3\Log\SystemLoggerInterface $systemLogger
+	 * @return void
+	 * @author Robert Lemke <robert@typo3.org>
+	 */
+	public function injectSystemLogger(\F3\FLOW3\Log\SystemLoggerInterface $systemLogger) {
+		$this->systemLogger = $systemLogger;
+	}
+
+	/**
+	 * Initializes the identifier prefix
+	 *
+	 * @return void
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function initializeObject() {
+		$this->identifierPrefix = 'FLOW3_' . md5($this->environment->getScriptPathAndFilename() . $this->environment->getSAPIName()) . '_';
+	}
+
+	/**
+	 * Saves data in the cache.
+	 *
+	 * Note on lifetime: the number of seconds may not exceed 2592000 (30 days),
+	 * otherwise it is interpreted as a UNIX timestamp (seconds since epoch).
+	 *
+	 * @param string $entryIdentifier An identifier for this specific cache entry
+	 * @param string $data The data to be stored
+	 * @param array $tags Tags to associate with this cache entry
+	 * @param integer $lifetime Lifetime of this cache entry in seconds. If NULL is specified, the default lifetime is used. "0" means unlimited liftime.
+	 * @return void
+	 * @throws \F3\FLOW3\Cache\Exception if no cache frontend has been set.
+	 * @throws \InvalidArgumentException if the identifier is not valid
+	 * @throws \F3\FLOW3\Cache\Exception\InvalidData if $data is not a string
+	 * @author Christian Jul Jensen <julle@typo3.org>
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 **/
+	public function set($entryIdentifier, $data, array $tags = array(), $lifetime = NULL) {
+		if (!$this->isValidEntryIdentifier($entryIdentifier)) throw new \InvalidArgumentException('"' . $entryIdentifier . '" is not a valid cache entry identifier.', 1232986813);
+		if (!$this->cache instanceof \F3\FLOW3\Cache\CacheInterface) throw new \F3\FLOW3\Cache\Exception('No cache frontend has been set yet via setCache().', 1232986818);
+		if (!is_string($data)) throw new \F3\FLOW3\Cache\Exception\InvalidData('The specified data is of type "' . gettype($data) . '" but a string is expected.', 1232986825);
+		foreach ($tags as $tag) {
+			if (!$this->isValidTag($tag))  throw new \InvalidArgumentException('"' . $tag . '" is not a valid tag.', 1232986830);
+		}
+
+		$tags[] = '%MEMCACHE%' . $this->cache->getIdentifier();
+		$expiration = $lifetime ? $lifetime : $this->defaultLifetime;
+
+		$success = apc_store($this->identifierPrefix . $entryIdentifier, $data, $expiration);
+		if ($success === TRUE) {
+			$this->removeIdentifierFromAllTags($entryIdentifier);
+			$this->addTagsToTagIndex($tags);
+			$this->addIdentifierToTags($entryIdentifier, $tags);
+		} else {
+			throw new \F3\FLOW3\Cache\Exception('Could not set value. ' . $exception->getMessage(), 1232986877);
+		}
+	}
+
+	/**
+	 * Loads data from the cache.
+	 *
+	 * @param string $entryIdentifier An identifier which describes the cache entry to load
+	 * @return mixed The cache entry's content as a string or FALSE if the cache entry could not be loaded
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function get($entryIdentifier) {
+		$success = FALSE;
+		$value = apc_fetch($this->identifierPrefix . $entryIdentifier, $success);
+		return ($success ? $value : $success);
+	}
+
+	/**
+	 * Checks if a cache entry with the specified identifier exists.
+	 *
+	 * @param string $entryIdentifier An identifier specifying the cache entry
+	 * @return boolean TRUE if such an entry exists, FALSE if not
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function has($entryIdentifier) {
+		$success = FALSE;
+		apc_fetch($this->identifierPrefix . $entryIdentifier, $success);
+		return $success;
+	}
+
+	/**
+	 * Removes all cache entries matching the specified identifier.
+	 * Usually this only affects one entry but if - for what reason ever -
+	 * old entries for the identifier still exist, they are removed as well.
+	 *
+	 * @param string $entryIdentifier Specifies the cache entry to remove
+	 * @return boolean TRUE if (at least) an entry could be removed or FALSE if no entry was found
+	 * @author Christian Jul Jensen <julle@typo3.org>
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function remove($entryIdentifier) {
+		$this->systemLogger->log(sprintf('Cache %s: removing entry "%s".', $this->cache->getIdentifier(), $entryIdentifier), LOG_DEBUG);
+		$this->removeIdentifierFromAllTags($entryIdentifier);
+		return apc_delete($this->identifierPrefix . $entryIdentifier);
+	}
+
+	/**
+	 * Finds and returns all cache entry identifiers which are tagged by the
+	 * specified tag.
+	 * The asterisk ("*") is allowed as a wildcard at the beginning and the end of
+	 * the tag.
+	 *
+	 * @param string $tag The tag to search for, the "*" wildcard is supported
+	 * @return array An array with identifiers of all matching entries. An empty array if no entries matched
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 * @todo implement wildcard support
+	 */
+	public function findIdentifiersByTag($tag) {
+		if (!$this->isValidTag($tag))  throw new \InvalidArgumentException('"' . $tag . '" is not a valid tag.', 1232986966);
+
+		return $this->findIdentifiersTaggedWith($tag);
+	}
+
+	/**
+	 * Finds all tags for the given identifier. This function uses reverse tag
+	 * index to search for tags.
+	 *
+	 * @param string $identifier Identifier to find tags by
+	 * @return array Array with tags
+	 * @author Dmitry Dulepov
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	protected function findTagsByIdentifier($identifier) {
+		$success = FALSE;
+		$tags = apc_fetch($this->identifierPrefix . 'ident_' . $identifier, $success);
+		return ($success ? (array)$tags : array());
+	}
+
+	/**
+	 * Removes all cache entries of this cache.
+	 *
+	 * @return void
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function flush() {
+		if (!$this->cache instanceof \F3\FLOW3\Cache\CacheInterface) throw new \F3\FLOW3\Cache\Exception('Yet no cache frontend has been set via setCache().', 1232986971);
+		$this->flushByTag('%MEMCACHE%' . $this->cache->getIdentifier());
+	}
+
+	/**
+	 * Removes all cache entries of this cache which are tagged by the specified tag.
+	 *
+	 * @param string $tag The tag the entries must have
+	 * @return void
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function flushByTag($tag) {
+		if (!$this->isValidTag($tag)) throw new \InvalidArgumentException('"' . $tag . '" is not a valid tag.', 1232986987);
+		$identifiers = $this->findIdentifiersTaggedWith($tag);
+		$this->systemLogger->log(sprintf('Cache %s: removing %s entries matching tag "%s"', $this->cache->getIdentifier(), count($identifiers), $tag), LOG_INFO);
+		foreach ($identifiers as $identifier) {
+			$this->remove($identifier);
+		}
+	}
+
+	/**
+	 * Returns an array with all known tags
+	 *
+	 * @return array
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	protected function getTagIndex() {
+		$success = FALSE;
+		$tagIndex = apc_fetch($this->identifierPrefix . 'tagIndex', $success);
+		return ($success ? (array)$tagIndex : array());
+	}
+
+	/**
+	 * Saves the tags known to the backend
+	 *
+	 * @param array $tags
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	protected function setTagIndex(array $tags) {
+		apc_store($this->identifierPrefix . 'tagIndex', array_unique($tags));
+	}
+
+	/**
+	 * Adds the given tags to the tag index
+	 *
+	 * @param array $tags
+	 * @return void
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	protected function addTagsToTagIndex(array $tags) {
+		if (count($tags)) {
+			$this->setTagIndex(array_merge($tags, $this->getTagIndex()));
+		}
+	}
+
+	/**
+	 * Removes the given tags from the tag index
+	 *
+	 * @param array $tags
+	 * @return void
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	protected function removeTagsFromTagIndex(array $tags) {
+		if (count($tags)) {
+			$this->setTagIndex(array_diff($this->getTagIndex(), $tags));
+		}
+	}
+
+	/**
+	 * Associates the identifier with the given tags
+	 *
+	 * @param string $entryIdentifier
+	 * @param array $tags
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 * @author Dmitry Dulepov
+	 */
+	protected function addIdentifierToTags($entryIdentifier, array $tags) {
+		foreach ($tags as $tag) {
+				// Update tag-to-identifier index
+			$identifiers = $this->findIdentifiersTaggedWith($tag);
+			if (array_search($entryIdentifier, $identifiers) === FALSE) {
+				$identifiers[] = $entryIdentifier;
+				apc_store($this->identifierPrefix . 'tag_' . $tag, $identifiers);
+			}
+
+				// Update identifier-to-tag index
+			$existingTags = $this->findTagsByIdentifier($entryIdentifier);
+			if (array_search($entryIdentifier, $existingTags) === false) {
+				apc_store($this->identifierPrefix . 'ident_' . $entryIdentifier, array_merge($existingTags, $tags));
+			}
+
+		}
+	}
+
+	/**
+	 * Removes association of the identifier with the given tags
+	 *
+	 * @param string $entryIdentifier
+	 * @param array $tags
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 * @author Dmitry Dulepov
+	 */
+	protected function removeIdentifierFromAllTags($entryIdentifier) {
+			// Get tags for this identifier
+		$tags = $this->findTagsByIdentifier($entryIdentifier);
+			// Deassociate tags with this identifier
+		foreach ($tags as $tag) {
+			$identifiers = $this->findIdentifiersTaggedWith($tag);
+				// Formally array_search() below should never return false due to
+				// the behavior of findTagsByIdentifier(). But if reverse index is
+				// corrupted, we still can get 'false' from array_search(). This is
+				// not a problem because we are removing this identifier from
+				// anywhere.
+			if (($key = array_search($entryIdentifier, $identifiers)) !== FALSE) {
+				unset($identifiers[$key]);
+				if (count($identifiers)) {
+					apc_store($this->identifierPrefix . 'tag_' . $tag, $identifiers);
+				} else {
+					$this->removeTagsFromTagIndex(array($tag));
+					apc_delete($this->identifierPrefix . 'tag_' . $tag);
+				}
+			}
+		}
+			// Clear reverse tag index for this identifier
+		apc_delete($this->identifierPrefix . 'ident_' . $entryIdentifier);
+	}
+
+	/**
+	 * Returns all identifiers associated with $tag
+	 *
+	 * @param string $tag
+	 * @return array
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function findIdentifiersTaggedWith($tag) {
+		$success = FALSE;
+		$identifiers = apc_fetch($this->identifierPrefix . 'tag_' . $tag, $success);
+		if ($success === FALSE) {
+			return array();
+		} else {
+			return (array) $identifiers;
+		}
+	}
+
+	/**
+	 * Does nothing, as APC does GC itself
+	 *
+	 * @return void
+	 */
+	public function collectGarbage() {
+		$this->systemLogger->log(sprintf('Cache %s: garbage collection is done by APC', $this->cache->getIdentifier()), LOG_INFO);
+	}
+
+}
+
+?>
---
+++ TYPO3.Flow/Tests/Cache/Backend/F3_FLOW3_Cache_Backend_APCBackendTest.php
@@ -0,0 +1,319 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\FLOW3\Cache\Backend;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 framework.                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * @package FLOW3
+ * @subpackage Tests
+ * @version $Id$
+ */
+
+/**
+ * Testcase for the APC cache backend
+ *
+ * @package FLOW3
+ * @subpackage Tests
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ */
+class APCBackendTest extends \F3\Testing\BaseTestCase {
+
+	/**
+	 * @var \F3\FLOW3\Utility\Environment
+	 */
+	protected $environment;
+
+	/**
+	 * Sets up this testcase
+	 *
+	 * @return void
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function setUp() {
+		if (!extension_loaded('apc')) {
+			$this->markTestSkipped('APC extension was not available');
+		}
+
+		$this->environment = new \F3\FLOW3\Utility\Environment();
+	}
+
+	/**
+	 * @test
+	 * @author Christian Jul Jensen <julle@typo3.org>
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 * @expectedException \F3\FLOW3\Cache\Exception
+	 */
+	public function setThrowsExceptionIfNoFrontEndHasBeenSet() {
+		$backend = new \F3\FLOW3\Cache\Backend\APCBackend('Testing');
+		$backend->injectEnvironment($this->environment);
+		$backend->initializeObject();
+		$data = 'Some data';
+		$identifier = 'MyIdentifier';
+		$backend->set($identifier, $data);
+	}
+
+	/**
+	 * @test
+	 * @author Robert Lemke <robert@typo3.org>
+	 */
+	public function setRejectsInvalidIdentifiers() {
+		$backend = $this->setUpBackend();
+		$data = 'Somedata';
+
+		foreach (array('', 'abc def', 'foo!', 'bar:', 'some/', 'bla*', 'one+', 'äöü', str_repeat('x', 251), 'x$', '\\a', 'b#', 'some&') as $entryIdentifier) {
+			try {
+				$backend->set($entryIdentifier, $data);
+				$this->fail('set() did no reject the entry identifier "' . $entryIdentifier . '".');
+			} catch (\InvalidArgumentException $exception) {
+			}
+		}
+	}
+
+	/**
+	 * @test
+	 * @author Christian Jul Jensen <julle@typo3.org>
+	 */
+	public function itIsPossibleToSetAndCheckExistenceInCache() {
+		$backend = $this->setUpBackend();
+		$data = 'Some data';
+		$identifier = 'MyIdentifier';
+		$backend->set($identifier, $data);
+		$inCache = $backend->has($identifier);
+		$this->assertTrue($inCache, 'APC backend failed to set and check entry');
+	}
+
+	/**
+	 * @test
+	 * @author Christian Jul Jensen <julle@typo3.org>
+	 */
+	public function itIsPossibleToSetAndGetEntry() {
+		$backend = $this->setUpBackend();
+		$data = 'Some data';
+		$identifier = 'MyIdentifier';
+		$backend->set($identifier, $data);
+		$fetchedData = $backend->get($identifier);
+		$this->assertEquals($data, $fetchedData, 'APC backend failed to set and retrieve data');
+	}
+
+	/**
+	 * @test
+	 * @author Christian Jul Jensen <julle@typo3.org>
+	 */
+	public function itIsPossibleToRemoveEntryFromCache() {
+		$backend = $this->setUpBackend();
+		$data = 'Some data';
+		$identifier = 'MyIdentifier';
+		$backend->set($identifier, $data);
+		$backend->remove($identifier);
+		$inCache = $backend->has($identifier);
+		$this->assertFalse($inCache, 'Failed to set and remove data from APC backend');
+	}
+
+	/**
+	 * @test
+	 * @author Christian Jul Jensen <julle@typo3.org>
+	 */
+	public function itIsPossibleToOverwriteAnEntryInTheCache() {
+		$backend = $this->setUpBackend();
+		$data = 'Some data';
+		$identifier = 'MyIdentifier';
+		$backend->set($identifier, $data);
+		$otherData = 'some other data';
+		$backend->set($identifier, $otherData);
+		$fetchedData = $backend->get($identifier);
+		$this->assertEquals($otherData, $fetchedData, 'APC backend failed to overwrite and retrieve data');
+	}
+
+	/**
+	 * @test
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function findIdentifiersByTagFindsSetEntries() {
+		$backend = $this->setUpBackend();
+
+		$data = 'Some data';
+		$entryIdentifier = 'MyIdentifier';
+		$backend->set($entryIdentifier, $data, array('UnitTestTag%tag1', 'UnitTestTag%tag2'));
+
+		$retrieved = $backend->findIdentifiersByTag('UnitTestTag%tag1');
+		$this->assertEquals($entryIdentifier, $retrieved[0], 'Could not retrieve expected entry by tag.');
+
+		$retrieved = $backend->findIdentifiersByTag('UnitTestTag%tag2');
+		$this->assertEquals($entryIdentifier, $retrieved[0], 'Could not retrieve expected entry by tag.');
+	}
+
+	/**
+	 * @test
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function setRemovesTagsFromPreviousSet() {
+		$backend = $this->setUpBackend();
+
+		$data = 'Some data';
+		$entryIdentifier = 'MyIdentifier';
+		$backend->set($entryIdentifier, $data, array('UnitTestTag%tag1', 'UnitTestTag%tag2'));
+		$backend->set($entryIdentifier, $data, array('UnitTestTag%tag3'));
+
+		$retrieved = $backend->findIdentifiersByTag('UnitTestTag%tag2');
+		$this->assertEquals(array(), $retrieved, 'Found entry which should no longer exist.');
+	}
+
+	/**
+	 * @test
+	 * @author Christian Jul Jensen <julle@typo3.org>
+	 */
+	public function hasReturnsFalseIfTheEntryDoesntExist() {
+		$backend = $this->setUpBackend();
+		$identifier = 'NonExistingIdentifier';
+		$inCache = $backend->has($identifier);
+		$this->assertFalse($inCache,'"has" did not return false when checking on non existing identifier');
+	}
+
+	/**
+	 * @test
+	 * @author Christian Jul Jensen <julle@typo3.org>
+	 */
+	public function removeReturnsFalseIfTheEntryDoesntExist() {
+		$backend = $this->setUpBackend();
+		$identifier = 'NonExistingIdentifier';
+		$inCache = $backend->remove($identifier);
+		$this->assertFalse($inCache,'"remove" did not return false when checking on non existing identifier');
+	}
+
+	/**
+	 * @test
+	 * @expectedException InvalidArgumentException
+	 * @author Robert Lemke <robert@typo3.org>
+	 */
+	public function flushByTagRejectsInvalidTags() {
+		$backend = $this->setUpBackend();
+		$backend->flushByTag('SomeInvalid\Tag');
+	}
+
+	/**
+	 * @test
+	 * @author Robert Lemke <robert@typo3.org>
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function flushByTagRemovesCacheEntriesWithSpecifiedTag() {
+		$backend = $this->setUpBackend();
+
+		$data = 'some data' . microtime();
+		$backend->set('BackendMemcacheTest1', $data, array('UnitTestTag%test', 'UnitTestTag%boring'));
+		$backend->set('BackendMemcacheTest2', $data, array('UnitTestTag%test', 'UnitTestTag%special'));
+		$backend->set('BackendMemcacheTest3', $data, array('UnitTestTag%test'));
+
+		$backend->flushByTag('UnitTestTag%special');
+
+		$this->assertTrue($backend->has('BackendMemcacheTest1'), 'BackendMemcacheTest1');
+		$this->assertFalse($backend->has('BackendMemcacheTest2'), 'BackendMemcacheTest2');
+		$this->assertTrue($backend->has('BackendMemcacheTest3'), 'BackendMemcacheTest3');
+	}
+
+	/**
+	 * @test
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function flushRemovesAllCacheEntries() {
+		$backend = $this->setUpBackend();
+
+		$data = 'some data' . microtime();
+		$backend->set('BackendMemcacheTest1', $data);
+		$backend->set('BackendMemcacheTest2', $data);
+		$backend->set('BackendMemcacheTest3', $data);
+
+		$backend->flush();
+
+		$this->assertFalse($backend->has('BackendMemcacheTest1'), 'BackendMemcacheTest1');
+		$this->assertFalse($backend->has('BackendMemcacheTest2'), 'BackendMemcacheTest2');
+		$this->assertFalse($backend->has('BackendMemcacheTest3'), 'BackendMemcacheTest3');
+	}
+
+	/**
+	 * @test
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function flushRemovesOnlyOwnEntries() {
+		$mockSystemLogger = $this->getMock('F3\FLOW3\Log\SystemLoggerInterface');
+
+		$thisCache = $this->getMock('F3\FLOW3\Cache\AbstractCache', array(), array(), '', FALSE);
+		$thisCache->expects($this->any())->method('getIdentifier')->will($this->returnValue('thisCache'));
+		$thisBackend = new \F3\FLOW3\Cache\Backend\APCBackend('Testing');
+		$thisBackend->injectEnvironment($this->environment);
+		$thisBackend->injectSystemLogger($mockSystemLogger);
+		$thisBackend->setCache($thisCache);
+		$thisBackend->initializeObject();
+
+		$thatCache = $this->getMock('F3\FLOW3\Cache\AbstractCache', array(), array(), '', FALSE);
+		$thatCache->expects($this->any())->method('getIdentifier')->will($this->returnValue('thatCache'));
+		$thatBackend = new \F3\FLOW3\Cache\Backend\APCBackend('Testing');
+		$thatBackend->injectEnvironment($this->environment);
+		$thatBackend->injectSystemLogger($mockSystemLogger);
+		$thatBackend->setCache($thatCache);
+		$thatBackend->initializeObject();
+
+		$thisBackend->set('thisEntry', 'Hello');
+		$thatBackend->set('thatEntry', 'World!');
+		$thatBackend->flush();
+
+		$this->assertEquals('Hello', $thisBackend->get('thisEntry'));
+		$this->assertFalse($thatBackend->has('thatEntry'));
+	}
+
+	/**
+	 * Check if we can store ~5 MB of data, this gives some headroom for the
+	 * reflection data.
+	 *
+	 * @test
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function largeDataIsStored() {
+		$backend = $this->setUpBackend();
+
+		$data = str_repeat('abcde', 1024 * 1024);
+		$backend->set('tooLargeData', $data);
+
+		$this->assertTrue($backend->has('tooLargeData'));
+		$this->assertEquals($backend->get('tooLargeData'), $data);
+	}
+
+	/**
+	 * Sets up the memcached backend used for testing
+	 *
+	 * @param array $backendOptions Options for the memcache backend
+	 * @return \F3\FLOW3\Cache\Backend\APCBackend
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	protected function setUpBackend(array $backendOptions = array()) {
+		$mockSystemLogger = $this->getMock('F3\FLOW3\Log\SystemLoggerInterface');
+		$cache = $this->getMock('F3\FLOW3\Cache\AbstractCache', array(), array(), '', FALSE);
+		$backend = new \F3\FLOW3\Cache\Backend\APCBackend('Testing');
+		$backend->injectEnvironment($this->environment);
+		$backend->injectSystemLogger($mockSystemLogger);
+		$backend->setCache($cache);
+		$backend->initializeObject();
+		return $backend;
+	}
+
+}
+?>
