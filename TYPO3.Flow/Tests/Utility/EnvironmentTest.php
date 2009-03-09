8f6b29b92ea216f1de85d98fd147439a9694f7ef
Karsten Dambekalns <karsten@typo3.org>
2009-03-09 15:39:32 +0000

    FLOW3: * removed an useless unit test forgotten when changing stuff earlier... Original-Commit-Hash: b1e95f8080b99e1114f7ef190b2bd7d70128d0a4

--- TYPO3.Flow/Tests/Utility/EnvironmentTest.php
+++ TYPO3.Flow/Tests/Utility/EnvironmentTest.php
@@ -104,23 +104,6 @@ class EnvironmentTest extends \F3\Testing\BaseTestCase {
 
 	/**
 	 * @test
-	 * @author Karsten Dambekalns <karsten@typo3.org>
-	 */
-	public function getRequestURIReturnsExpectedURIWhenUsingRequestURI() {
-		$expectedURIString = 'http://flow3.typo3.org/is/the/base/for/typo3?5=0';
-		$environment = new \F3\FLOW3\Utility\MockEnvironment();
-		$environment->SERVER = array(
-			'HTTP_HOST' => 'flow3.typo3.org',
-			'QUERY_STRING' => '5=0',
-			'SCRIPT_NAME' => '/index.php',
-			'REQUEST_URI' => '/index.php/is/the/base/for/typo3?5=0'
-		);
-		$returnedURIString = (string)$environment->getRequestURI();
-		$this->assertEquals($expectedURIString, $returnedURIString, 'The URI returned did not match the expected value.');
-	}
-
-	/**
-	 * @test
 	 * @author Robert Lemke <robert@typo3.org>
 	 */
 	public function getRawServerEnvironmentJustReturnsTheSERVERVariable() {
