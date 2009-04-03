00ae26f297a08f392f0687da412c9cc6ae986363
Robert Lemke <robert@typo3.org>
2009-04-03 20:16:14 +0000

    * FLOW3: (MVC) Test for #3034 Original-Commit-Hash: 2c2781b5077cd3097b0cbb32ed25c9df0e6b8c34

--- TYPO3.Flow/Tests/MVC/RequestHandlerResolverTest.php
+++ TYPO3.Flow/Tests/MVC/RequestHandlerResolverTest.php
@@ -50,8 +50,10 @@ class RequestHandlerResolverTest extends \F3\Testing\BaseTestCase {
 		$mockRequestHandler2->expects($this->once())->method('getPriority')->will($this->returnValue(100));
 
 		$mockObjectManager = $this->getMock('F3\FLOW3\Object\ManagerInterface', array(), array(), '', FALSE);
-		$mockObjectManager->expects($this->at(0))->method('getObject')->with('RequestHandler1')->will($this->returnValue($mockRequestHandler1));
-		$mockObjectManager->expects($this->at(1))->method('getObject')->with('RequestHandler2')->will($this->returnValue($mockRequestHandler2));
+		$mockObjectManager->expects($this->at(0))->method('isObjectRegistered')->with('RequestHandler1')->will($this->returnValue(TRUE));
+		$mockObjectManager->expects($this->at(1))->method('getObject')->with('RequestHandler1')->will($this->returnValue($mockRequestHandler1));
+		$mockObjectManager->expects($this->at(2))->method('isObjectRegistered')->with('RequestHandler2')->will($this->returnValue(TRUE));
+		$mockObjectManager->expects($this->at(3))->method('getObject')->with('RequestHandler2')->will($this->returnValue($mockRequestHandler2));
 
 		$mockReflectionService = $this->getMock('F3\FLOW3\Reflection\Service', array(), array(), '', FALSE);
 		$mockReflectionService->expects($this->once())
