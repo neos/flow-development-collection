7e7565adfa563c0b0e8c9e580e9865f233966b8e
Christopher Hlubek <hlubek@networkteam.com>
2009-05-14 16:04:19 +0000

    * Added testcase for the DisjunctionValidator Original-Commit-Hash: 07221707eda75f1a56cc65951be09b5d79cc0c67

---
+++ TYPO3.Flow/Tests/Validation/Validator/DisjunctionValidatorTest.php
@@ -0,0 +1,148 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\FLOW3\Validation\Validator;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 framework.                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * @package FLOW3
+ * @subpackage Tests
+ * @version $Id: $
+ */
+
+/**
+ * Testcase for the Disjunction Validator
+ *
+ * @package FLOW3
+ * @subpackage Tests
+ * @version $Id: ConjunctionValidatorTest.php 2203 2009-05-12 18:44:47Z networkteam_hlubek $
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ */
+class DisjunctionValidatorTest extends \F3\Testing\BaseTestCase {
+
+	/**
+	 * @test
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function allValidatorsInTheDisjunctionAreCalledEvenIfOneReturnsTrue() {
+		$validatorDisjunction = new \F3\FLOW3\Validation\Validator\ConjunctionValidator();
+		$validatorObject = $this->getMock('F3\FLOW3\Validation\Validator\ValidatorInterface');
+		$validatorObject->expects($this->once())->method('isValid')->will($this->returnValue(TRUE));
+
+		$secondValidatorObject = $this->getMock('F3\FLOW3\Validation\Validator\ValidatorInterface');
+		$secondValidatorObject->expects($this->once())->method('isValid')->will($this->returnValue(FALSE));
+		$secondValidatorObject->expects($this->any())->method('getErrors')->will($this->returnValue(array()));
+		
+		$validatorDisjunction->addValidator($validatorObject);
+		$validatorDisjunction->addValidator($secondValidatorObject);
+
+		$validatorDisjunction->isValid('some subject');
+	}
+
+	/**
+	 * @test
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function isValidReturnsTrueIfOneValidatorReturnsTrue() {
+		$validatorDisjunction = new \F3\FLOW3\Validation\Validator\DisjunctionValidator();
+		$validatorObject = $this->getMock('F3\FLOW3\Validation\Validator\ValidatorInterface');
+		$validatorObject->expects($this->any())->method('isValid')->will($this->returnValue(TRUE));
+
+		$secondValidatorObject = $this->getMock('F3\FLOW3\Validation\Validator\ValidatorInterface');
+		$secondValidatorObject->expects($this->any())->method('isValid')->will($this->returnValue(FALSE));
+		$secondValidatorObject->expects($this->any())->method('getErrors')->will($this->returnValue(array()));
+		
+		$validatorDisjunction->addValidator($validatorObject);
+		$validatorDisjunction->addValidator($secondValidatorObject);
+
+		$this->assertTrue($validatorDisjunction->isValid('some subject'));
+	}
+
+	/**
+	 * @test
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function isValidReturnsFalseIfAllValidatorsReturnFalse() {
+		$validatorDisjunction = new \F3\FLOW3\Validation\Validator\ConjunctionValidator();
+		$validatorObject = $this->getMock('F3\FLOW3\Validation\Validator\ValidatorInterface');
+		$validatorObject->expects($this->any())->method('isValid')->will($this->returnValue(FALSE));
+		$validatorObject->expects($this->any())->method('getErrors')->will($this->returnValue(array()));
+
+		$secondValidatorObject = $this->getMock('F3\FLOW3\Validation\Validator\ValidatorInterface');
+		$secondValidatorObject->expects($this->any())->method('isValid')->will($this->returnValue(FALSE));
+		$secondValidatorObject->expects($this->any())->method('getErrors')->will($this->returnValue(array()));
+		
+		$validatorDisjunction->addValidator($validatorObject);
+		$validatorDisjunction->addValidator($secondValidatorObject);
+
+		$this->assertFalse($validatorDisjunction->isValid('some subject'));
+	}
+
+	/**
+	 * @test
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function getErrorsIsEmptyForValidDisjunctionEvenIfOneValidatorReturnsFalse() {
+		$error1 = new \F3\FLOW3\Validation\Error('foo', 1);
+		
+		$validatorDisjunction = new \F3\FLOW3\Validation\Validator\DisjunctionValidator();
+		$validatorObject = $this->getMock('F3\FLOW3\Validation\Validator\ValidatorInterface');
+		$validatorObject->expects($this->any())->method('isValid')->will($this->returnValue(TRUE));
+
+		$secondValidatorObject = $this->getMock('F3\FLOW3\Validation\Validator\ValidatorInterface');
+		$secondValidatorObject->expects($this->any())->method('isValid')->will($this->returnValue(FALSE));
+		$secondValidatorObject->expects($this->any())->method('getErrors')->will($this->returnValue(array($error1)));
+		
+		$validatorDisjunction->addValidator($validatorObject);
+		$validatorDisjunction->addValidator($secondValidatorObject);
+		
+		$validatorDisjunction->isValid('some subject');
+
+		$this->assertEquals(0, count($validatorDisjunction->getErrors()));
+	}
+
+	/**
+	 * @test
+	 * @author Christopher Hlubek <hlubek@networkteam.com>
+	 */
+	public function getErrorsReturnsAllErrorsForInvalidDisjunction() {
+		$error1 = new \F3\FLOW3\Validation\Error('foo', 1);
+		$error2 = new \F3\FLOW3\Validation\Error('bar', 2);
+		
+		$validatorDisjunction = new \F3\FLOW3\Validation\Validator\DisjunctionValidator();
+		$validatorObject = $this->getMock('F3\FLOW3\Validation\Validator\ValidatorInterface');
+		$validatorObject->expects($this->any())->method('isValid')->will($this->returnValue(FALSE));
+		$validatorObject->expects($this->any())->method('getErrors')->will($this->returnValue(array($error1)));
+
+		$secondValidatorObject = $this->getMock('F3\FLOW3\Validation\Validator\ValidatorInterface');
+		$secondValidatorObject->expects($this->any())->method('isValid')->will($this->returnValue(FALSE));
+		$secondValidatorObject->expects($this->any())->method('getErrors')->will($this->returnValue(array($error2)));
+		
+		$validatorDisjunction->addValidator($validatorObject);
+		$validatorDisjunction->addValidator($secondValidatorObject);
+		
+		$validatorDisjunction->isValid('some subject');
+
+		$this->assertEquals(array($error1, $error2), $validatorDisjunction->getErrors());
+	}
+}
+
+?>
