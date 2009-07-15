941a0c20d055f749f31c358e78f3b348b4489f93
Robert Lemke <robert@typo3.org>
2009-07-15 11:17:34 +0000

    [+BUGFIX][~TASK] FLOW3: Moved the setfilepermissions.sh script to a new location within the FLOW3 package. Also changed the strategy for setting file permissions according to the new tutorial. Original-Commit-Hash: 0a12014cac4723926fa64051699c7fb9a614afc5

---
+++ TYPO3.Flow/Scripts/setfilepermissions.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This is a little convenience script which sets / fixes the permissions of the Data
+# and the Public directory. This script will disappear as soon as we have some proper
+# installation routine in place.
+#
+# Make sure to set the webserver group name to the one used by your system.
+
+echo FLOW3 File Permission Script
+
+usage() {
+	echo
+	echo Usage: $0 \<commandlineuser\> \<webuser\> \<webgroup\>
+	echo Run as superuser, if needed
+	echo
+	exit 1
+}
+
+if [ "$#" != "3" ]; then
+  usage
+fi
+
+echo Setting file permissions, this might take a minute ...
+
+COMMANDLINE_USER="$1"
+WEBSERVER_USER="$2"
+WEBSERVER_GROUP="$3"
+
+find . -type d -exec chmod 2770 {} \;
+find . -type f -exec chmod 660 {} \;
+
+chown -R $COMMANDLINE_USER:$WEBSERVER_GROUP ./*
+
+chmod 770 flow3 
+chmod 770 $0
+
+chown -R $WEBSERVER_USER:$WEBSERVER_GROUP Public
+chmod 770 Public
+chmod 770 Public/index.php
