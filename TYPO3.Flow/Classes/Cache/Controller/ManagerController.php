a93096b9edf596e41751a55b9838c81a9f124bce
Robert Lemke <robert@typo3.org>
2009-08-10 16:00:12 +0000

    [+FEATURE] FLOW3 (Cache): Implemented a very basic controller for the cache manager which at least allows for flushing all caches in most, but not all, situations (if the code cache contains code causing fatal errors, this method unfortunately doesn't work). Resolves #4221 Original-Commit-Hash: 42927d4ed9b5d0ad4a73e1b428b1cc90ac273b02

---
+++ TYPO3.Flow/Classes/Cache/Controller/ManagerController.php
@@ -0,0 +1,64 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\FLOW3\Cache\Controller;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 framework.                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Controller for managing caches
+ *
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ */
+class ManagerController extends \F3\FLOW3\MVC\Controller\ActionController {
+
+	/**
+	 * @var F3\FLOW3\Cache\ManagerInterface
+	 */
+	protected $cacheManager;
+
+	/**
+	 * @var array
+	 */
+	protected $supportedRequestTypes = array('F3\FLOW3\MVC\CLI\Request');
+
+	/**
+	 * Injects the cache manager
+	 *
+	 * @param \F3\FLOW3\Cache\Manager $cacheManager
+	 * @return void
+	 */
+	public function injectCacheManager(\F3\FLOW3\Cache\Manager $cacheManager) {
+		$this->cacheManager = $cacheManager;
+	}
+
+	/**
+	 * Action for flushing all caches
+	 *
+	 * @return void
+	 */
+	public function flushAction() {
+		$this->cacheManager->flushCaches();
+		return 'Flushed all caches.' . PHP_EOL;
+	}
+}
+
+?>
