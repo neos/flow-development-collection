4fefb9b8be838b77e780a309709684553d1bc992
Karsten Dambekalns <karsten@typo3.org>
2009-09-23 15:30:12 +0000

    [+FEATURE] FLOW3 (Validation): Added CountValidator, resolves #3956. Original-Commit-Hash: a95bc59321d47ed4553bb823ac3cc39e35286988

---
+++ TYPO3.Flow/Classes/Validation/Validator/CountValidator.php
@@ -0,0 +1,61 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\FLOW3\Validation\Validator;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 framework.                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Validator for countable things
+ *
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @scope prototype
+ */
+class CountValidator extends \F3\FLOW3\Validation\Validator\AbstractValidator {
+
+	/**
+	 * Returns TRUE, if the given property ($propertyValue) has a valid count in the given range.
+	 *
+	 * If at least one error occurred, the result is FALSE.
+	 *
+	 * @param mixed $value The value that should be validated
+	 * @param \F3\FLOW3\Validation\Errors $errors An Errors object which will contain any errors which occurred during validation
+	 * @return boolean TRUE if the value is within the range, otherwise FALSE
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 * @api
+	 */
+	public function isValid($value) {
+		$this->errors = array();
+		if (!is_array($value) && !($value instanceof \Countable)) {
+			$this->addError('The given subject was not countable.', 1253718666);
+			return FALSE;
+		}
+
+		$min = (isset($this->options['min'])) ? intval($this->options['min']) : 0;
+		$max = (isset($this->options['max'])) ? intval($this->options['max']) : PHP_INT_MAX;
+		if (count($value) >= $min && count($value) <= $max) return TRUE;
+
+		$this->addError('The count on the given subject was not in the valid range (' . $min . ' - ' . $max . '). Got: "' . count($value) . '"', 1253718831);
+		return FALSE;
+	}
+}
+
+?>
---
+++ TYPO3.Flow/Tests/Validation/Validator/CountValidatorTest.php
@@ -0,0 +1,94 @@
+<?php
+declare(ENCODING = 'utf-8');
+namespace F3\FLOW3\Validation\Validator;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 framework.                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Testcase for the count validator
+ *
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ */
+class CountValidatorTest extends \F3\Testing\BaseTestCase {
+
+	/**
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function countables() {
+		$splObjectStorage = new \SplObjectStorage();
+		$splObjectStorage->attach(new \stdClass);
+		return array(
+			array(array('Foo', 'Bar')),
+			array(new \ArrayObject(array('Baz', 'Quux'))),
+			array($splObjectStorage)
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider countables
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function countValidatorReturnsTrueForValidCountables($countable) {
+		$countValidator = new \F3\FLOW3\Validation\Validator\CountValidator();
+		$countValidator->setOptions(array('min' => 1, 'max' => 10));
+
+		$this->assertTrue($countValidator->isValid($countable));
+	}
+
+	/**
+	 * @test
+	 * @dataProvider countables
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function countValidatorReturnsFalseForInvalidCountables($countable) {
+		$countValidator = $this->getMock('F3\FLOW3\Validation\Validator\CountValidator', array('addError'));
+		$countValidator->setOptions(array('min' => 5, 'max' => 10));
+		$this->assertFalse($countValidator->isValid($countable));
+	}
+
+	/**
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function nonCountables() {
+		$splObjectStorage = new \SplObjectStorage();
+		$splObjectStorage->attach(new \stdClass);
+		return array(
+			array('Bar'),
+			array(1),
+			array(new \stdClass)
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider nonCountables
+	 * @author Karsten Dambekalns <karsten@typo3.org>
+	 */
+	public function countValidatorReturnsFalseForNonCountables($nonCountable) {
+		$countValidator = $this->getMock('F3\FLOW3\Validation\Validator\CountValidator', array('addError'));
+		$this->assertFalse($countValidator->isValid($nonCountable));
+	}
+
+}
+
+?>
