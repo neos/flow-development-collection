21c2097dc91ae1e19e02c16c44c50d0b40f3b947
Karsten Dambekalns <karsten@typo3.org>
2010-04-08 16:02:57 +0000

    [+TASK] FLOW3 Distribution: Added Phing build file. [~TASK] FLOW3 (MVC): Removed some leftover @package annotations. [~TASK] FLOW3 (Object): Removed a leftover @package annotation. [~TASK] Fluid: Fixed some param annotations. Original-Commit-Hash: bb0903479c516841d37778a25a08802d6fc4586e

--- TYPO3.Flow/Classes/MVC/Controller/Exception/InactivePackageException.php
+++ TYPO3.Flow/Classes/MVC/Controller/Exception/InactivePackageException.php
@@ -25,8 +25,6 @@ namespace F3\FLOW3\MVC\Controller\Exception;
 /**
  * An Inactive Package Exception
  *
- * @package FLOW3
- * @subpackage MVC
  * @version $Id$
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  */
--- TYPO3.Flow/Classes/Object/ObjectSerializer.php
+++ TYPO3.Flow/Classes/Object/ObjectSerializer.php
@@ -29,8 +29,6 @@ namespace F3\FLOW3\Object;
  * is not annotated transient.
  * Afterwards it can reconstitute the objects from the array.
  *
- * @package FLOW3
- * @subpackage Object
  * @version $Id$
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  */
