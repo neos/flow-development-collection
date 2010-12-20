433bfe1925b46dbf8e81e5c804f1521d98c76c3c
Karsten Dambekalns <karsten@typo3.org>
2010-12-20 16:24:44 +0100

    [+BUGFIX] Kickstarter: Make kickstarter usable in Phoenix distribution
    Change-Id: Ie4158da47c22226676314e64143d31703e5fc7b0
    Fixes: #11597
    Original-Commit-Hash: 5e0323dfc5f090b7e8d8584facfc0d96dbf6d868

--- TYPO3.Kickstart/Classes/Service/GeneratorService.php
+++ TYPO3.Kickstart/Classes/Service/GeneratorService.php
@@ -260,7 +260,7 @@ class GeneratorService {
 	 * @param array $contextVariables
 	 */
 	protected function buildRenderingContext(array $contextVariables) {
-		$renderingContext = $this->objectManager->create('F3\Fluid\Core\Rendering\RenderingContextInterface');
+		$renderingContext = $this->objectManager->create('F3\Fluid\Core\Rendering\RenderingContext');
 
 		$renderingContext->injectTemplateVariableContainer($this->objectManager->create('F3\Fluid\Core\ViewHelper\TemplateVariableContainer', $contextVariables));
 		$renderingContext->injectViewHelperVariableContainer($this->objectManager->create('F3\Fluid\Core\ViewHelper\ViewHelperVariableContainer'));
