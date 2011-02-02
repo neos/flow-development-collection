6d3119516a05aef0d3b3cc66d8dfb11672bceb69
Karsten Dambekalns <karsten@typo3.org>
2011-02-02 15:46:37 +0100

    [~TASK] FLOW3 (Reflection): Add some @scope annotations
    They make no functional difference, but make intentions clear, eh?
    
    Change-Id: Ie805d1bf0b6f1dbc688ae8e9df90258eb22193e3
    Original-Commit-Hash: 1e9a0324c3183484fe124eb6116eafe0429cf30d

--- TYPO3.Flow/Classes/Reflection/MethodReflection.php
+++ TYPO3.Flow/Classes/Reflection/MethodReflection.php
@@ -26,6 +26,7 @@ namespace F3\FLOW3\Reflection;
  * Extended version of the ReflectionMethod
  *
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @scope prototype
  */
 class MethodReflection extends \ReflectionMethod {
 
