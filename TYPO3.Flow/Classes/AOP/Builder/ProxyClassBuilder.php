0f924c7cf6ce5fab962103422ec6a47f86ef06cb
Karsten Dambekalns <karsten@typo3.org>
2011-04-18 16:34:00 +0200

    [FEATURE] Allow AOP to work on abstract classes
    Change-Id: I5d507975a6d78de2e80aa0770bf8f343c3eb7f69
    Original-Commit-Hash: ff0f1880e4a68af0d4d5f39c9881244e7660a68d

--- TYPO3.Flow/Classes/AOP/Builder/ProxyClassBuilder.php
+++ TYPO3.Flow/Classes/AOP/Builder/ProxyClassBuilder.php
@@ -269,7 +269,6 @@ class ProxyClassBuilder {
 		foreach ($classNames as $className) {
 			if (!in_array(substr($className, 0, 12), $this->blacklistedSubPackages)) {
 				if (!$this->reflectionService->isClassTaggedWith($className, 'aspect') &&
-					!$this->reflectionService->isClassAbstract($className) &&
 					!$this->reflectionService->isClassFinal($className)) {
 					$proxyableClasses[] = $className;
 				}
@@ -427,7 +426,7 @@ class ProxyClassBuilder {
 
 		$proxyClass->addInterfaces($introducedInterfaces);
 
-		foreach($propertyIntroductions as $propertyIntroduction) {
+		foreach ($propertyIntroductions as $propertyIntroduction) {
 			$proxyClass->addProperty($propertyIntroduction->getPropertyName(), 'NULL', $propertyIntroduction->getPropertyVisibility(), $propertyIntroduction->getPropertyDocComment());
 		}
 
