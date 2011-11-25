87dcb9b190d5c7630fc1fb31b622d95a2437400c
Christopher Hlubek <hlubek@networkteam.com>
2011-11-25 15:58:54 +0100

    [TASK] Functional tests for i18n Translator and FormatResolver
    Change-Id: I4b5bc27fcebae92a8870ed1f1232779f607fec8d
    Related: #11955
    Releases: 1.1
    Original-Commit-Hash: c804bf2553993f527c422125dc9cf1dca2685c84

---
+++ TYPO3.Flow/Tests/Functional/I18n/FormatResolverTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace TYPO3\FLOW3\Tests\Functional\I18n;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 framework.                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Testcase for the I18N placeholder replacing
+ *
+ */
+class FormatResolverTest extends \TYPO3\FLOW3\Tests\FunctionalTestCase {
+
+	/**
+	 * @var \TYPO3\FLOW3\I18n\FormatResolver
+	 */
+	protected $formatResolver;
+
+	/**
+	 * Initialize dependencies
+	 */
+	public function setUp() {
+		$this->formatResolver = $this->objectManager->get('TYPO3\FLOW3\I18n\FormatResolver');
+	}
+
+	/**
+	 * @return array
+	 */
+	public function placeholderAndDateValues() {
+		$date = new \DateTime('@1322228231');
+		return array(
+			array('{0,datetime,date,short}', array($date), new \TYPO3\FLOW3\I18n\Locale('de'), '25.11.11'),
+			array('{0,datetime,date,short}', array($date), new \TYPO3\FLOW3\I18n\Locale('en'), '11/25/11'),
+			array('{0,datetime,time,full}', array($date), new \TYPO3\FLOW3\I18n\Locale('de'), '13:37:11 +00:00'),
+			array('{0,datetime,dateTime,short}', array($date), new \TYPO3\FLOW3\I18n\Locale('en'), '11/25/11 1:37 p.m.')
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider placeholderAndDateValues
+	 */
+	public function formatResolverWithDatetimeReplacesCorrectValues($stringWithPlaceholders, $arguments, $locale, $expected) {
+		$result = $this->formatResolver->resolvePlaceholders($stringWithPlaceholders, $arguments, $locale);
+		$this->assertEquals($expected, $result);
+	}
+
+}
+?>
---
+++ TYPO3.Flow/Tests/Functional/I18n/TranslatorTest.php
@@ -0,0 +1,71 @@
+<?php
+namespace TYPO3\FLOW3\Tests\Functional\I18n;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 framework.                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Testcase for the I18N translations
+ *
+ */
+class TranslatorTest extends \TYPO3\FLOW3\Tests\FunctionalTestCase {
+
+	/**
+	 * @var \TYPO3\FLOW3\I18n\Translator
+	 */
+	protected $translator;
+
+	/**
+	 * Initialize dependencies
+	 */
+	public function setUp() {
+		$this->translator = $this->objectManager->get('TYPO3\FLOW3\I18n\Translator');
+	}
+
+	/**
+	 * @return array
+	 */
+	public function idAndLocaleForTranslation() {
+		return array(
+			array('update', new \TYPO3\FLOW3\I18n\Locale('en'), 'Update'),
+			array('update', new \TYPO3\FLOW3\I18n\Locale('de'), 'Aktualisieren')
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider idAndLocaleForTranslation
+	 */
+	public function simpleTranslationByIdWorks($id, $locale, $translation) {
+		$result = $this->translator->translateById($id, array(), NULL, $locale, 'Main', 'TYPO3.FLOW3');
+		$this->assertEquals($translation, $result);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function labelAndLocaleForTranslation() {
+		return array(
+			array('Update', new \TYPO3\FLOW3\I18n\Locale('en'), 'Update'),
+			array('Update', new \TYPO3\FLOW3\I18n\Locale('de'), 'Aktualisieren')
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider labelAndLocaleForTranslation
+	 */
+	public function simpleTranslationByLabelWorks($label, $locale, $translation) {
+		$result = $this->translator->translateByOriginalLabel($label, array(), NULL, $locale, 'Main', 'TYPO3.FLOW3');
+		$this->assertEquals($translation, $result);
+	}
+
+}
+?>
