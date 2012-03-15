14be89f646952d3aca9c0e52f80510f16c9b73e3
Karsten Dambekalns <karsten@typo3.org>
2012-03-15 11:29:30 +0100

    [TASK] Fixes code style
    Change-Id: Icc19e43460194988bccd36cbc25e2eaeff72de0c
    Related: #34875
    Releases: 1.1
    Original-Commit-Hash: 1162fb4826874f16eefd30ba45c58146868e8af9

--- TYPO3.Kickstart/Classes/Package.php
+++ TYPO3.Kickstart/Classes/Package.php
@@ -11,7 +11,7 @@ namespace TYPO3\Kickstart;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-use \TYPO3\FLOW3\Package\Package as BasePackage;
+use TYPO3\FLOW3\Package\Package as BasePackage;
 
 /**
  * The Kickstart Package
