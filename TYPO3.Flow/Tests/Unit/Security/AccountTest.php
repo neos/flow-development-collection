4c82448da672e754e0495464d13c49ec91c6eef4
Rens Admiraal <rens.admiraal@typo3.org>
2012-04-11 15:11:40 +0200

    [FEATURE] Add hasRole() method to Account entity
    At the moment the roles still have to be persisted as
    strings in the serialized roles property in the database.
    Already converting all roles to objects would require
    some more refactoring.
    This change is tested on already existing, and on new
    users.
    
    Change-Id: Ibb4989f43d4e92e2e19732786999654eb6531fb8
    Releases: 1.2
    Related: #37451
    Original-Commit-Hash: a79904d9c67634f6562632db0bc4cbc45465504a

--- TYPO3.Flow/Tests/Unit/Security/AccountTest.php
+++ TYPO3.Flow/Tests/Unit/Security/AccountTest.php
@@ -57,7 +57,7 @@ class AccountTest extends \TYPO3\FLOW3\Tests\UnitTestCase {
 		$account->setRoles(array($this->role1));
 		$account->addRole($this->role1);
 
-		$this->assertAttributeEquals(array('role1'), 'roles', $account);
+		$this->assertAttributeEquals(array($this->role1), 'roles', $account);
 	}
 
 	/**
@@ -80,6 +80,46 @@ class AccountTest extends \TYPO3\FLOW3\Tests\UnitTestCase {
 		$account->removeRole($this->role2);
 
 		$this->assertAttributeEquals(array('role1'), 'roles', $account);
+	}
+
+	/**
+	 * @test
+	 */
+	public function hasRoleWorks() {
+		$account = new Account();
+		$account->setRoles(array($this->role1));
+
+		$this->assertTrue($account->hasRole($this->role1));
+		$this->assertFalse($account->hasRole($this->role2));
+	}
+
+	/**
+	 * @test
+	 */
+	public function hasRoleWorksOnRoleInstancesWithTheSameIdentifier() {
+		$account = new Account();
+		$account->addRole(new Role('role1'));
+		$this->assertEquals(array($this->role1), $account->getRoles());
+	}
+
+	/**
+	 * @test
+	 */
+	public function setRolesWorks() {
+		$account = new Account();
+		$account->setRoles(array($this->role1, $this->role2));
+
+		$this->assertEquals(array($this->role1, $this->role2), $account->getRoles());
+	}
+
+	/**
+	 * @test
+	 */
+	public function setRolesHandlesStringValuesAsRole() {
+		$account = new Account();
+		$account->setRoles(array($this->role1, 'role2'));
+
+		$this->assertEquals(array($this->role1, $this->role2), $account->getRoles());
 	}
 
 }
