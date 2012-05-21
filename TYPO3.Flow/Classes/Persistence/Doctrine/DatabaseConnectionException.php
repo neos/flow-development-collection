a10c8440a85ee1412e983d04d071026fef900310
Aske Ertmann <aske@moc.net>
2012-05-21 15:03:50 +0200

    [FEATURE] Catch PDOExceptions to prevent database information in debug backtrace
    Add catching of \PDOExceptions to the Doctrine Query class, and output the
    exception message & code, but prevent the debug backtrace leading up to
    it, since it contains the database information in the method calls.
    
    Change-Id: Id0fcb66ed3d567f5190805cc023f195f425ceb14
    Resolves: #37360
    Releases: 1.2
    Original-Commit-Hash: 4df796af78cca60f1fddb5ea09cec0081908dd86

---
+++ TYPO3.Flow/Classes/Persistence/Doctrine/DatabaseConnectionException.php
@@ -0,0 +1,21 @@
+<?php
+namespace TYPO3\FLOW3\Persistence\Doctrine;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 framework.                            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * An database connection exception
+ *
+ * @api
+ */
+class DatabaseConnectionException extends \TYPO3\FLOW3\Persistence\Exception {
+}
+?>
