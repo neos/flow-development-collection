6da4f7417136bde724cab5a389b29fa24295215e
Bastian Waidelich <bastian@typo3.org>
2012-07-06 11:16:06 +0200

    [BUGFIX] Reset InternalRequestEngine for each functional test
    Currently all functional tests share the same instance of
    \TYPO3\FLOW3\Mvc\Routing\Router because the InternalRequestEngine
    is of scope singleton.
    This leads to unexpected results and failing tests if tests add
    custom routes.
    
    This change fixes this by making the ObjectManager forget the
    singleton instance of the request engine after each testrun.
    This also reverts the changes that were introduced with
    I5a5bf17d00c5be05748a18122dae42989f429221 as they did not
    have the expected effect.
    
    Change-Id: Ie0cd7abf0a5b5021681e3db4854628990bec1e4b
    Fixes: #38435
    Releases: 1.1, 1.2
    Original-Commit-Hash: cff94d79b2146f315f1a1aff5fc8be4b30ff55ae

--- TYPO3.Flow/Tests/FunctionalTestCase.php
+++ TYPO3.Flow/Tests/FunctionalTestCase.php
@@ -228,6 +228,8 @@ abstract class FunctionalTestCase extends \TYPO3\FLOW3\Tests\BaseTestCase {
 		if (is_callable(array($persistenceManager, 'tearDown'))) {
 			$persistenceManager->tearDown();
 		}
+
+		self::$bootstrap->getObjectManager()->forgetInstance('TYPO3\FLOW3\Http\Client\InternalRequestEngine');
 		$this->emitFunctionalTestTearDown();
 	}
 
@@ -381,7 +383,6 @@ abstract class FunctionalTestCase extends \TYPO3\FLOW3\Tests\BaseTestCase {
 
 		$this->browser = new \TYPO3\FLOW3\Http\Client\Browser();
 		$this->router = $this->browser->getRequestEngine()->getRouter();
-		$this->browser->getRequestEngine()->initializeObject();
 
 		$requestHandler = self::$bootstrap->getActiveRequestHandler();
 		$requestHandler->setHttpRequest(\TYPO3\FLOW3\Http\Request::create(new \TYPO3\FLOW3\Http\Uri('http://localhost')));
