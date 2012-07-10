ebbc99c8ed3f5f6eaad1d42ed1272c95e406b611
Karsten Dambekalns <karsten@typo3.org>
2012-07-10 11:40:48 +0200

    [!!!][BUGFIX] Correct RepositoryInterface
    Fixes a docblock and adds __call() to the interface (thus the breaking
    change flag), because everyone probably would expect it to be there.
    
    Change-Id: I1cab4e16f6d1af1cbf762ea0e9b50403808bceb1
    Fixes: #38840
    Releases: 1.1, 1.2
    Original-Commit-Hash: d2ea90ac1c02d5bb63cdc3f8d1ac38041e5bee22

--- TYPO3.Flow/Classes/Persistence/Repository.php
+++ TYPO3.Flow/Classes/Persistence/Repository.php
@@ -201,27 +201,27 @@ abstract class Repository implements \TYPO3\FLOW3\Persistence\RepositoryInterfac
 	 *  - findOneBy<PropertyName>($value, $caseSensitive = TRUE)
 	 *  - countBy<PropertyName>($value, $caseSensitive = TRUE)
 	 *
-	 * @param string $methodName Name of the method
+	 * @param string $method Name of the method
 	 * @param array $arguments The arguments
 	 * @return mixed The result of the repository method
 	 * @api
 	 */
-	public function __call($methodName, array $arguments) {
+	public function __call($method, $arguments) {
 		$query = $this->createQuery();
 		$caseSensitive = isset($arguments[1]) ? (boolean)$arguments[1] : TRUE;
 
-		if (substr($methodName, 0, 6) === 'findBy' && strlen($methodName) > 7) {
-			$propertyName = lcfirst(substr($methodName, 6));
+		if (substr($method, 0, 6) === 'findBy' && strlen($method) > 7) {
+			$propertyName = lcfirst(substr($method, 6));
 			return $query->matching($query->equals($propertyName, $arguments[0], $caseSensitive))->execute();
-		} elseif (substr($methodName, 0, 7) === 'countBy' && strlen($methodName) > 8) {
-			$propertyName = lcfirst(substr($methodName, 7));
+		} elseif (substr($method, 0, 7) === 'countBy' && strlen($method) > 8) {
+			$propertyName = lcfirst(substr($method, 7));
 			return $query->matching($query->equals($propertyName, $arguments[0], $caseSensitive))->count();
-		} elseif (substr($methodName, 0, 9) === 'findOneBy' && strlen($methodName) > 10) {
-			$propertyName = lcfirst(substr($methodName, 9));
+		} elseif (substr($method, 0, 9) === 'findOneBy' && strlen($method) > 10) {
+			$propertyName = lcfirst(substr($method, 9));
 			return $query->matching($query->equals($propertyName, $arguments[0], $caseSensitive))->execute()->getFirst();
 		}
 
-		trigger_error('Call to undefined method ' . get_class($this) . '::' . $methodName, E_USER_ERROR);
+		trigger_error('Call to undefined method ' . get_class($this) . '::' . $method, E_USER_ERROR);
 	}
 
 }
