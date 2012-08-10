97160de42efda323f425abb5ab7416180d65d033
Bastian Waidelich <bastian@typo3.org>
2012-08-10 12:20:06 +0200

    [TASK] Detect inject methods more strictly
    Currently every method starting with "inject" is considered
    for property injection by the ConfigurationManager.
    That includes methods named "inject" or "injectionFoo".
    
    This change makes the detection more strict to only accept
    methods that start with "inject" followed by an upper case
    character.
    
    Change-Id: Iada3a11a9831b4c08cb93d2809c6d05c2017bd68
    Related: #39681
    Releases: 1.2
    Original-Commit-Hash: 45f872a57b3a07b7d08e3136f42effa3ebb15072

--- TYPO3.Flow/Classes/Object/Configuration/ConfigurationBuilder.php
+++ TYPO3.Flow/Classes/Object/Configuration/ConfigurationBuilder.php
@@ -360,8 +360,8 @@ class ConfigurationBuilder {
 			$properties = $objectConfiguration->getProperties();
 
 			foreach (get_class_methods($className) as $methodName) {
-				if (substr($methodName, 0, 6) === 'inject') {
-					$propertyName = strtolower(substr($methodName, 6, 1)) . substr($methodName, 7);
+				if (strlen($methodName) > 6 && substr($methodName, 0, 6) === 'inject' && $methodName[6] === strtoupper($methodName[6])) {
+					$propertyName = lcfirst(substr($methodName, 6));
 
 					$autowiringAnnotation = $this->reflectionService->getMethodAnnotation($className, $methodName, 'TYPO3\FLOW3\Annotations\Autowiring');
 					if ($autowiringAnnotation !== NULL && $autowiringAnnotation->enabled === FALSE) {
