cbffdc2151680c24dbba99043fe71cfb88ac3223
Christian Jul Jensen <julle@typo3.org>
2012-09-28 11:48:47 +0200

    [TASK] Allow package installer actions / scripts
    Allow packages to register scripts and resources to be executed or
    installed upon composer install and update.
    
    Change-Id: I06a2c5e35004f400291e7697c215bea8e685266b
    Releases: master
    Original-Commit-Hash: 0dd80fb832dd1cb58ca3e8d241f6413c32493053

--- TYPO3.Flow/Classes/TYPO3/Flow/Composer/InstallerScripts.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Composer/InstallerScripts.php
@@ -11,7 +11,8 @@ namespace TYPO3\Flow\Composer;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-use Composer\Script\Event;
+use Composer\Script\CommandEvent;
+use Composer\Script\PackageEvent;
 use TYPO3\Flow\Utility\Files;
 
 /**
@@ -26,7 +27,7 @@ class InstallerScripts {
 	 * @param \Composer\Script\Event $event
 	 * @return void
 	 */
-	static public function postUpdateAndInstall(Event $event) {
+	static public function postUpdateAndInstall(CommandEvent $event) {
 		Files::createDirectoryRecursively('Configuration');
 		Files::createDirectoryRecursively('Data');
 
@@ -35,5 +36,63 @@ class InstallerScripts {
 
 		chmod('flow', 0755);
 	}
+
+	/**
+	 * Calls actions and install scripts provided by installed packages.
+	 *
+	 * @param \Composer\Script\PackageEvent $event
+	 * @return void
+	 * @throws Exception\UnexpectedOperationException
+	 */
+	static public function postPackageUpdateAndInstall(PackageEvent $event) {
+		$operation = $event->getOperation();
+		if (!$operation instanceof \Composer\DependencyResolver\Operation\InstallOperation &&
+			!$operation instanceof \Composer\DependencyResolver\Operation\UpdateOperation) {
+			throw new Exception\UnexpectedOperationException('Handling of operation with type "' . $operation->getJobType() . '" not supported', 1348750840);
+		}
+		$package = ($operation->getJobType() === 'install') ? $operation->getPackage() : $operation->getTargetPackage();
+		$packageExtraConfig = $package->getExtra();
+
+		if (isset($packageExtraConfig['typo3/flow'])) {
+			if (isset($packageExtraConfig['typo3/flow']['post-install']) && $operation->getJobType() === 'install') {
+				self::runPackageScripts($packageExtraConfig['typo3/flow']['post-install']);
+			} elseif (isset($packageExtraConfig['typo3/flow']['post-update']) && $operation->getJobType() === 'update') {
+				self::runPackageScripts($packageExtraConfig['typo3/flow']['post-update']);
+			}
+
+			$installPath = $event->getComposer()->getInstallationManager()->getInstallPath($package);
+			$relativeInstallPath = str_replace(getcwd() . '/', '', $installPath);
+
+			if (isset($packageExtraConfig['typo3/flow']['manage-resources']) && $packageExtraConfig['typo3/flow']['manage-resources'] === TRUE) {
+				if (is_dir($relativeInstallPath . '/Resources/Private/Installer/Distribution/Essentials')) {
+					Files::copyDirectoryRecursively($relativeInstallPath . '/Resources/Private/Installer/Distribution/Essentials', '.', FALSE, TRUE);
+				}
+				if (is_dir($relativeInstallPath . '/Resources/Private/Installer/Distribution/Defaults')) {
+					Files::copyDirectoryRecursively($relativeInstallPath . '/Resources/Private/Installer/Distribution/Defaults', '.', TRUE, TRUE);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Calls a static method from it's string representation
+	 *
+	 * @param string $staticMethodReference
+	 * @return void
+	 * @throws Exception\InvalidConfigurationException
+	 */
+	static protected function runPackageScripts($staticMethodReference) {
+		$className = substr($staticMethodReference, 0, strpos($staticMethodReference, '::'));
+		$methodName = substr($staticMethodReference, strpos($staticMethodReference, '::') + 2);
+
+		if (!class_exists($className)) {
+			throw new Exception\InvalidConfigurationException('Class "' . $className . '" is not autoloadable, can not call "' . $staticMethodReference . '"', 1348751076);
+		}
+		if (!is_callable($staticMethodReference)) {
+			throw new Exception\InvalidConfigurationException('Method "' . $staticMethodReference. '" is not callable', 1348751082);
+		}
+		$className::$methodName();
+	}
 }
+
 ?>
