df88f4bf99b8715d41dc33dd8ae91a1a675b0f62
Ferdinand Kuhl <fcool@coolys.de>
2012-10-26 22:26:34 +0200

    [BUGFIX] Initialize resource wrapper before session
    If you try to attach a resource-dependent object to a session, this
    will fail, as the _wakeup() method is called before the resource
    wrappers are initialized.
    
    This patch makes sure that the resource framework is initialized
    before the session framework in the standard runtime sequence of the
    bootstrap.
    
    Fixes: #42434
    Releases: 1.2
    
    Change-Id: I22bf23618527164cad6a282bc8230a69ad309ed6
    Original-Commit-Hash: 84ef743d1530b5344b2599efea211bd311f9e16c

--- TYPO3.Flow/Classes/TYPO3/Flow/Core/Bootstrap.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Core/Bootstrap.php
@@ -314,9 +314,9 @@ class Bootstrap {
 
 		$sequence->addStep(new Step('typo3.flow:reflectionservice', array('TYPO3\Flow\Core\Booting\Scripts', 'initializeReflectionService')), 'typo3.flow:objectmanagement:runtime');
 		$sequence->addStep(new Step('typo3.flow:persistence', array('TYPO3\Flow\Core\Booting\Scripts', 'initializePersistence')), 'typo3.flow:reflectionservice');
-		$sequence->addStep(new Step('typo3.flow:session', array('TYPO3\Flow\Core\Booting\Scripts', 'initializeSession')), 'typo3.flow:persistence');
-		$sequence->addStep(new Step('typo3.flow:resources', array('TYPO3\Flow\Core\Booting\Scripts', 'initializeResources')), 'typo3.flow:session');
-		$sequence->addStep(new Step('typo3.flow:i18n', array('TYPO3\Flow\Core\Booting\Scripts', 'initializeI18n')), 'typo3.flow:resources');
+		$sequence->addStep(new Step('typo3.flow:resources', array('TYPO3\Flow\Core\Booting\Scripts', 'initializeResources')), 'typo3.flow:persistence');
+		$sequence->addStep(new Step('typo3.flow:session', array('TYPO3\Flow\Core\Booting\Scripts', 'initializeSession')), 'typo3.flow:resources');
+		$sequence->addStep(new Step('typo3.flow:i18n', array('TYPO3\Flow\Core\Booting\Scripts', 'initializeI18n')), 'typo3.flow:session');
 		return $sequence;
 	}
 
