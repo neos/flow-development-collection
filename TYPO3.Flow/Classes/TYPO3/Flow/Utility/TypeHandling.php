6259cdeaa1ad7c2a4e648001ddf2d0a0c8ec5210
Karsten Dambekalns <karsten@typo3.org>
2013-01-15 13:12:16 +0100

    [BUGFIX] Check inheritance in TypeHandling::isCollectionType()
    With this change TypeHandling::isCollectionType() does consider
    inheritance when it comes to the checking it does. Thus a subclass of
    SplObjectStorage or an implementation of Doctrine's Collection interface
    will now be seen as a collection type.
    
    Change-Id: I007b0faf486680ecfb2fe7c753ef4cabcf6d618c
    Fixes: #44535
    Releases: master, 2.0, 1.1
    Original-Commit-Hash: 59c0976870264f59ca88b186f27b2a89c4c2cd4e

--- TYPO3.Flow/Classes/TYPO3/Flow/Utility/TypeHandling.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Utility/TypeHandling.php
@@ -30,7 +30,7 @@ class TypeHandling {
 	/**
 	 * @var array
 	 */
-	static $collectionTypes = array('array', 'ArrayObject', 'SplObjectStorage', 'Doctrine\Common\Collections\Collection', 'Doctrine\Common\Collections\ArrayCollection', 'Doctrine\ORM\PersistentCollection');
+	static $collectionTypes = array('array', 'ArrayObject', 'SplObjectStorage', 'Doctrine\Common\Collections\Collection');
 
 	/**
 	 * Returns an array with type information, including element type for
@@ -46,7 +46,7 @@ class TypeHandling {
 			$type = self::normalizeType($matches['type']);
 			$elementType = isset($matches['elementType']) ? self::normalizeType($matches['elementType']) : NULL;
 
-			if ($elementType !== NULL && !in_array($type, self::$collectionTypes)) {
+			if ($elementType !== NULL && !self::isCollectionType($type)) {
 				throw new \TYPO3\Flow\Utility\Exception\InvalidTypeException('Found an invalid element type declaration in %s. Type "' . $type . '" must not have an element type hint (' . $elementType . ').', 1264093642);
 			}
 
