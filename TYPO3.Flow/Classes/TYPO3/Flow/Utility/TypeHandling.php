c6b75e359eb1abae3b1e35e86587a689b8718742
Benno Weinzierl <bw@s2intermedia.de>
2013-03-14 12:16:49 +0100

    [BUGFIX] Exception thrown in TypeHandling (follow up)
    When using isCollectionType() on an string, a warning is thrown.
    The change id Iae59221cbd72677e6c18ad0a383792a798447253 introduced this.
    
    Change-Id: Idb1a18fddae425fe03c4c1102c22133d716a6b65
    Resolves: #45876
    Releases: master, 2.0, 1.1
    Original-Commit-Hash: 5772372b5dcb5e3c7b1a24443a8db12a76bd0766

--- TYPO3.Flow/Classes/TYPO3/Flow/Utility/TypeHandling.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Utility/TypeHandling.php
@@ -114,19 +114,19 @@ class TypeHandling {
 			return TRUE;
 		}
 
-		if (class_exists($type) === TRUE) {
+		if (class_exists($type) === TRUE || interface_exists($type) === TRUE) {
 			foreach (self::$collectionTypes as $collectionType) {
 				if (is_subclass_of($type, $collectionType) === TRUE) {
 					return TRUE;
 				}
 			}
-		}
 
-			// is_subclasss_of does not check for interfaces in PHP < 5.3.7
-		if (version_compare(PHP_VERSION, '5.3.7', '<') === TRUE) {
-			foreach (self::$collectionTypes as $collectionType) {
-				if (in_array($collectionType, class_implements($type)) === TRUE) {
-					return TRUE;
+				// is_subclasss_of does not check for interfaces in PHP < 5.3.7
+			if (version_compare(PHP_VERSION, '5.3.7', '<') === TRUE) {
+				foreach (self::$collectionTypes as $collectionType) {
+					if (in_array($collectionType, class_implements($type)) === TRUE) {
+						return TRUE;
+					}
 				}
 			}
 		}
