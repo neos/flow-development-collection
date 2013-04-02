987e35608080dd044dc8bba99da8315199f4ea15
Christian Jul Jensen <julle@typo3.org>
2013-04-02 15:12:46 +0200

    [BUGFIX] generateUUID can be broken by seeding mt_rand
    Use randomizer which cannot be seeded / configured by userspace code.
    
    Change-Id: I1237fdfbee75cbfca2f2082cf30a69383dbc7fd5
    Resolves: #46862
    Releases: master, 2.0
    Original-Commit-Hash: e8f8e08e0885419651c494aff6ae804e8bf643b3

--- TYPO3.Flow/Classes/TYPO3/Flow/Utility/Algorithms.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Utility/Algorithms.php
@@ -30,11 +30,7 @@ class Algorithms {
 	 * @todo check for randomness, optionally generate type 1 and type 5 UUIDs, use php5-uuid extension if available
 	 */
 	static public function generateUUID() {
-		return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-			mt_rand( 0, 0x0fff ) | 0x4000,
-			mt_rand( 0, 0x3fff ) | 0x8000,
-			mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ) );
+		return \Security_Randomizer::getRandomGUID();
 	}
 
 	/**
