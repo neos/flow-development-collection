99c608256f96a59f114f3bcf4fabf932ce7bb227
Philipp Maier <zedd@akii.de>
2013-04-09 21:38:59 +0200

    [BUGFIX] PropertyMapper now normalizes simple types
    This fixes a bug where the property mapper was unable to find a
    valid typeconverter for properties annotated with int or bool.
    
    Change-Id: Ic9db9626613083bc8400ca3d96ca85475c7fb106
    Fixes: #47054
    Releases: master, 2.0, 1.1
    Original-Commit-Hash: 8eca52b6c44787b60a210dec1c653ca532326aff

--- TYPO3.Flow/Tests/Unit/Property/PropertyMapperTest.php
+++ TYPO3.Flow/Tests/Unit/Property/PropertyMapperTest.php
@@ -127,6 +127,22 @@ class PropertyMapperTest extends \TYPO3\Flow\Tests\UnitTestCase {
 						1 => $this->getMockTypeConverter('array2string,prio1')
 					)
 				)), 'array2string,prio10'
+			),
+			array('someStringSource', 'bool', array(
+				'string' => array(
+					'boolean' => array(
+						10 => $this->getMockTypeConverter('string2boolean,prio10'),
+						1 => $this->getMockTypeConverter('string2boolean,prio1')
+					)
+				)), 'string2boolean,prio10'
+			),
+			array('someStringSource', 'int', array(
+				'string' => array(
+					'integer' => array(
+						10 => $this->getMockTypeConverter('string2integer,prio10'),
+						1 => $this->getMockTypeConverter('string2integer,prio1')
+					)
+				)), 'string2integer,prio10'
 			)
 		);
 	}
