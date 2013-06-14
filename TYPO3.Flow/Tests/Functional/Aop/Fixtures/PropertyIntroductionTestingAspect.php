71ab4e22cdf8f69888fcc9768a7d2a5b82187eb8
Alexander Berl <a.berl@outlook.com>
2013-06-14 12:46:16 +0200

    [BUGFIX] Property introduction is broken
    This change fixes property introduction through aspects by adding
    missing checks for available property introductions.
    
    Change-Id: I0e89a6f9eca218c2e29956c7416dcede4b7d2b1a
    Fixes: #39413
    Releases: master, 2.0
    Original-Commit-Hash: 23492025086f7482d96dcc668ecb4615c31b1aa5

---
+++ TYPO3.Flow/Tests/Functional/Aop/Fixtures/PropertyIntroductionTestingAspect.php
@@ -0,0 +1,36 @@
+<?php
+namespace TYPO3\Flow\Tests\Functional\Aop\Fixtures;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow framework.                       *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * An aspect for testing the basic functionality of the AOP framework
+ *
+ * @Flow\Aspect
+ */
+class PropertyIntroductionTestingAspect {
+
+	/**
+	 * @Flow\Introduce("class(TYPO3\Flow\Tests\Functional\Aop\Fixtures\TargetClass04)")
+	 * @var string
+	 */
+	protected $introducedProtectedProperty;
+
+	/**
+	 * @Flow\Introduce("class(TYPO3\Flow\Tests\Functional\Aop\Fixtures\TargetClass04)")
+	 * @var array
+	 */
+	public $introducedPublicProperty;
+
+}
+?>
