5858c18ed6432d85c0514da767b96eefc774ba77
Karsten Dambekalns <karsten@typo3.org>
2013-08-09 14:01:29 +0200

    [TASK] Fix license in file level docblocks
    Change-Id: I8d604f5a161beeed43b0c38da7e7b9c82a23249c
    Related: #50835
    Releases: master, 2.0
    Original-Commit-Hash: 7eeeb6dd3ebc2ea09b0fb33219b25c017b41d3fb

--- TYPO3.Kickstart/Classes/TYPO3/Kickstart/Package.php
+++ TYPO3.Kickstart/Classes/TYPO3/Kickstart/Package.php
@@ -2,7 +2,7 @@
 namespace TYPO3\Kickstart;
 
 /*                                                                        *
- * This script belongs to the TYPO3 Flow package "Kickstart".             *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Kickstart".       *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
