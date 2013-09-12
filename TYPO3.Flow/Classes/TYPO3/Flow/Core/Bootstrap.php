ae1006a5dc9a172df3641405b61e8379c79e15de
Robert Lemke <robert@typo3.org>
2013-09-12 14:37:00 +0200

    Revert "[BUGFIX] Published resources don't support symlinks"
    This reverts commit 64480cd47066458bfd9a15a9a10f1760e5a0a3b4
    
    Change-Id: I762320407bc1cb624ee4ca1c51216f61831e1546
    Original-Commit-Hash: b7ac599a27ee59ce1cc6d433816a0792209565b7

--- TYPO3.Flow/Classes/TYPO3/Flow/Core/Bootstrap.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Core/Bootstrap.php
@@ -472,8 +472,7 @@ class Bootstrap {
 				}
 			}
 			if ($rootPath !== FALSE) {
-				$rootPathWithPossibleSymlinks = str_replace('//', '/', \TYPO3\Flow\Utility\Files::getUnixStylePath($rootPath) . '/');
-				$rootPath = realpath($rootPathWithPossibleSymlinks) . '/';
+				$rootPath = \TYPO3\Flow\Utility\Files::getUnixStylePath(realpath($rootPath)) . '/';
 				$testPath = \TYPO3\Flow\Utility\Files::getUnixStylePath(realpath(\TYPO3\Flow\Utility\Files::concatenatePaths(array($rootPath, 'Packages/Framework/TYPO3.Flow')))) . '/';
 				$expectedPath = \TYPO3\Flow\Utility\Files::getUnixStylePath(realpath(FLOW_PATH_FLOW)) . '/';
 				if ($testPath !== $expectedPath) {
@@ -506,7 +505,7 @@ class Bootstrap {
 		}
 
 		define('FLOW_PATH_CONFIGURATION', FLOW_PATH_ROOT . 'Configuration/');
-		define('FLOW_PATH_DATA', (isset($rootPathWithPossibleSymlinks) ? $rootPathWithPossibleSymlinks : FLOW_PATH_ROOT) . 'Data/');
+		define('FLOW_PATH_DATA', FLOW_PATH_ROOT . 'Data/');
 		define('FLOW_PATH_PACKAGES', FLOW_PATH_ROOT . 'Packages/');
 
 		define('FLOW_VERSION_BRANCH', 'dev-master');
