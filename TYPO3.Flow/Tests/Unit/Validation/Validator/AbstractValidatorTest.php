e237c0292dea65c58d29e5091c6d96e275bb3849
Michael Gerdemann <michael.gerdemann@avency.de>
2013-10-04 07:43:41 +0200

    [BUGFIX] "Required option" flag in AbstractValidator works correct
    This fix adds a boolean condition to the "Required option" flag
    condition. With this fix the flag can also be FALSE.
    
    Fixes: #52425
    Change-Id: If174b3223f0a2d48650697548cf15d5e8705cbb3
    Releases: master, 2.0
    Original-Commit-Hash: de5ed46793fe07c5d37362dcccde79df44f124f8

---
+++ TYPO3.Flow/Tests/Unit/Validation/Validator/AbstractValidatorTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace TYPO3\Flow\Tests\Unit\Validation\Validator;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow framework.                       *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Testcase for the Abstract Validator
+ *
+ */
+class AbstractValidatorTest extends \TYPO3\Flow\Tests\UnitTestCase {
+
+	protected $validator;
+
+	public function setUp() {
+		$this->validator = $this->getAccessibleMockForAbstractClass('\TYPO3\Flow\Validation\Validator\AbstractValidator', array(), '', FALSE );
+		$this->validator->_set( 'supportedOptions', array(
+			'placeHolder'   	=> array('default', 'Desc', 'mixed', FALSE),
+			'secondPlaceHolder' => array('default', 'Desc', 'mixed'),
+			'thirdPlaceHolder'  => array('default', 'Desc', 'mixed', TRUE),
+		) );
+	}
+
+	/**
+	 * @test
+	 */
+	public function abstractValidatorConstructWithRequiredOptionShouldNotFail() {
+
+		$this->validator->__construct( array( 'thirdPlaceHolder' => 'dummy' ) );
+
+		$this->assertInstanceOf('\TYPO3\Flow\Validation\Validator\AbstractValidator', $this->validator);
+
+	}
+
+	/**
+	 * @test
+	 * @expectedException \TYPO3\Flow\Validation\Exception\InvalidValidationOptionsException
+	 */
+	public function abstractValidatorConstructWithoutRequiredOptionShouldFail() {
+
+		$this->validator->__construct( array() );
+
+	}
+
+}
+
+?>
