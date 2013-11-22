ee38749896fbcbb0b7b34343ca652672dbb954c2
Robert Lemke <robert@typo3.org>
2013-11-22 20:48:26 +0100

    [FEATURE] Configuration Helper: settings
    This new helper is supposed to be the one which provides information
    about the environment. The first implemented method allows for retrieving
    settings.
    
    Example for use within TypoScript:
    
    	sitePackageKey = ${Configuration.setting(‚Acme.Demo.sitePackageKey')}
    
    Change-Id: Ibf229c6a4b5fac3663dbd7c895e78be5a976c5cc
    Releases: master
    Reviewed-on: https://review.typo3.org/25652
    Reviewed-by: Robert Lemke
    Tested-by: Robert Lemke
    Reviewed-by: Christopher Hlubek
    Original-Commit-Hash: fbf13aba9ba5c044161d6186ef6acea57e35f801

---
+++ TYPO3.Eel/Classes/TYPO3/Eel/Helper/ConfigurationHelper.php
@@ -0,0 +1,55 @@
+<?php
+namespace TYPO3\Eel\Helper;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Eel".             *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Eel\ProtectedContextAwareInterface;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Configuration\ConfigurationManager;
+
+/**
+ * Configuration helpers for Eel contexts
+ */
+class ConfigurationHelper implements ProtectedContextAwareInterface {
+
+	/**
+	 * @Flow\Inject
+	 * @var ConfigurationManager
+	 */
+	protected $configurationManager;
+
+	/**
+	 * Return the specified settings
+	 *
+	 * Examples:
+	 *
+	 *   Configuration.setting('TYPO3.Flow.core.context') === 'Production'
+	 *
+	 *   Configuration.setting('Acme.Demo.speedMode') === 'light speed'
+	 *
+	 * @param string $settingPath
+	 * @return mixed
+	 */
+	public function setting($settingPath) {
+		return $this->configurationManager->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_SETTINGS, $settingPath);
+	}
+
+	/**
+	 * All methods are considered safe
+	 *
+	 * @param string $methodName
+	 * @return boolean
+	 */
+	public function allowsCallOfMethod($methodName) {
+		return TRUE;
+	}
+
+}
