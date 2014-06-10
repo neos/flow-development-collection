435bf15cdd1f16440bb25bfe162ebd1b3c92ea9b
Marc Neuhaus <mneuhaus@famelo.com>
2014-06-11 00:27:24 +0200

    [!!!][FEATURE] Integrate Symfony/Console into CommandController
    This extends the base ``CommandController`` by some convenience
    helpers from the ``symfony/console`` package:
    
    * easy output coloring through "<error>Warning!</error>"
    * TableHelper to render values to a grid
    * ProgressHelper to render and advance and progress bars
    * DialogHelper with numerous types of questions like: select,
      ask, confirm, askHidden, etc
    
    Additionally this change improves the
    ``mapRequestArgumentsToControllerArguments()`` method to ask for
    missing required arguments instead of quitting with an exception.
    
    You can make use of the new features by calling the introduced
    proxy methods from within your CommandController:
    
    * outputTable()
    * select()
    * ask()
    * askConfirmation()
    * askHiddenResponse()
    * askAndValidate()
    * askHiddenResponseAndValidate()
    * progressStart()
    * progressSet()
    * progressAdvance()
    * progressFinish()
    
    This change does not alter the public API so it is not breaking
    in the strict sense. But it introduces a new behavior:
    Previously all outputs where collected in the ``Cli\Response``
    and only rendered to the console at the end of a CLI request.
    Now all methods producing output (inluding ``output()`` and
    ``outputLine()``) render the result directly to the console.
    
    If you use ``$this->response`` directly or let the command method
    return a string, the rendering is still deferred until the end of
    the CLI request.
    
    Resolves: #49016
    Releases: master
    Change-Id: I063742aca1898695f2e40f36b3e207248ac6e55c
    Original-Commit-Hash: 57a18f828b3c0c73c37523ed576e1f6ac9d77396

--- TYPO3.Flow/Classes/TYPO3/Flow/Cli/CommandController.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Cli/CommandController.php
@@ -11,9 +11,24 @@ namespace TYPO3\Flow\Cli;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
+use Symfony\Component\Console\Helper\DialogHelper;
+use Symfony\Component\Console\Helper\FormatterHelper;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Helper\ProgressHelper;
+use Symfony\Component\Console\Helper\TableHelper;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use TYPO3\Flow\Mvc\Controller\Argument;
 use TYPO3\Flow\Mvc\Controller\ControllerInterface;
 use TYPO3\Flow\Mvc\Controller\Arguments;
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Mvc\Exception\CommandException;
+use TYPO3\Flow\Mvc\Exception\InvalidArgumentTypeException;
+use TYPO3\Flow\Mvc\Exception\NoSuchCommandException;
+use TYPO3\Flow\Mvc\Exception\StopActionException;
+use TYPO3\Flow\Mvc\Exception\UnsupportedRequestTypeException;
+use TYPO3\Flow\Mvc\RequestInterface;
+use TYPO3\Flow\Mvc\ResponseInterface;
+use TYPO3\Flow\Reflection\ReflectionService;
 
 /**
  * A controller which processes requests from the command line
@@ -22,22 +37,47 @@ use TYPO3\Flow\Annotations as Flow;
  */
 class CommandController implements ControllerInterface {
 
+	/**
+	 * The maximum length a line should be before it's wrapped when using outputFormatted()
+	 *
+	 * @var integer
+	 */
 	const MAXIMUM_LINE_LENGTH = 79;
 
 	/**
-	 * @var \TYPO3\Flow\Cli\Request
+	 * @var Request
 	 */
 	protected $request;
 
 	/**
-	 * @var \TYPO3\Flow\Cli\Response
+	 * @var Response
 	 */
 	protected $response;
 
 	/**
-	 * @var \TYPO3\Flow\Mvc\Controller\Arguments
+	 * @var Arguments
 	 */
 	protected $arguments;
+
+	/**
+	 * @var ConsoleOutput
+	 */
+	protected $output;
+
+	/**
+	 * @var DialogHelper
+	 */
+	protected $dialogHelper;
+
+	/**
+	 * @var ProgressHelper
+	 */
+	protected $progressHelper;
+
+	/**
+	 * @var TableHelper
+	 */
+	protected $tableHelper;
 
 	/**
 	 * Name of the command method
@@ -47,40 +87,40 @@ class CommandController implements ControllerInterface {
 	protected $commandMethodName = '';
 
 	/**
-	 * @var \TYPO3\Flow\Reflection\ReflectionService
+	 * @var ReflectionService
 	 */
 	protected $reflectionService;
 
 	/**
-	 * Constructs the controller
-	 *
+	 * Constructs the command controller
 	 */
 	public function __construct() {
 		$this->arguments = new Arguments(array());
+		$this->output = new ConsoleOutput();
 	}
 
 	/**
 	 * Injects the reflection service
 	 *
-	 * @param \TYPO3\Flow\Reflection\ReflectionService $reflectionService
+	 * @param ReflectionService $reflectionService
 	 * @return void
 	 */
-	public function injectReflectionService(\TYPO3\Flow\Reflection\ReflectionService $reflectionService) {
+	public function injectReflectionService(ReflectionService $reflectionService) {
 		$this->reflectionService = $reflectionService;
 	}
 
 	/**
 	 * Processes a command line request.
 	 *
-	 * @param \TYPO3\Flow\Mvc\RequestInterface $request The request object
-	 * @param \TYPO3\Flow\Mvc\ResponseInterface $response The response, modified by this handler
+	 * @param RequestInterface $request The request object
+	 * @param ResponseInterface $response The response, modified by this handler
 	 * @return void
-	 * @throws \TYPO3\Flow\Mvc\Exception\UnsupportedRequestTypeException if the controller doesn't support the current request type
+	 * @throws UnsupportedRequestTypeException if the controller doesn't support the current request type
 	 * @api
 	 */
-	public function processRequest(\TYPO3\Flow\Mvc\RequestInterface $request, \TYPO3\Flow\Mvc\ResponseInterface $response) {
-		if (!$request instanceof \TYPO3\Flow\Cli\Request) {
-			throw new \TYPO3\Flow\Mvc\Exception\UnsupportedRequestTypeException(get_class($this) . ' only supports command line requests – requests of type "' . get_class($request) . '" given.', 1300787096);
+	public function processRequest(RequestInterface $request, ResponseInterface $response) {
+		if (!$request instanceof Request) {
+			throw new UnsupportedRequestTypeException(sprintf('%s only supports command line requests – requests of type "%s" given.', get_class($this), get_class($request)), 1300787096);
 		}
 
 		$this->request = $request;
@@ -96,16 +136,15 @@ class CommandController implements ControllerInterface {
 	/**
 	 * Resolves and checks the current command method name
 	 *
-	 * Note: The resulting command method name might not have the correct case, which isn't a problem because PHP is
-	 *       case insensitive regarding method names.
+	 * Note: The resulting command method name might not have the correct case, which isn't a problem because PHP is case insensitive regarding method names.
 	 *
 	 * @return string Method name of the current command
-	 * @throws \TYPO3\Flow\Mvc\Exception\NoSuchCommandException
+	 * @throws NoSuchCommandException
 	 */
 	protected function resolveCommandMethodName() {
 		$commandMethodName = $this->request->getControllerCommandName() . 'Command';
 		if (!is_callable(array($this, $commandMethodName))) {
-			throw new \TYPO3\Flow\Mvc\Exception\NoSuchCommandException('A command method "' . $commandMethodName . '()" does not exist in controller "' . get_class($this) . '".', 1300902143);
+			throw new NoSuchCommandException(sprintf('A command method "%s()" does not exist in controller "%s".', $commandMethodName, get_class($this)), 1300902143);
 		}
 		return $commandMethodName;
 	}
@@ -115,7 +154,7 @@ class CommandController implements ControllerInterface {
 	 * method arguments found in the designated command method.
 	 *
 	 * @return void
-	 * @throws \TYPO3\Flow\Mvc\Exception\InvalidArgumentTypeException
+	 * @throws InvalidArgumentTypeException
 	 */
 	protected function initializeCommandMethodArguments() {
 		$this->arguments->removeAll();
@@ -129,7 +168,7 @@ class CommandController implements ControllerInterface {
 				$dataType = 'array';
 			}
 			if ($dataType === NULL) {
-				throw new \TYPO3\Flow\Mvc\Exception\InvalidArgumentTypeException('The argument type for parameter $' . $parameterName . ' of method ' . get_class($this) . '->' . $this->commandMethodName . '() could not be detected.', 1306755296);
+				throw new InvalidArgumentTypeException(sprintf('The argument type for parameter $%s of method %s->%s() could not be detected.', $parameterName, get_class($this), $this->commandMethodName), 1306755296);
 			}
 			$defaultValue = (isset($parameterInfo['defaultValue']) ? $parameterInfo['defaultValue'] : NULL);
 			$this->arguments->addNewArgument($parameterName, $dataType, ($parameterInfo['optional'] === FALSE), $defaultValue);
@@ -142,15 +181,27 @@ class CommandController implements ControllerInterface {
 	 * @return void
 	 */
 	protected function mapRequestArgumentsToControllerArguments() {
+		/** @var Argument $argument */
 		foreach ($this->arguments as $argument) {
 			$argumentName = $argument->getName();
 
 			if ($this->request->hasArgument($argumentName)) {
 				$argument->setValue($this->request->getArgument($argumentName));
-			} elseif ($argument->isRequired()) {
-				$exception = new \TYPO3\Flow\Mvc\Exception\CommandException('Required argument "' . $argumentName  . '" is not set.', 1306755520);
+				continue;
+			}
+			if (!$argument->isRequired()) {
+				continue;
+			}
+			$argumentValue = NULL;
+			while ($argumentValue === NULL) {
+				$argumentValue = $this->ask(sprintf('<comment>Please specify the required argument "%s":</comment> ', $argumentName));
+			}
+
+			if ($argumentValue === NULL) {
+				$exception = new CommandException(sprintf('Required argument "%s" is not set.', $argumentName), 1306755520);
 				$this->forward('error', 'TYPO3\Flow\Command\HelpCommandController', array('exception' => $exception));
 			}
+			$argument->setValue($argumentValue);
 		}
 	}
 
@@ -164,7 +215,7 @@ class CommandController implements ControllerInterface {
 	 * @param string $controllerObjectName
 	 * @param array $arguments
 	 * @return void
-	 * @throws \TYPO3\Flow\Mvc\Exception\StopActionException
+	 * @throws StopActionException
 	 */
 	protected function forward($commandName, $controllerObjectName = NULL, array $arguments = array()) {
 		$this->request->setDispatched(FALSE);
@@ -175,7 +226,7 @@ class CommandController implements ControllerInterface {
 		$this->request->setArguments($arguments);
 
 		$this->arguments->removeAll();
-		throw new \TYPO3\Flow\Mvc\Exception\StopActionException();
+		throw new StopActionException();
 	}
 
 	/**
@@ -189,6 +240,7 @@ class CommandController implements ControllerInterface {
 	 */
 	protected function callCommandMethod() {
 		$preparedArguments = array();
+		/** @var Argument $argument */
 		foreach ($this->arguments as $argument) {
 			$preparedArguments[] = $argument->getValue();
 		}
@@ -228,7 +280,7 @@ class CommandController implements ControllerInterface {
 		if ($arguments !== array()) {
 			$text = vsprintf($text, $arguments);
 		}
-		$this->response->appendContent($text);
+		$this->output->write($text);
 	}
 
 	/**
@@ -263,16 +315,164 @@ class CommandController implements ControllerInterface {
 	}
 
 	/**
+	 * Renders a table like output of the given $rows
+	 *
+	 * @param array $rows
+	 * @param array $headers
+	 */
+	protected function outputTable($rows, $headers = NULL) {
+		$tableHelper = $this->getTableHelper();
+		if ($headers !== NULL) {
+			$tableHelper->setHeaders($headers);
+		}
+		$tableHelper->setRows($rows);
+		$tableHelper->render($this->output);
+	}
+
+	/**
+	 * Asks the user to select a value
+	 *
+	 * @param string|array $question The question to ask. If an array each array item is turned into one line of a multi-line question
+	 * @param array $choices List of choices to pick from
+	 * @param boolean $default The default answer if the user enters nothing
+	 * @param boolean $multiSelect If TRUE the result will be an array with the selected options. Multiple options can be given separated by commas
+	 * @param boolean|integer $attempts Max number of times to ask before giving up (false by default, which means infinite)
+	 * @return integer|string|array The selected value or values (the key of the choices array)
+	 * @throws \InvalidArgumentException
+	 */
+	protected function select($question, $choices, $default = NULL, $multiSelect = FALSE, $attempts = FALSE) {
+		return $this->getDialogHelper()->select($this->output, $question, $choices, $default, $attempts, 'Value "%s" is invalid', $multiSelect);
+	}
+
+	/**
+	 * Asks a question to the user
+	 *
+	 * @param string|array $question The question to ask. If an array each array item is turned into one line of a multi-line question
+	 * @param string $default The default answer if none is given by the user
+	 * @param array $autocomplete List of values to autocomplete. This only works if "stty" is installed
+	 * @return string The user answer
+	 * @throws \RuntimeException If there is no data to read in the input stream
+	 */
+	protected function ask($question, $default = NULL, array $autocomplete = NULL) {
+		return $this->getDialogHelper()->ask($this->output, $question, $default, $autocomplete);
+	}
+
+	/**
+	 * Asks a confirmation to the user.
+	 *
+	 * The question will be asked until the user answers by nothing, yes, or no.
+	 *
+	 * @param string|array $question The question to ask. If an array each array item is turned into one line of a multi-line question
+	 * @param boolean $default The default answer if the user enters nothing
+	 * @return boolean true if the user has confirmed, false otherwise
+	 */
+	protected function askConfirmation($question, $default = TRUE) {
+		return $this->getDialogHelper()->askConfirmation($this->output, $question, $default);
+	}
+
+	/**
+	 * Asks a question to the user, the response is hidden
+	 *
+	 * @param string|array $question The question. If an array each array item is turned into one line of a multi-line question
+	 * @param Boolean $fallback In case the response can not be hidden, whether to fallback on non-hidden question or not
+	 * @return string The answer
+	 * @throws \RuntimeException In case the fallback is deactivated and the response can not be hidden
+	 */
+	protected function askHiddenResponse($question, $fallback = TRUE) {
+		return $this->getDialogHelper()->askHiddenResponse($this->output, $question, $fallback);
+	}
+
+	/**
+	 * Asks for a value and validates the response
+	 *
+	 * The validator receives the data to validate. It must return the
+	 * validated data when the data is valid and throw an exception
+	 * otherwise.
+	 *
+	 * @param string|array $question The question to ask. If an array each array item is turned into one line of a multi-line question
+	 * @param callable $validator A PHP callback that gets a value and is expected to return the (transformed) value or throw an exception if it wasn't valid
+	 * @param integer|boolean $attempts Max number of times to ask before giving up (false by default, which means infinite)
+	 * @param string $default The default answer if none is given by the user
+	 * @param array $autocomplete List of values to autocomplete. This only works if "stty" is installed
+	 * @return mixed
+	 * @throws \Exception When any of the validators return an error
+	 */
+	protected function askAndValidate($question, $validator, $attempts = FALSE, $default = NULL, array $autocomplete = NULL) {
+		return $this->getDialogHelper()->askAndValidate($this->output, $question, $validator, $attempts, $default, $autocomplete);
+	}
+
+	/**
+	 * Asks for a value, hide and validates the response
+	 *
+	 * The validator receives the data to validate. It must return the
+	 * validated data when the data is valid and throw an exception
+	 * otherwise.
+	 *
+	 * @param string|array $question The question to ask. If an array each array item is turned into one line of a multi-line question
+	 * @param callable $validator A PHP callback that gets a value and is expected to return the (transformed) value or throw an exception if it wasn't valid
+	 * @param integer|boolean $attempts Max number of times to ask before giving up (false by default, which means infinite)
+	 * @param boolean $fallback In case the response can not be hidden, whether to fallback on non-hidden question or not
+	 * @return string The response
+	 * @throws \Exception When any of the validators return an error
+	 * @throws \RuntimeException In case the fallback is deactivated and the response can not be hidden
+	 */
+	protected function askHiddenResponseAndValidate($question, $validator, $attempts = FALSE, $fallback = TRUE) {
+		return $this->getDialogHelper()->askHiddenResponseAndValidate($this->output, $question, $validator, $attempts, $fallback);
+	}
+
+	/**
+	 * Starts the progress output
+	 *
+	 * @param integer $max Maximum steps. If NULL an indeterminate progress bar is rendered
+	 * @return void
+	 */
+	protected function progressStart($max = NULL) {
+		$this->getProgressHelper()->start($this->output, $max);
+	}
+
+	/**
+	 * Advances the progress output X steps
+	 *
+	 * @param integer $step Number of steps to advance
+	 * @param Boolean $redraw Whether to redraw or not
+	 * @return void
+	 * @throws \LogicException
+	 */
+	protected function progressAdvance($step = 1, $redraw = false) {
+		$this->getProgressHelper()->advance($step, $redraw);
+	}
+
+	/**
+	 * Sets the current progress
+	 *
+	 * @param integer $current The current progress
+	 * @param Boolean $redraw Whether to redraw or not
+	 * @return void
+	 * @throws \LogicException
+	 */
+	protected function progressSet($current, $redraw = false) {
+		$this->getProgressHelper()->setCurrent($current, $redraw);
+	}
+
+	/**
+	 * Finishes the progress output
+	 *
+	 * @return void
+	 */
+	protected function progressFinish() {
+		$this->getProgressHelper()->finish();
+	}
+
+	/**
 	 * Exits the CLI through the dispatcher
 	 * An exit status code can be specified @see http://www.php.net/exit
 	 *
 	 * @param integer $exitCode Exit code to return on exit
 	 * @return void
-	 * @throws \TYPO3\Flow\Mvc\Exception\StopActionException
+	 * @deprecated since Flow 2.3. This has no advantage over using \exit() any longer
 	 */
 	protected function quit($exitCode = 0) {
-		$this->response->setExitCode($exitCode);
-		throw new \TYPO3\Flow\Mvc\Exception\StopActionException();
+		exit($exitCode);
 	}
 
 	/**
@@ -285,6 +485,44 @@ class CommandController implements ControllerInterface {
 	protected function sendAndExit($exitCode = 0) {
 		$this->response->send();
 		exit($exitCode);
+	}
+
+	/**
+	 * Returns or initializes the symfony/console DialogHelper
+	 *
+	 * @return DialogHelper
+	 */
+	protected function getDialogHelper() {
+		if ($this->dialogHelper === NULL) {
+			$this->dialogHelper = new DialogHelper();
+			$helperSet = new HelperSet(array(new FormatterHelper()));
+			$this->dialogHelper->setHelperSet($helperSet);
+		}
+		return $this->dialogHelper;
+	}
+
+	/**
+	 * Returns or initializes the symfony/console ProgressHelper
+	 *
+	 * @return ProgressHelper
+	 */
+	protected function getProgressHelper() {
+		if ($this->progressHelper === NULL) {
+			$this->progressHelper = new ProgressHelper();
+		}
+		return $this->progressHelper;
+	}
+
+	/**
+	 * Returns or initializes the symfony/console TableHelper
+	 *
+	 * @return TableHelper
+	 */
+	protected function getTableHelper() {
+		if ($this->tableHelper === NULL) {
+			$this->tableHelper = new TableHelper();
+		}
+		return $this->tableHelper;
 	}
 
 }
