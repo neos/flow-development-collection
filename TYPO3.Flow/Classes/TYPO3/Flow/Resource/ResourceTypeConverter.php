0173fc754ca517e0f05a902c5bb151d71c02735e
Sebastian Kurfuerst <sebastian@typo3.org>
2014-08-29 16:09:43 +0200

    [BUGFIX] Make file uploads work again
    This change fixes a regression which was introduced by
    Ib5acdb59ee2ff9593456fe35960cdc9c41d71ace. That change
    created a second TypeConverter for Resource objects, but
    gave a wrong priority for that one -- effectively disabling
    the default type converter which handled file uploads.
    
    Here, we merge the behavior of both TypeConverters into
    a single one.
    
    When testing this, make sure to fully clear your cache.
    
    Change-Id: I479e025afbd7c382751c3d0521c18b7097b1e08f
    Related: NEOS-158
    Fixes: FLOW-60
    Releases: master, 2.2
    Original-Commit-Hash: f38b3570c95f6d25906d101d3634091cc3ecf420

--- TYPO3.Flow/Classes/TYPO3/Flow/Resource/ResourceTypeConverter.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Resource/ResourceTypeConverter.php
@@ -12,25 +12,55 @@ namespace TYPO3\Flow\Resource;
  *                                                                        */
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Error\Error;
+use TYPO3\Flow\Property\PropertyMappingConfigurationInterface;
+use TYPO3\Flow\Property\TypeConverter\AbstractTypeConverter;
+use TYPO3\Flow\Utility\Files;
 
 /**
- * An type converter for converting uploaded files to Resource objects.
+ * An type converter for converting strings, array and uploaded files to Resource objects.
  *
- * The input array is expected to be a fresh file upload following the native PHP handling. The temporary upload file
- * is then imported through the resource manager.
+ * Has two big working modes:
  *
- * To enable the handling of files that have already been uploaded earlier, the special fields ['submittedFile'],
- * ['submittedFile']['filename'] and ['submittedFile']['resourcePointer'] are checked. If set, they are used to
- * fetch a file that has already been uploaded even if no file has been actually uploaded in the current request.
+ * 1. File Uploads by PHP
+ *
+ *    In this case, the input array is expected to be a fresh file upload following the native PHP handling. The
+ *    temporary upload file is then imported through the resource manager.
+ *
+ *    To enable the handling of files that have already been uploaded earlier, the special fields ['submittedFile'],
+ *    ['submittedFile']['filename'] and ['submittedFile']['resourcePointer'] are checked. If set, they are used to
+ *    fetch a file that has already been uploaded even if no file has been actually uploaded in the current request.
+ *
+ *
+ * 2. Strings / arbitrary Arrays
+ *
+ *    If the source
+ *
+ *    - is a string looking like a SHA1 (40 characters [0-9a-f]) or
+ *    - is an array and contains the key 'hash' with a value looking like a SHA1 (40 characters [0-9a-f])
+ *
+ *    the converter will look up an existing Resource(Pointer) with that hash and return it if found. If that fails,
+ *    the converter will try to import a file named like that hash from the configured CONFIGURATION_RESOURCE_LOAD_PATH.
+ *
+ *    If no hash is given in an array source but the key 'data' is set, the content of that key is assumed a binary string
+ *    and a Resource representing this content is created and returned.
+ *
+ *    The imported Resource will be given a 'filename' if set in the source array in both cases (import from file or data).
+
  *
  * @Flow\Scope("singleton")
  */
-class ResourceTypeConverter extends \TYPO3\Flow\Property\TypeConverter\AbstractTypeConverter {
+class ResourceTypeConverter extends AbstractTypeConverter {
+
+	/**
+	 * @var string
+	 */
+	const CONFIGURATION_RESOURCE_LOAD_PATH = 'resourceLoadPath';
 
 	/**
 	 * @var array<string>
 	 */
-	protected $sourceTypes = array('array');
+	protected $sourceTypes = array('string', 'array');
 
 	/**
 	 * @var string
@@ -66,19 +96,6 @@ class ResourceTypeConverter extends \TYPO3\Flow\Property\TypeConverter\AbstractT
 	protected $convertedResources = array();
 
 	/**
-	 * Here, the TypeConverter can do some additional runtime checks to see whether
-	 * it can handle the given source data and the given target type.
-	 *
-	 * @param mixed $source the source data
-	 * @param string $targetType the type to convert to.
-	 * @return boolean TRUE if this TypeConverter can convert from $source to $targetType, FALSE otherwise.
-	 * @api
-	 */
-	public function canConvertFrom($source, $targetType) {
-		return isset($source['error']) || isset($source['submittedFile']);
-	}
-
-	/**
 	 * Converts the given string or array to a Resource object.
 	 *
 	 * This method expects an array input and assumes the resource to be a
@@ -88,10 +105,27 @@ class ResourceTypeConverter extends \TYPO3\Flow\Property\TypeConverter\AbstractT
 	 * @param array $source The upload info (expected keys: error, name, tmp_name)
 	 * @param string $targetType
 	 * @param array $convertedChildProperties
-	 * @param \TYPO3\Flow\Property\PropertyMappingConfigurationInterface $configuration
-	 * @return \TYPO3\Flow\Resource\Resource|TYPO3\Flow\Error\Error if the input format is not supported or could not be converted for other reasons
+	 * @param PropertyMappingConfigurationInterface $configuration
+	 * @return Resource|Error if the input format is not supported or could not be converted for other reasons
 	 */
-	public function convertFrom($source, $targetType, array $convertedChildProperties = array(), \TYPO3\Flow\Property\PropertyMappingConfigurationInterface $configuration = NULL) {
+	public function convertFrom($source, $targetType, array $convertedChildProperties = array(), PropertyMappingConfigurationInterface $configuration = NULL) {
+		if (is_string($source)) {
+			$source = array('hash' => $source);
+		}
+
+		// $source is ALWAYS an array at this point
+		if (isset($source['error']) || isset($source['submittedFile'])) {
+			return $this->handleFileUploads($source);
+		} elseif (isset($source['hash']) || isset($source['data'])) {
+			return $this->handleHashAndData($source, $configuration);
+		}
+	}
+
+	/**
+	 * @param array $source
+	 * @return Resource|Error|NULL
+	 */
+	protected function handleFileUploads(array $source) {
 		if (!isset($source['error']) || $source['error'] === \UPLOAD_ERR_NO_FILE) {
 			if (isset($source['submittedFile']) && isset($source['submittedFile']['filename']) && isset($source['submittedFile']['resourcePointer'])) {
 				$resourcePointer = $this->persistenceManager->getObjectByIdentifier($source['submittedFile']['resourcePointer'], 'TYPO3\Flow\Resource\ResourcePointer');
@@ -110,10 +144,10 @@ class ResourceTypeConverter extends \TYPO3\Flow\Property\TypeConverter\AbstractT
 				case \UPLOAD_ERR_INI_SIZE:
 				case \UPLOAD_ERR_FORM_SIZE:
 				case \UPLOAD_ERR_PARTIAL:
-					return new \TYPO3\Flow\Error\Error(\TYPO3\Flow\Utility\Files::getUploadErrorMessage($source['error']), 1264440823);
+					return new Error(Files::getUploadErrorMessage($source['error']), 1264440823);
 				default:
-					$this->systemLogger->log(sprintf('A server error occurred while converting an uploaded resource: "%s"', \TYPO3\Flow\Utility\Files::getUploadErrorMessage($source['error'])), LOG_ERR);
-					return new \TYPO3\Flow\Error\Error('An error occurred while uploading. Please try again or contact the administrator if the problem remains', 1340193849);
+					$this->systemLogger->log(sprintf('A server error occurred while converting an uploaded resource: "%s"', Files::getUploadErrorMessage($source['error'])), LOG_ERR);
+					return new Error('An error occurred while uploading. Please try again or contact the administrator if the problem remains', 1340193849);
 			}
 		}
 
@@ -123,9 +157,48 @@ class ResourceTypeConverter extends \TYPO3\Flow\Property\TypeConverter\AbstractT
 
 		$resource = $this->resourceManager->importUploadedResource($source);
 		if ($resource === FALSE) {
-			return new \TYPO3\Flow\Error\Error('The resource manager could not create a Resource instance.', 1264517906);
+			return new Error('The resource manager could not create a Resource instance.', 1264517906);
 		} else {
 			$this->convertedResources[$source['tmp_name']] = $resource;
+			return $resource;
+		}
+	}
+
+	/**
+	 * @param array $source
+	 * @param PropertyMappingConfigurationInterface $configuration
+	 * @return Resource|Error
+	 */
+	protected function handleHashAndData(array $source, PropertyMappingConfigurationInterface $configuration = NULL) {
+		$hash = NULL;
+		$resource = FALSE;
+		if (is_array($source) && isset($source['hash']) && preg_match('/[0-9a-f]{40}/', $source['hash'])) {
+			$hash = $source['hash'];
+		}
+
+		if ($hash !== NULL) {
+			$resourcePointer = $this->persistenceManager->getObjectByIdentifier($hash, 'TYPO3\Flow\Resource\ResourcePointer');
+			if ($resourcePointer) {
+				$resource = new Resource();
+				$resource->setFilename($source['filename']);
+				$resource->setResourcePointer($resourcePointer);
+
+				return $resource;
+			}
+		}
+
+		if (isset($source['data'])) {
+			$resource = $this->resourceManager->createResourceFromContent($source['data'], $source['filename']);
+		} elseif ($hash !== NULL) {
+			$resource = $this->resourceManager->importResource($configuration->getConfigurationValue('TYPO3\Flow\Resource\ResourceTypeConverter', self::CONFIGURATION_RESOURCE_LOAD_PATH) . '/' . $hash);
+			if (is_array($source) && isset($source['filename'])) {
+				$resource->setFilename($source['filename']);
+			}
+		}
+
+		if ($resource === FALSE) {
+			return new Error('The resource manager could not create a Resource instance.', 1404312901);
+		} else {
 			return $resource;
 		}
 	}
