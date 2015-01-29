c4d994ce04919e1a15de6e6ccd901a83ea0226bd
Rens Admiraal <rens.admiraal@typo3.org>
2015-01-29 23:10:08 +0100

    [TASK] Remove leftover table
    This change removes the typo3_flow_resource_publishing_abstractpublishingconfiguration
    table that should not be present.
    
    Change-Id: I7a6ae16f4c573ed95be928c8bca719c5d9e88a83
    Releases: master
    Resolves: FLOW-185
    Original-Commit-Hash: eb93c7b584d537419431131bab7535fdb416335a

---
+++ TYPO3.Flow/Migrations/Mysql/Version20150129225152.php
@@ -0,0 +1,31 @@
+<?php
+namespace TYPO3\Flow\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Remove leftover table from resource management changes
+ */
+class Version20150129225152 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("DROP TABLE typo3_flow_resource_publishing_abstractpublishingconfiguration");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("CREATE TABLE typo3_flow_resource_publishing_abstractpublishingconfiguration (persistence_object_identifier VARCHAR(40) NOT NULL, dtype VARCHAR(255) NOT NULL, PRIMARY KEY(persistence_object_identifier)) ENGINE = InnoDB");
+	}
+}
