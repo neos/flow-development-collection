60b11b97b69e4c93997d5d08bf13ad9f33b78ae7
Karsten Dambekalns <karsten@typo3.org>
2015-02-14 19:41:28 +0100

    [TASK] Fix unit tests failing on PHP 5.6
    Some unit tests failed under PHP 5.6 because an argument to be passed by
    reference was by value.
    The affected tests now use ``_callRef`` in the ``AccessibleMock``.
    
    This should solve those issues on HHVM as well.
    
    Change-Id: I5d5f61685637c22fc79a1ad395378052152b0267
    Releases: master, 2.3, 2.2
    Related: FLOW-194
    Original-Commit-Hash: e1a2d2033ff73b6ae96b991f3492ef5cfc3f0d38

--- TYPO3.Flow/Tests/Unit/Property/PropertyMapperTest.php
+++ TYPO3.Flow/Tests/Unit/Property/PropertyMapperTest.php
@@ -355,7 +355,7 @@ class PropertyMapperTest extends \TYPO3\Flow\Tests\UnitTestCase {
 		$targetType = 'ArrayObject';
 		$propertyPath = '';
 		$propertyMapper = $this->getAccessibleMock('TYPO3\Flow\Property\PropertyMapper', array('dummy'));
-		$this->assertSame($source, $propertyMapper->_call('doMapping', $source, $targetType, $this->mockConfiguration, $propertyPath));
+		$this->assertSame($source, $propertyMapper->_callRef('doMapping', $source, $targetType, $this->mockConfiguration, $propertyPath));
 	}
 
 	/**
@@ -366,7 +366,7 @@ class PropertyMapperTest extends \TYPO3\Flow\Tests\UnitTestCase {
 		$targetType = 'ArrayObject<SomeEntity>';
 		$propertyPath = '';
 		$propertyMapper = $this->getAccessibleMock('TYPO3\Flow\Property\PropertyMapper', array('dummy'));
-		$this->assertSame($source, $propertyMapper->_call('doMapping', $source, $targetType, $this->mockConfiguration, $propertyPath));
+		$this->assertSame($source, $propertyMapper->_callRef('doMapping', $source, $targetType, $this->mockConfiguration, $propertyPath));
 	}
 
 	/**
