f19116537a1475c4e7cd6121c83ae2209a880404
Martin Helmich <m.helmich@mittwald.de>
2015-04-07 20:55:26 +0200

    [BUGFIX] Fix "Call to undefined method ...::getPrototype()"
    When injecting dependencies that take prototype-scoped objects as
    constructor parameter, the ``ProxyClassBuilder`` class tries to
    instantiate these objects using the ``getPrototype`` method. This method
    is apparently supposed to be implemented by the proxy class, but never
    is.
    
    This results in a fatal error when the owning object is instantiated:
    
        Fatal error: Call to undefined method My\Class::getPrototype() in ...
    
    This commit fixes this behaviour by replacing the ``getPrototype`` call
    with a simple constructor call and adds a (kind-of) simple functional test
    for this behaviour.
    
    Change-Id: I52a968e9f0e694b8097c1a84890e80a75caefd45
    Releases: master, 3.0, 2.3, 2.2
    Fixes: FLOW-175
    Original-Commit-Hash: f7ffe3939cc3ef99c4fdf1d3132b54194920bde1

---
+++ TYPO3.Flow/Tests/Functional/Object/Fixtures/Flow175/OuterPrototype.php
@@ -0,0 +1,31 @@
+<?php
+namespace TYPO3\Flow\Tests\Functional\Object\Fixtures\Flow175;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow framework.                       *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+class OuterPrototype {
+
+	/**
+	 * @var GreeterInterface
+	 */
+	private $inner;
+
+	public function __construct(GreeterInterface $inner) {
+		$this->inner = $inner;
+	}
+
+	/**
+	 * @return Greeter
+	 */
+	public function getInner(){
+		return $this->inner;
+	}
+}
