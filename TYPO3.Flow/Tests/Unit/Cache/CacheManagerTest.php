3b273b3f1c6dd02857d67d39b3e511eab02731cd
Bastian Waidelich <bastian@typo3.org>
2015-04-23 12:09:38 +0200

    [BUGFIX] Fix unit test for ``CacheManager``
    This is a follow-up to the "Improve security performance" change
    (If6ccf8d7bbbe25cfcd6596a88a6192bf051bc9f5) fixing a unit test.
    
    Background:
    The backport of this change from master to the 3.0 branch did not
    include all the required changes in the unit test case.
    
    Change-Id: I7d5d62718bebdc74410b2aea8906e9f217e3e3e6
    Releases: 3.0
    Original-Commit-Hash: 50e8089e1451bdc275243dbf5815d6690b6410c9

--- TYPO3.Flow/Tests/Unit/Cache/CacheManagerTest.php
+++ TYPO3.Flow/Tests/Unit/Cache/CacheManagerTest.php
@@ -345,6 +345,7 @@ class CacheManagerTest extends \TYPO3\Flow\Tests\UnitTestCase {
 		$this->registerCache('Flow_Persistence_Doctrine');
 		$this->registerCache('Flow_Persistence_Doctrine_Results');
 		$this->registerCache('Flow_Mvc_Routing_Resolve');
+		$this->registerCache('Flow_Aop_RuntimeExpressions');
 
 		$objectClassesCache = $this->registerCache('Flow_Object_Classes');
 		$objectConfigurationCache = $this->registerCache('Flow_Object_Configuration');
