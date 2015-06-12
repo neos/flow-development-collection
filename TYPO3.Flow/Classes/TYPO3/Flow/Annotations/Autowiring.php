cbd82fc22b30a1cec10f36f95c5b258d76cfc14e
Karsten Dambekalns <karsten@typo3.org>
2015-06-12 16:28:28 +0200

    [BUGFIX] Fix warning about undefined index in Autowiring
    The Autowiring annotation did not check the existence the existence of
    the value key, leading to a warning, followed by an exception, when
    the annotation was used like::
    
      Autowiring(enable=false)
    
    instead of::
    
      Autowiring(false)
    
    and the ReflectionService was asked for reflection data on such a class.
    
    The same problem could affect Proxy annotation, so it is fixed as well.
    
    Change-Id: I439950cddd394dd690cd382078b0bb4500045dd3
    Releases: master, 3.0, 2.3, 2.2, 2.1
    Original-Commit-Hash: e4b00e9284d45bdbc01f47a34a87b4c75a167e9c

--- TYPO3.Flow/Classes/TYPO3/Flow/Annotations/Autowiring.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Annotations/Autowiring.php
@@ -30,8 +30,10 @@ final class Autowiring {
 	 * @param array $values
 	 */
 	public function __construct(array $values) {
-		if ($values['value'] !== array()) {
-			$this->enabled = isset($values['enabled']) ? (boolean)$values['enabled'] : (boolean)$values['value'];
+		if (isset($values['enabled'])) {
+			$this->enabled = (boolean)$values['enabled'];
+		} elseif (isset($values['value'])) {
+			$this->enabled = (boolean)$values['value'];
 		}
 	}
 
