ce03197ac24bf19278456b0dfda46ed7e430f3f4
Robert Lemke <robert@flownative.com>
2015-11-10 20:11:46 +0100

    Fix a copy & paste error

--- TYPO3.Flow/Classes/TYPO3/Flow/Http/Request.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Http/Request.php
@@ -95,8 +95,8 @@ class Request extends Message
 
         if ($this->headers->has('X-Forwarded-Port')) {
             $this->uri->setPort($this->headers->get('X-Forwarded-Port'));
-        } elseif ($this->headers->has('X-Forwarded-Proto') && $protocol === 'https') {
-            $this->uri->setPort(443);
+        } elseif ($this->headers->has('X-Forwarded-Proto')) {
+            $this->uri->setPort($protocol === 'https' ? 443 : 80);
         } elseif (isset($server['SERVER_PORT'])) {
             $this->uri->setPort($server['SERVER_PORT']);
         }
