75e45cd107d1399cd159b0900191f1e9e0447bcb
Bastian Waidelich <bastian@typo3.org>
2015-11-16 22:29:09 +0100

    [TASK] Re-Add unit tests for Environment

--- TYPO3.Flow/Tests/Unit/Utility/EnvironmentTest.php
+++ TYPO3.Flow/Tests/Unit/Utility/EnvironmentTest.php
@@ -21,6 +21,29 @@ class EnvironmentTest extends \TYPO3\Flow\Tests\UnitTestCase
     /**
      * @test
      */
+    public function getPathToTemporaryDirectoryReturnsPathWithTrailingSlash()
+    {
+        $environment = new \TYPO3\Flow\Utility\Environment(new ApplicationContext('Testing'));
+        $environment->setTemporaryDirectoryBase(\TYPO3\Flow\Utility\Files::concatenatePaths(array(sys_get_temp_dir(), 'FlowEnvironmentTest')));
+        $path = $environment->getPathToTemporaryDirectory();
+        $this->assertEquals('/', substr($path, -1, 1), 'The temporary path did not end with slash.');
+    }
+
+    /**
+     * @test
+     */
+    public function getPathToTemporaryDirectoryReturnsAnExistingPath()
+    {
+        $environment = new \TYPO3\Flow\Utility\Environment(new ApplicationContext('Testing'));
+        $environment->setTemporaryDirectoryBase(\TYPO3\Flow\Utility\Files::concatenatePaths(array(sys_get_temp_dir(), 'FlowEnvironmentTest')));
+
+        $path = $environment->getPathToTemporaryDirectory();
+        $this->assertTrue(file_exists($path), 'The temporary path does not exist.');
+    }
+
+    /**
+     * @test
+     */
     public function getMaximumPathLengthReturnsCorrectValue()
     {
         $environment = new \TYPO3\Flow\Utility\Environment(new ApplicationContext('Testing'));
