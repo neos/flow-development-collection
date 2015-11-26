8f051a1f4d21f8ba995dff4ae89db66d535a68fa
Robert Lemke <robert@flownative.com>
2015-11-26 17:16:27 +0100

    [TASK] Adjust exception handling for compatibility with PHP 7
    This change adjusts the non-publi API of Flow's exception handling to
    fit the new exception types in PHP 7 (`\Throwable`). It does not yet
    take advantage of the new possibilites, but rather makes the existing
    code compatible with both, PHP 5 and PHP 7.

--- TYPO3.Flow/Classes/TYPO3/Flow/Error/ProductionExceptionHandler.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Error/ProductionExceptionHandler.php
@@ -25,10 +25,10 @@ class ProductionExceptionHandler extends AbstractExceptionHandler
     /**
      * Echoes an exception for the web.
      *
-     * @param \Exception $exception The exception
+     * @param object $exception \Exception or \Throwable
      * @return void
      */
-    protected function echoExceptionWeb(\Exception $exception)
+    protected function echoExceptionWeb($exception)
     {
         $statusCode = 500;
         if ($exception instanceof FlowException) {
