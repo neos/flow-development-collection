46701a0f665a4e897102e65722ca8f7f163271fb
Christian Mueller <christian@flownative.com>
2016-03-12 14:09:50 +0100

    TASK: Move MemcacheBackendTest after rebase to master

--- Neos.Cache/Tests/Unit/Backend/MemcacheBackendTest.php
+++ Neos.Cache/Tests/Unit/Backend/MemcacheBackendTest.php
@@ -1,8 +1,10 @@
 <?php
-namespace TYPO3\Flow\Tests\Unit\Cache\Backend;
+namespace TYPO3\Flow\Cache\Tests\Unit\Backend;
+
+include_once('MemcachedBackendTest.php');
 
 /*
- * This file is part of the TYPO3.Flow package.
+ * This file is part of the Neos.Cache package.
  *
  * (c) Contributors of the Neos Project - www.neos.io
  *
