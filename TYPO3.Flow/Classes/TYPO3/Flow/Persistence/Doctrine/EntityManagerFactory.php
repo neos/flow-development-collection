ab437d80078e3bc2b2106d1e75ac0d8faa234639
Johannes Steu <js@johannessteu.de>
2016-03-22 08:50:28 +0100

    TASK: Don't set a default host for persistence backend
    Flow won't work on a server without a running mysql service because a default host ist configured.
    This fix will set the default host to an empty string so flow does also run without a running mysql service.
    
    FLOW-379 #close

--- TYPO3.Flow/Classes/TYPO3/Flow/Persistence/Doctrine/EntityManagerFactory.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Persistence/Doctrine/EntityManagerFactory.php
@@ -117,6 +117,11 @@ class EntityManagerFactory
         $config->setProxyNamespace('TYPO3\Flow\Persistence\Doctrine\Proxies');
         $config->setAutoGenerateProxyClasses(false);
 
+        // Set default host to 127.0.0.1 if there is no host configured but a dbname
+        if (empty($this->settings['backendOptions']['host']) && !empty($this->settings['backendOptions']['dbname'])) {
+            $this->settings['backendOptions']['host'] = '127.0.0.1';
+        }
+
         // The following code tries to connect first, if that succeeds, all is well. If not, the platform is fetched directly from the
         // driver - without version checks to the database server (to which no connection can be made) - and is added to the config
         // which is then used to create a new connection. This connection will then return the platform directly, without trying to
