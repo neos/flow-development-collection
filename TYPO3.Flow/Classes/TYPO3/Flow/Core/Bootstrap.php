82a4b57a3da01ed626dff49b2d4f553d8f7e9128
Karsten Dambekalns <karsten@dambekalns.de>
2016-04-28 22:46:31 +0200

    TASK: Update FLOW_VERSION_BRANCH to 3.2

--- TYPO3.Flow/Classes/TYPO3/Flow/Core/Bootstrap.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Core/Bootstrap.php
@@ -551,7 +551,7 @@ class Bootstrap
             define('FLOW_PATH_TEMPORARY', $temporaryDirectoryPath);
         }
 
-        define('FLOW_VERSION_BRANCH', 'dev-master');
+        define('FLOW_VERSION_BRANCH', '3.2');
     }
 
     /**
