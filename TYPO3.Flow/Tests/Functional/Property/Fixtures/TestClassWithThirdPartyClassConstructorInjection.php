db9d7858c18ed381485929879e48205df0dfe26a
Bastian <b.waidelich@wwwision.de>
2016-08-15 11:19:43 +0200

    BUGFIX: Add fixture for previous commit

---
+++ TYPO3.Flow/Tests/Functional/Property/Fixtures/TestClassWithThirdPartyClassConstructorInjection.php
@@ -0,0 +1,28 @@
+<?php
+namespace TYPO3\Flow\Tests\Functional\Property\Fixtures;
+
+/*
+ * This file is part of the TYPO3.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+/**
+ * A simple class for PropertyMapper test
+ *
+ */
+class TestClassWithThirdPartyClassConstructorInjection
+{
+
+    /**
+     * @param \Some\UnknownCLass $someDependency
+     */
+    public function __construct(\Some\UnknownCLass $someDependency)
+    {
+    }
+
+}
