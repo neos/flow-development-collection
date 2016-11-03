93e7988cc8283f98c907f1164d6e441cac917d5b
Dominique Feyer <dfeyer@ttree.ch>
2016-11-03 22:26:02 +0100

    TASK: Implement ServerRequestInterface getQueryParams and withQueryParams

--- TYPO3.Flow/Classes/TYPO3/Flow/Http/Request.php
+++ TYPO3.Flow/Classes/TYPO3/Flow/Http/Request.php
@@ -53,6 +53,13 @@ class Request extends BaseRequest implements ServerRequestInterface
     protected $attributes = [];
 
     /**
+     * PSR-7
+     *
+     * @var array
+     */
+    protected $queryParams = [];
+
+    /**
      * PSR-7 Attribute containing the resolved trusted client IP address as string
      */
     const ATTRIBUTE_CLIENT_IP = 'clientIpAddress';
@@ -823,14 +830,52 @@ class Request extends BaseRequest implements ServerRequestInterface
         return $newRequest;
     }
 
+    /**
+     * Retrieve query string arguments.
+     *
+     * Retrieves the deserialized query string arguments, if any.
+     *
+     * Note: the query params might not be in sync with the URI or server
+     * params. If you need to ensure you are only getting the original
+     * values, you may need to parse the query string from `getUri()->getQuery()`
+     * or from the `QUERY_STRING` server param.
+     *
+     * @return array
+     */
     public function getQueryParams()
     {
         // TODO: Implement getQueryParams() method.
+        return $this->queryParams;
     }
 
+    /**
+     * Return an instance with the specified query string arguments.
+     *
+     * These values SHOULD remain immutable over the course of the incoming
+     * request. They MAY be injected during instantiation, such as from PHP's
+     * $_GET superglobal, or MAY be derived from some other value such as the
+     * URI. In cases where the arguments are parsed from the URI, the data
+     * MUST be compatible with what PHP's parse_str() would return for
+     * purposes of how duplicate query parameters are handled, and how nested
+     * sets are handled.
+     *
+     * Setting query string arguments MUST NOT change the URI stored by the
+     * request, nor the values in the server params.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * updated query string arguments.
+     *
+     * @param array $query Array of query string arguments, typically from $_GET.
+     * @return static
+     */
     public function withQueryParams(array $query)
     {
         // TODO: Implement withQueryParams() method.
+        $newRequest = clone $this;
+        $newRequest->queryParams = $query;
+
+        return $newRequest;
     }
 
     public function getUploadedFiles()
