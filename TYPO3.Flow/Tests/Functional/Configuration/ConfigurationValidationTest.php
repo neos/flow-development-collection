3ff35454f5e31ad4d80d7bf8f7bc5e9d9e723786
Martin Ficzel <ficzel@sitegeist.de>
2016-11-21 11:13:43 +0100

    TASK: Use createMock instead of getMock to avoid deprecation warnings

--- TYPO3.Flow/Tests/Functional/Configuration/ConfigurationValidationTest.php
+++ TYPO3.Flow/Tests/Functional/Configuration/ConfigurationValidationTest.php
@@ -94,7 +94,7 @@ class ConfigurationValidationTest extends FunctionalTestCase
             }
         }
 
-        $this->mockPackageManager = $this->getMock(PackageManager::class, ['getActivePackages']);
+        $this->mockPackageManager = $this->createMock(PackageManager::class);
         $this->mockPackageManager->expects($this->any())->method('getActivePackages')->will($this->returnValue($schemaPackages));
 
         //
