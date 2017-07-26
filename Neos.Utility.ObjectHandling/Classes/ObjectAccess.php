14421dc7d4fac42a3c2b2f5dbd3d0482c9a0db4e
Christian Müller <christian@flownative.com>
2017-07-26 10:41:52 +0200

    FEATURE: Introduce ``ObjectAccess::instantiate``
    This static method allows instantiating a given class with
    and array of arguments in an efficient way.
    
    Most of this method was previously hidden in the ``ObjectManager``
    of Flow but as the same code is replicated in other packages it
    makes sense to open it as generic method for re-use.

--- Neos.Utility.ObjectHandling/Classes/ObjectAccess.php
+++ Neos.Utility.ObjectHandling/Classes/ObjectAccess.php
@@ -414,4 +414,41 @@ abstract class ObjectAccess
     {
         return 'set' . ucfirst($propertyName);
     }
+
+    /**
+     * @param string $className
+     * @param array $arguments
+     * @return object
+     */
+    public static function instantiateClass($className, $arguments)
+    {
+        switch (count($arguments)) {
+            case 0:
+                $object = new $className();
+                break;
+            case 1:
+                $object = new $className($arguments[0]);
+                break;
+            case 2:
+                $object = new $className($arguments[0], $arguments[1]);
+                break;
+            case 3:
+                $object = new $className($arguments[0], $arguments[1], $arguments[2]);
+                break;
+            case 4:
+                $object = new $className($arguments[0], $arguments[1], $arguments[2], $arguments[3]);
+                break;
+            case 5:
+                $object = new $className($arguments[0], $arguments[1], $arguments[2], $arguments[3], $arguments[4]);
+                break;
+            case 6:
+                $object = new $className($arguments[0], $arguments[1], $arguments[2], $arguments[3], $arguments[4], $arguments[5]);
+                break;
+            default:
+                $class = new \ReflectionClass($className);
+                $object = $class->newInstanceArgs($arguments);
+        }
+
+        return $object;
+    }
 }
