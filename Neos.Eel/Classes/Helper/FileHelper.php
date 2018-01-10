0c8c7b0fa9d6cc5082e7164e41a8258118d7bc12
Christian Müller <christian@flownative.com>
2018-01-10 14:51:31 +0100

    Add missing copyright headers

--- Neos.Eel/Classes/Helper/FileHelper.php
+++ Neos.Eel/Classes/Helper/FileHelper.php
@@ -1,6 +1,16 @@
 <?php
 namespace Neos\Eel\Helper;
 
+/*
+ * This file is part of the Neos.Eel package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
 use Neos\Eel\ProtectedContextAwareInterface;
 use Neos\Utility\Unicode\Functions;
 
--- Neos.Eel/Tests/Unit/Helper/FileHelperTest.php
+++ Neos.Eel/Tests/Unit/Helper/FileHelperTest.php
@@ -1,12 +1,22 @@
 <?php
 namespace Neos\Eel\Tests\Unit;
 
+/*
+ * This file is part of the Neos.Eel package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
 use Neos\Eel\Helper\FileHelper;
 use Neos\Flow\Tests\UnitTestCase;
 use org\bovigo\vfs\vfsStream;
 
 /**
- *
+ * Test for FileHelper
  */
 class FileHelperTest extends UnitTestCase
 {
