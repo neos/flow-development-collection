64d6c56f21a2244c60ccc155ada86359a4009257
Christian Müller <christian@flownative.com>
2018-01-10 14:30:08 +0100

    FEATURE: FileHelper to read files and metadata about files
    Adds a basic FileHelper to allow reading files and getting
    file metadata from EEL.

---
+++ Neos.Eel/Classes/Helper/FileHelper.php
@@ -0,0 +1,62 @@
+<?php
+namespace Neos\Eel\Helper;
+
+use Neos\Eel\ProtectedContextAwareInterface;
+use Neos\Utility\Unicode\Functions;
+
+/**
+ * Helper to read files.
+ */
+class FileHelper implements ProtectedContextAwareInterface
+{
+    /**
+     * Read and return the files contents for further use.
+     *
+     * @param string $filepath
+     * @return string
+     */
+    public function readFile(string $filepath): string
+    {
+        return file_get_contents($filepath);
+    }
+
+    /**
+     * @param string $filepath
+     * @return string
+     */
+    public function getSha1(string $filepath): string
+    {
+        return sha1_file($filepath);
+    }
+
+    /**
+     * Get file name and path information
+     *
+     * @param string $filepath
+     * @return array with keys dirname, basename, extension (if any), and filename
+     */
+    public function fileInfo(string $filepath)
+    {
+        return Functions::pathinfo($filepath);
+    }
+
+    /**
+     * Get file information like creation and modification times as well as size.
+     *
+     * @param string $filepath
+     * @return array with keys mode, uid, gid, size, atime, mtime, ctime, (blksize, blocks, dev, ino, nlink, rdev)
+     */
+    public function stat(string $filepath)
+    {
+        return stat($filepath);
+    }
+
+    /**
+     * @param string $methodName
+     * @return bool
+     */
+    public function allowsCallOfMethod($methodName)
+    {
+        return true;
+    }
+}
---
+++ Neos.Eel/Tests/Unit/Helper/FileHelperTest.php
@@ -0,0 +1,88 @@
+<?php
+namespace Neos\Eel\Tests\Unit;
+
+use Neos\Eel\Helper\FileHelper;
+use Neos\Flow\Tests\UnitTestCase;
+use org\bovigo\vfs\vfsStream;
+
+/**
+ *
+ */
+class FileHelperTest extends UnitTestCase
+{
+    public function setUp()
+    {
+        vfsStream::setup('Foo');
+    }
+
+    /**
+     * @test
+     */
+    public function readFileWillReturnFileContents()
+    {
+        $filepath = 'vfs://Foo/bar.txt';
+        $fileContent = 'Hello';
+        file_put_contents($filepath, $fileContent);
+
+        $fileHelper = new FileHelper();
+
+        self::assertEquals($fileContent, $fileHelper->readFile($filepath));
+    }
+
+    /**
+     * @test
+     */
+    public function getSha1WillReturnTheFileSha1()
+    {
+        $filepath = 'vfs://Foo/bar.txt';
+        $fileContent = 'Hello';
+        file_put_contents($filepath, $fileContent);
+        $expected = sha1_file($filepath);
+
+        $fileHelper = new FileHelper();
+
+        self::assertEquals($expected, $fileHelper->getSha1($filepath));
+    }
+
+    /**
+     * @test
+     */
+    public function getPathInfoReturnsPathInformation()
+    {
+        $filepath = 'vfs://Foo/bar.txt';
+        file_put_contents($filepath, 'does not matter');
+        $fileHelper = new FileHelper();
+
+        $result = $fileHelper->fileInfo($filepath);
+
+        self::assertTrue(is_array($result));
+        self::assertEquals('txt', $result['extension']);
+        self::assertEquals('bar', $result['filename']);
+        self::assertEquals('vfs://Foo', $result['dirname']);
+        self::assertEquals('bar.txt', $result['basename']);
+    }
+
+    /**
+     * @test
+     */
+    public function statReturnsStatInfo()
+    {
+        $filepath = 'vfs://Foo/bar.txt';
+        file_put_contents($filepath, 'does not matter');
+        $filesize = filesize($filepath);
+
+        $fileHelper = new FileHelper();
+
+        $result = $fileHelper->stat($filepath);
+
+        self::assertTrue(is_array($result));
+        self::assertArrayHasKey('mtime', $result);
+        self::assertArrayHasKey('ctime', $result);
+        self::assertArrayHasKey('atime', $result);
+        self::assertArrayHasKey('size', $result);
+        self::assertArrayHasKey('mode', $result);
+        self::assertArrayHasKey('uid', $result);
+        self::assertArrayHasKey('gid', $result);
+        self::assertEquals($filesize, $result['size']);
+    }
+}
