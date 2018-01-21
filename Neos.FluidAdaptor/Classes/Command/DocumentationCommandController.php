cc6eb7155375304ae375e4cff073345a5105590e
Markus Günther <markus.guenther@noerdisch.de>
2018-01-21 23:17:16 +0100

    TASK: Adjust XSD domain to neos.io
    The XSD file generator still uses typo3.org instead of neos.io

--- Neos.FluidAdaptor/Classes/Command/DocumentationCommandController.php
+++ Neos.FluidAdaptor/Classes/Command/DocumentationCommandController.php
@@ -35,17 +35,17 @@ class DocumentationCommandController extends CommandController
      * file to be placed online and used by any XSD-aware editor.
      * After creating the XSD file, reference it in your IDE and import the namespace
      * in your Fluid template by adding the xmlns:* attribute(s):
-     * <html xmlns="http://www.w3.org/1999/xhtml" xmlns:f="http://typo3.org/ns/TYPO3/Fluid/ViewHelpers" ...>
+     * <html xmlns="http://www.w3.org/1999/xhtml" xmlns:f="http://neos.io/ns/Neos/Neos/ViewHelpers" ...>
      *
      * @param string $phpNamespace Namespace of the Fluid ViewHelpers without leading backslash (for example 'Neos\FluidAdaptor\ViewHelpers'). NOTE: Quote and/or escape this argument as needed to avoid backslashes from being interpreted!
-     * @param string $xsdNamespace Unique target namespace used in the XSD schema (for example "http://yourdomain.org/ns/viewhelpers"). Defaults to "http://typo3.org/ns/<php namespace>".
+     * @param string $xsdNamespace Unique target namespace used in the XSD schema (for example "http://yourdomain.org/ns/viewhelpers"). Defaults to "http://neos.io/ns/<php namespace>".
      * @param string $targetFile File path and name of the generated XSD schema. If not specified the schema will be output to standard output.
      * @return void
      */
     public function generateXsdCommand($phpNamespace, $xsdNamespace = null, $targetFile = null)
     {
         if ($xsdNamespace === null) {
-            $xsdNamespace = sprintf('http://typo3.org/ns/%s', str_replace('\\', '/', $phpNamespace));
+            $xsdNamespace = sprintf('http://neos.io/ns/%s', str_replace('\\', '/', $phpNamespace));
         }
         $xsdSchema = '';
         try {
