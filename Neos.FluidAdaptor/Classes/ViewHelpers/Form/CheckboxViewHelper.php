c4b15e8addab24a0e9987860e01d8a5273b47f56
Karsten Dambekalns <karsten@dambekalns.de>
2018-03-07 12:49:16 +0100

    BUGFIX: Detect checked state for bound object collections
    When a collection property with obejcts is bound to a checkbox VH,
    this change makes sure to check object identifiers against the passed
    value when automatically determining the `checked` state.
    
    Fixes #1229

--- Neos.FluidAdaptor/Classes/ViewHelpers/Form/CheckboxViewHelper.php
+++ Neos.FluidAdaptor/Classes/ViewHelpers/Form/CheckboxViewHelper.php
@@ -11,6 +11,9 @@ namespace Neos\FluidAdaptor\ViewHelpers\Form;
  * source code.
  */
 
+use Neos\Utility\Arrays;
+use Neos\Utility\TypeHandling;
+
 /**
  * View Helper which creates a simple checkbox (<input type="checkbox">).
  *
@@ -89,7 +92,22 @@ class CheckboxViewHelper extends AbstractFormFieldViewHelper
         }
         if (is_array($propertyValue)) {
             if ($checked === null) {
-                $checked = in_array($valueAttribute, $propertyValue, true);
+                if (TypeHandling::isSimpleType(TypeHandling::getTypeForValue(current($propertyValue))) === false || Arrays::containsMultipleTypes($propertyValue)) {
+                    $checked = false;
+                    foreach ($propertyValue as $value) {
+                        if (TypeHandling::isSimpleType(TypeHandling::getTypeForValue(current($propertyValue)))) {
+                            $checked = $valueAttribute === $value;
+                        } else {
+                            // assume an entity
+                            $checked = $valueAttribute === $this->persistenceManager->getIdentifierByObject($value);
+                        }
+                        if ($checked === true) {
+                            break;
+                        }
+                    }
+                } else {
+                    $checked = in_array($valueAttribute, $propertyValue, true);
+                }
             }
             $this->arguments['multiple'] = true;
         } elseif (!$multiple && $propertyValue !== null) {
