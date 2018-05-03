28b0e5d63cf98c5303ab52a99e473eed1aa9ce27
Markus Günther <mukguenther@gmail.com>
2018-05-03 20:41:04 +0200

    FEATURE: Add parameter xsd-domain to the command
    With the new optional parameter --xsd-domain the users are able to generate
    xsd files for their own view helpers with the own domain.

--- Neos.FluidAdaptor/Classes/Command/DocumentationCommandController.php
+++ Neos.FluidAdaptor/Classes/Command/DocumentationCommandController.php
@@ -35,17 +35,25 @@ class DocumentationCommandController extends CommandController
      * file to be placed online and used by any XSD-aware editor.
      * After creating the XSD file, reference it in your IDE and import the namespace
      * in your Fluid template by adding the xmlns:* attribute(s):
-     * <html xmlns="http://www.w3.org/1999/xhtml" xmlns:f="http://neos.io/ns/Neos/Neos/ViewHelpers" ...>
+     * <html xmlns="http://www.w3.org/1999/xhtml" xmlns:f="http://typo3.org/ns/TYPO3/Fluid/ViewHelpers" ...>
      *
      * @param string $phpNamespace Namespace of the Fluid ViewHelpers without leading backslash (for example 'Neos\FluidAdaptor\ViewHelpers'). NOTE: Quote and/or escape this argument as needed to avoid backslashes from being interpreted!
-     * @param string $xsdNamespace Unique target namespace used in the XSD schema (for example "http://yourdomain.org/ns/viewhelpers"). Defaults to "http://neos.io/ns/<php namespace>".
+     * @param string $xsdNamespace Unique target namespace used in the XSD schema (for example "http://yourdomain.org/ns/viewhelpers"). Defaults to "http://typo3.org/ns/<php namespace>".
      * @param string $targetFile File path and name of the generated XSD schema. If not specified the schema will be output to standard output.
+     * @param string $xsdDomain Domain used in the XSD schema (for example "http://yourdomain.org"). Defaults to "http://typo3.org".
      * @return void
      */
-    public function generateXsdCommand($phpNamespace, $xsdNamespace = null, $targetFile = null)
+    public function generateXsdCommand(
+        $phpNamespace, $xsdNamespace = null, $targetFile = null, $xsdDomain = ''
+    )
     {
+        $xsdDomain = trim($xsdDomain);
+        $parsedDomain = parse_url($xsdDomain);
+        if (empty($xsdDomain) || !isset($parsedDomain['host'], $parsedDomain['scheme'])) {
+            $xsdDomain = 'http://typo3.org';
+        }
         if ($xsdNamespace === null) {
-            $xsdNamespace = sprintf('http://neos.io/ns/%s', str_replace('\\', '/', $phpNamespace));
+            $xsdNamespace = sprintf('%s/ns/%s', $xsdDomain, str_replace('\\', '/', $phpNamespace));
         }
         $xsdSchema = '';
         try {
