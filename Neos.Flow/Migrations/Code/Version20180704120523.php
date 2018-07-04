c6f687787aacdfb35aa643562327c76b971c422e
Max Strübing <mxstrbng@gmail.com>
2018-07-04 12:10:28 +0200

    TASK: Add Code Migration for allowsCallsOfMehtod

---
+++ Neos.Flow/Migrations/Code/Version20180704120523.php
@@ -0,0 +1,34 @@
+<?php
+namespace Neos\Flow\Core\Migrations;
+
+/*
+ * This file is part of the Neos.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+/**
+ * Add scalar type hint to allowsCallOfMethod implementations
+ */
+class Version20180704120523 extends AbstractMigration
+{
+    /**
+     * @return string
+     */
+    public function getIdentifier() : string
+    {
+        return 'Neos.Flow-20180704120523';
+    }
+
+    /**
+     * @return void
+     */
+    public function up() : void
+    {
+        $this->searchAndReplace('public function allowsCallOfMethod($methodName)', 'public function allowsCallOfMethod(string $methodName) : bool', ['php']);
+    }
+}
