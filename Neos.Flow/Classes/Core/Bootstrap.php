9a32abec88cdc33964822c856e29ecdff5bddcdb
Helmut Hummel <info@helhum.io>
2018-09-12 22:44:09 +0200

    [TASK] Require composer autoloader in entry scripts
    Instead of requiring the bootstrap class file and
    then requiring Composer autoloader in the Bootstrap constructor,
    we can require the autoloader early in the two entry scripts.
    
    This makes it possible to set FLOW_CONTEXT through .env files
    e.g with packages like helhum/dotenv-connector

--- Neos.Flow/Classes/Core/Bootstrap.php
+++ Neos.Flow/Classes/Core/Bootstrap.php
@@ -79,11 +79,12 @@ class Bootstrap
      * Constructor
      *
      * @param string $context The application context, for example "Production" or "Development"
+     * @param \Composer\Autoload\ClassLoader $composerAutoloader Composer autoloader
      */
-    public function __construct(string $context)
+    public function __construct(string $context, \Composer\Autoload\ClassLoader $composerAutoloader = null)
     {
-        // Load the composer autoloader first
-        $composerAutoloader = require(__DIR__ . '/../../../../Libraries/autoload.php');
+        // Load the composer autoloader first if not provided
+        $composerAutoloader = $composerAutoloader ?? require(__DIR__ . '/../../../../Libraries/autoload.php');
 
         $this->context = new ApplicationContext($context);
         $this->earlyInstances[__CLASS__] = $this;
