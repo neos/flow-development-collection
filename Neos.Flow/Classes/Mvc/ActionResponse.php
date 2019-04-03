429c09d5874e4700de58db8f19011cf9e204a650
Christian Müller <christian@flownative.com>
2019-04-03 10:45:31 +0200

    Add ``setHeader`` to ``ActionResponse``

--- Neos.Flow/Classes/Mvc/ActionResponse.php
+++ Neos.Flow/Classes/Mvc/ActionResponse.php
@@ -2,11 +2,13 @@
 namespace Neos\Flow\Mvc;
 
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Http\AbstractMessage;
 use Psr\Http\Message\UriInterface;
 
 /**
- * The new minimal MVC response object.
- * It allows for simple interactions with the HTTP response from within MVC actions. More specific requirements can be implemented via HTTP Components. @see setComponentParameter() 
+ * The minimal MVC response object.
+ * It allows for simple interactions with the HTTP response from within MVC actions. More specific requirements can be implemented via HTTP Components.
+ * @see setComponentParameter()
  *
  * @Flow\Proxy(false)
  * @api
@@ -51,7 +53,7 @@ final class ActionResponse extends \Neos\Flow\Http\Response
     {
         $this->contentType = $contentType;
         // TODO: This can be removed after the full changes are done for next major.
-        $this->headers->set('Content-Type', $contentType);
+        $this->setHeader('Content-Type', $contentType, true);
     }
 
     /**
@@ -67,7 +69,7 @@ final class ActionResponse extends \Neos\Flow\Http\Response
         $this->redirectUri = $uri;
         $this->statusCode = $statusCode;
         // TODO: This can be removed after the full changes are done for next major.
-        $this->headers->set('Location', (string)$uri);
+        $this->setHeader('Location', (string)$uri, true);
         $this->setStatusCode($statusCode);
     }
 
@@ -82,6 +84,42 @@ final class ActionResponse extends \Neos\Flow\Http\Response
     public function setStatusCode(int $statusCode): void
     {
         $this->statusCode = $statusCode;
+    }
+
+    /**
+     * Sets the specified HTTP header
+     *
+     * In the next major this will
+     *
+     * Please note that dates are normalized to GMT internally, so that getHeader() will return
+     * the same point in time, but not necessarily in the same timezone, if it was not
+     * GMT previously. GMT is used synonymously with UTC as per RFC 2616 3.3.1.
+     *
+     * @param string $name Name of the header, for example "Location", "Content-Description" etc.
+     * @param string|string[]|\DateTime $values An array of values or a single value for the specified header field
+     * @param boolean $replaceExistingHeader If a header with the same name should be replaced. Default is true.
+     * @return void
+     * @throws \InvalidArgumentException
+     * @see withHeader()
+     * @see withAddedHeader()
+     */
+    public function setHeader($name, $values, $replaceExistingHeader = true): void
+    {
+        switch ($name) {
+            case 'Content-Type':
+                if (is_array($values)) {
+                    if (count($values) !== 1) {
+                        throw new \InvalidArgumentException('The "Content-Type" header must be unique and thus only one field value may be specified.', 1454949291);
+                    }
+                    $values = (string)$values[0];
+                }
+                if (stripos($values, 'charset') === false && stripos($values, 'text/') === 0) {
+                    $values .= '; charset=UTF-8';
+                }
+                break;
+        }
+
+        $this->headers->set($name, $values, $replaceExistingHeader);
     }
 
     /**
