7ab60e430e9262235e7a7f1e2cc416b1010bb593
Kay Strobach <github@kay-strobach.de>
2019-05-22 16:35:33 +0200

    [BUGFIX] avoid problem with loading files in simplexml
    Workaround for https://bugs.php.net/bug.php?id=62577
    
    fixes #1598

--- Neos.Flow/Classes/I18n/AbstractXmlParser.php
+++ Neos.Flow/Classes/I18n/AbstractXmlParser.php
@@ -55,7 +55,7 @@ abstract class AbstractXmlParser
             throw new Exception\InvalidXmlFileException('The path "' . $sourcePath . '" does not point to an existing and accessible XML file.', 1328879703);
         }
         libxml_use_internal_errors(true);
-        $rootXmlNode = simplexml_load_file($sourcePath, 'SimpleXmlElement', \LIBXML_NOWARNING);
+        $rootXmlNode = simplexml_load_string(file_get_contents($sourcePath), 'SimpleXmlElement', \LIBXML_NOWARNING);
         if ($rootXmlNode === false) {
             $errors = [];
             foreach (libxml_get_errors() as $error) {
