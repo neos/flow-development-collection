a8ac0edd15afe720108b06a2816f9a6b2600e906
Alexander Berl <a.berl@outlook.com>
2019-08-23 18:54:49 +0200

    TASK: Fix ScriptTest invoking dummy commands
    Also, the static class does not need to be mocked with PHPUnit, as PHPUnit can not stub static methods anyway.

--- Neos.Flow/Tests/Unit/Core/Booting/ScriptsTest.php
+++ Neos.Flow/Tests/Unit/Core/Booting/ScriptsTest.php
@@ -14,24 +14,21 @@ namespace Neos\Flow\Tests\Unit\Core\Booting;
 use Neos\Flow\Core\Booting\Scripts;
 use Neos\Flow\Tests\UnitTestCase;
 
+class ScriptsMock extends Scripts {
+    protected static function ensureCLISubrequestsUseCurrentlyRunningPhpBinary($phpBinaryPathAndFilename)
+    {
+    }
+
+    protected static function ensureWebSubrequestsUseCurrentlyRunningPhpVersion($phpCommand)
+    {
+    }
+}
+
 /**
  * Testcase for the initialization scripts
  */
 class ScriptsTest extends UnitTestCase
 {
-    /**
-     * @var Scripts|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $scriptsMock;
-
-    /**
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-        $this->scriptsMock = $this->getAccessibleMock(Scripts::class, ['dummy']);
-    }
-
     /**
      * @test
      */
@@ -43,22 +40,22 @@ class ScriptsTest extends UnitTestCase
         ]];
 
         $message = 'The command must contain the current ini because it is not explicitly set in settings.';
-        $actual = $this->scriptsMock->_call('buildSubprocessCommand', 'flow:foo:identifier', $settings);
+        $actual = ScriptsMock::buildSubprocessCommand('flow:foo:identifier', $settings);
         $this->assertContains(sprintf(' -c %s ', escapeshellarg(php_ini_loaded_file())), $actual, $message);
 
         $settings['core']['subRequestPhpIniPathAndFilename'] = null;
         $message = 'The command must contain the current ini because it is explicitly set, but NULL, in settings.';
-        $actual = $this->scriptsMock->_call('buildSubprocessCommand', 'flow:foo:identifier', $settings);
+        $actual = ScriptsMock::buildSubprocessCommand('flow:foo:identifier', $settings);
         $this->assertContains(sprintf(' -c %s ', escapeshellarg(php_ini_loaded_file())), $actual, $message);
 
         $settings['core']['subRequestPhpIniPathAndFilename'] = '/foo/ini/path';
         $message = 'The command must contain a specified ini file path because it is set in settings.';
-        $actual = $this->scriptsMock->_call('buildSubprocessCommand', 'flow:foo:identifier', $settings);
+        $actual = ScriptsMock::buildSubprocessCommand('flow:foo:identifier', $settings);
         $this->assertContains(sprintf(' -c %s ', escapeshellarg('/foo/ini/path')), $actual, $message);
 
         $settings['core']['subRequestPhpIniPathAndFilename'] = false;
         $message = 'The command must not contain an ini file path because it is set to FALSE in settings.';
-        $actual = $this->scriptsMock->_call('buildSubprocessCommand', 'flow:foo:identifier', $settings);
+        $actual = ScriptsMock::buildSubprocessCommand('flow:foo:identifier', $settings);
         $this->assertNotContains(' -c ', $actual, $message);
     }
 
@@ -72,7 +69,7 @@ class ScriptsTest extends UnitTestCase
             'phpBinaryPathAndFilename' => '/must/be/set/according/to/schema',
             'subRequestIniEntries' => ['someSetting' => 'withValue', 'someFlagSettingWithoutValue' => '']
         ]];
-        $actual = $this->scriptsMock->_call('buildSubprocessCommand', 'flow:foo:identifier', $settings);
+        $actual = ScriptsMock::buildSubprocessCommand('flow:foo:identifier', $settings);
 
         $this->assertContains(sprintf(' -d %s=%s ', escapeshellarg('someSetting'), escapeshellarg('withValue')), $actual);
         $this->assertContains(sprintf(' -d %s ', escapeshellarg('someFlagSettingWithoutValue')), $actual);
