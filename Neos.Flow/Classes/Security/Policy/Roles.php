a285c0ffab71ebe68e30d5fd1ad466a1deeee431
Soren Malling <soren@meteko.dk>
2020-03-15 20:59:23 +0100

    [TASK] Correct copyright and strict_types order and declaration

--- Neos.Flow/Classes/Security/Policy/Roles.php
+++ Neos.Flow/Classes/Security/Policy/Roles.php
@@ -1,19 +1,19 @@
 <?php
 declare(strict_types=1);
 
+/*
+ * This file is part of the Neos.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
 namespace Neos\Flow\Security\Policy;
 
 use Neos\Flow\Annotations as Flow;
-
-/*
-* This file is part of the Neos.Flow package.
-*
-* (c) Contributors of the Neos Project - www.neos.io
-*
-* This package is Open Source Software. For the full copyright and license
-* information, please view the LICENSE file which was distributed with this
-* source code.
-*/
 
 /**
  * A collection class containing all roles for an account
