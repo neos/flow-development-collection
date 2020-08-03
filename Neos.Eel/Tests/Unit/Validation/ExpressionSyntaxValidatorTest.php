d19f7a0e8574a88f580dcc67678e87a78841a1bd
The Neos Team <hello@neos.io>
2020-08-03 19:48:53 +0000

    Apply fixes from StyleCI

--- Neos.Eel/Tests/Unit/Validation/ExpressionSyntaxValidatorTest.php
+++ Neos.Eel/Tests/Unit/Validation/ExpressionSyntaxValidatorTest.php
@@ -20,7 +20,8 @@ class ExpressionSyntaxValidatorTest extends AbstractValidatorTestcase
     public function validExpressionPasses()
     {
         self::assertFalse(
-            $this->validator->validate('foo.bar() * (18 + 2)')->hasErrors());
+            $this->validator->validate('foo.bar() * (18 + 2)')->hasErrors()
+        );
     }
 
     /**
@@ -29,7 +30,8 @@ class ExpressionSyntaxValidatorTest extends AbstractValidatorTestcase
     public function invalidExpressionIsConsideredErroneous()
     {
         self::assertTrue(
-            $this->validator->validate('foo.bar( + (18 + 2)')->hasErrors());
+            $this->validator->validate('foo.bar( + (18 + 2)')->hasErrors()
+        );
     }
 
     /**
