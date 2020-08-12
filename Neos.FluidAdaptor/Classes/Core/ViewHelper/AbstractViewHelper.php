c81d4d66b5d04e15b30d60affe0586c02db93fe3
Karsten Dambekalns <kdambekalns@users.noreply.github.com>
2020-08-12 15:18:01 +0000

    Apply fixes from StyleCI

--- Neos.FluidAdaptor/Classes/Core/ViewHelper/AbstractViewHelper.php
+++ Neos.FluidAdaptor/Classes/Core/ViewHelper/AbstractViewHelper.php
@@ -125,7 +125,8 @@ abstract class AbstractViewHelper extends FluidAbstractViewHelper
                 throw $exception;
             }
 
-            $this->logger->error('A Fluid ViewHelper Exception was captured: ' . $exception->getMessage() . ' (' . $exception->getCode() . ')',
+            $this->logger->error(
+                'A Fluid ViewHelper Exception was captured: ' . $exception->getMessage() . ' (' . $exception->getCode() . ')',
                 ['exception' => $exception]
             );
 
