0591a81a80c24b537d37843b629b39c0abb461db
Alexander Berl <a.berl@outlook.com>
2020-08-13 19:58:01 +0200

    TASK: Apply suggested settings name extensionsBlockedFromUpload

--- Neos.Flow/Classes/ResourceManagement/ResourceManager.php
+++ Neos.Flow/Classes/ResourceManagement/ResourceManager.php
@@ -625,7 +625,7 @@ class ResourceManager
             throw new Exception('The given upload file "' . strip_tags($pathInfo['basename']) . '" was not uploaded through PHP. As it could pose a security risk it cannot be imported.', 1422461503);
         }
 
-        if (isset($pathInfo['extension']) && array_key_exists(strtolower($pathInfo['extension']), $this->settings['uploadExtensionsExcluded']) && $this->settings['uploadExtensionsExcluded'][strtolower($pathInfo['extension'])] === true) {
+        if (isset($pathInfo['extension']) && array_key_exists(strtolower($pathInfo['extension']), $this->settings['extensionsBlockedFromUpload']) && $this->settings['extensionsBlockedFromUpload'][strtolower($pathInfo['extension'])] === true) {
             throw new Exception('The extension of the given upload file "' . strip_tags($pathInfo['basename']) . '" is excluded. As it could pose a security risk it cannot be imported.', 1447148472);
         }
 
