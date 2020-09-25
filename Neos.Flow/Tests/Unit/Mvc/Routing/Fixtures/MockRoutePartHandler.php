bca5674e34f0c4c51ce40df06f78ce1db543e112
bwaidelich <b.waidelich@wwwision.de>
2020-09-25 15:03:44 +0200

    FEATURE: Allow RoutePart handlers to point to external URIs with query string
    With this change, RoutePart handlers can define all relevant URI features
    including query string and fragment in order to point to external URIs:
    
    ```php
    class SomeRoutePartHandler extends DynamicRoutePart {
    
        protected function resolveValue($value) {
            return new ResolveResult('', UriConstraints::fromUri(new Uri('https://neos.io:8080/some/path?some[query]=string#some-fragment')));
        }
    }
    ```
    
    Background:
    
    This is a preparation to fully support cross-domain routing. Also, for Neos, this will be
    required in order to deal with shortcut nodes pointing to external URLs within the routing context
    
    Related: #1126
    Fixes: #2140

--- Neos.Flow/Tests/Unit/Mvc/Routing/Fixtures/MockRoutePartHandler.php
+++ Neos.Flow/Tests/Unit/Mvc/Routing/Fixtures/MockRoutePartHandler.php
@@ -18,15 +18,42 @@ use Neos\Flow\Mvc\Routing\DynamicRoutePart;
  */
 class MockRoutePartHandler extends DynamicRoutePart
 {
+
+    /**
+     * @var \Closure|null
+     */
+    private $matchClosure;
+
+    /**
+     * @var \Closure|null
+     */
+    private $resolveClosure;
+
     protected function matchValue($value)
     {
+        if ($this->matchClosure !== null) {
+            return call_user_func($this->matchClosure, $value);
+        }
         $this->value = '_match_invoked_';
         return true;
     }
 
     protected function resolveValue($value)
     {
+        if ($this->resolveClosure !== null) {
+            return call_user_func($this->resolveClosure, $value);
+        }
         $this->value = '_resolve_invoked_';
         return true;
+    }
+
+    public function setMatchClosure(?\Closure $matchClosure): void
+    {
+        $this->matchClosure = $matchClosure;
+    }
+
+    public function setResolveClosure(?\Closure $resolveClosure): void
+    {
+        $this->resolveClosure = $resolveClosure;
     }
 }
