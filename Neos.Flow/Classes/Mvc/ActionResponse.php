7475e5bcb25176e941afcfd54b0050dfed1dba29
Alexander Berl <alexander.berl@xmv.de>
2020-11-03 22:18:11 +0100

    FEATURE: Move SetHeader, ReplaceHttpResponse, PrepareMvcRequest and DispatchComponent to a single middleware

--- Neos.Flow/Classes/Mvc/ActionResponse.php
+++ Neos.Flow/Classes/Mvc/ActionResponse.php
@@ -2,10 +2,10 @@
 namespace Neos\Flow\Mvc;
 
 use Neos\Flow\Http\Cookie;
+use Neos\Flow\Http\Headers;
 use Psr\Http\Message\ResponseInterface;
 use function GuzzleHttp\Psr7\stream_for;
 use Neos\Flow\Annotations as Flow;
-use Neos\Flow\Http\Component\ComponentContext;
 use Psr\Http\Message\StreamInterface;
 use Psr\Http\Message\UriInterface;
 use GuzzleHttp\Psr7\Stream;
@@ -13,8 +13,7 @@ use GuzzleHttp\Psr7\Response;
 
 /**
  * The minimal MVC response object.
- * It allows for simple interactions with the HTTP response from within MVC actions. More specific requirements can be implemented via HTTP Components.
- * @see setComponentParameter()
+ * It allows for simple interactions with the HTTP response from within MVC actions. More specific requirements can be implemented via HTTP middlewares.
  *
  * @Flow\Proxy(false)
  * @api
@@ -25,11 +24,6 @@ final class ActionResponse
      * @var Stream
      */
     protected $content;
-
-    /**
-     * @var array
-     */
-    protected $componentParameters = [];
 
     /**
      * @var UriInterface
@@ -61,9 +55,20 @@ final class ActionResponse
      */
     protected $httpResponse;
 
+    /**
+     * @var Headers
+     */
+    protected $headers;
+
+    /**
+     * @var Headers
+     */
+    protected $headers;
+
     public function __construct()
     {
         $this->content = stream_for();
+        $this->headers = new Headers();
     }
 
     /**
@@ -129,6 +134,7 @@ final class ActionResponse
     public function setCookie(Cookie $cookie): void
     {
         $this->cookies[$cookie->getName()] = clone $cookie;
+        $this->headers->setCookie($cookie);
     }
 
     /**
@@ -143,6 +149,7 @@ final class ActionResponse
         $cookie = new Cookie($cookieName);
         $cookie->expire();
         $this->cookies[$cookie->getName()] = $cookie;
+        $this->headers->deleteCookie($cookieName);
     }
 
     /**
@@ -153,14 +160,56 @@ final class ActionResponse
      * @param string $parameterName
      * @param mixed $value
      * @return void
-     * @api
+     * @deprecated since Flow 7.0 use setHttpHeader or replaceHttpResponse instead. See also #linkToDocs
      */
     public function setComponentParameter(string $componentClassName, string $parameterName, $value): void
     {
-        if (!isset($this->componentParameters[$componentClassName])) {
-            $this->componentParameters[$componentClassName] = [];
+        if ($componentClassName === SetHeaderComponent::class) {
+            $this->setHttpHeader($parameterName, $value);
+            return;
         }
-        $this->componentParameters[$componentClassName][$parameterName] = $value;
+
+        if ($componentClassName === ReplaceHttpResponseComponent::class && $parameterName === 'response') {
+            $this->replaceHttpResponse($value);
+            return;
+        }
+        throw new \InvalidArgumentException('todo');
+    }
+
+    /**
+     * Set the specified header in the response, overwriting any previous value set for this header.
+     *
+     * @param string $headerName The name of the header to set
+     * @param array|string|\DateTime $headerValue An array of values or a single value for the specified header field
+     * @return void
+     */
+    public function setHttpHeader($headerName, $headerValue)
+    {
+        $this->headers->set($headerName, $headerValue);
+    }
+
+    /**
+     * Add the specified header to the response, without overwriting any previous value set for this header.
+     *
+     * @param string $headerName The name of the header to set
+     * @param array|string|\DateTime $headerValue An array of values or a single value for the specified header field
+     * @return void
+     */
+    public function addHttpHeader($headerName, $headerValue)
+    {
+        $this->headers->set($headerName, $headerValue, false);
+    }
+
+    /**
+     * Return the specified HTTP header that was previously set.
+     * Dates are returned as DateTime objects with the timezone set to GMT.
+     *
+     * @param string $headerName The name of the header to get the value(s) for
+     * @return array|string|\DateTime An array of field values if multiple headers of that name exist, a string value if only one value exists and NULL if there is no such header.
+     */
+    public function getHttpHeader($headerName)
+    {
+        return $this->headers->get($headerName);
     }
 
     /**
@@ -171,14 +220,6 @@ final class ActionResponse
         $content = $this->content->getContents();
         $this->content->rewind();
         return $content;
-    }
-
-    /**
-     * @return array
-     */
-    public function getComponentParameters(): array
-    {
-        return $this->componentParameters;
     }
 
     /**
@@ -202,6 +243,7 @@ final class ActionResponse
      */
     public function getContentType(): string
     {
+        return $this->headers->get('Content-Type');
         return $this->contentType;
     }
 
@@ -239,55 +281,11 @@ final class ActionResponse
         if ($this->httpResponse !== null) {
             $actionResponse->replaceHttpResponse($this->httpResponse);
         }
-
-        foreach ($this->componentParameters as $componentClass => $parameters) {
-            foreach ($parameters as $parameterName => $parameterValue) {
-                $actionResponse->setComponentParameter($componentClass, $parameterName, $parameterValue);
-            }
-        }
         foreach ($this->cookies as $cookie) {
             $actionResponse->setCookie($cookie);
         }
 
         return $actionResponse;
-    }
-
-    /**
-     * @param ComponentContext $componentContext
-     * @return ComponentContext
-     */
-    public function mergeIntoComponentContext(ComponentContext $componentContext): ComponentContext
-    {
-        $httpResponse = $this->httpResponse ?? $componentContext->getHttpResponse();
-        if ($this->statusCode !== null) {
-            $httpResponse = $httpResponse->withStatus($this->statusCode);
-        }
-
-        if ($this->hasContent()) {
-            $httpResponse = $httpResponse->withBody($this->content);
-        }
-
-        if ($this->contentType) {
-            $httpResponse = $httpResponse->withHeader('Content-Type', $this->contentType);
-        }
-
-        if ($this->redirectUri) {
-            $httpResponse = $httpResponse->withHeader('Location', (string)$this->redirectUri);
-        }
-
-        foreach ($this->componentParameters as $componentClassName => $componentParameterGroup) {
-            foreach ($componentParameterGroup as $parameterName => $parameterValue) {
-                $componentContext->setParameter($componentClassName, $parameterName, $parameterValue);
-            }
-        }
-
-        foreach ($this->cookies as $cookie) {
-            $httpResponse = $httpResponse->withAddedHeader('Set-Cookie', (string)$cookie);
-        }
-
-        $componentContext->replaceHttpResponse($httpResponse);
-
-        return $componentContext;
     }
 
     /**
@@ -319,6 +317,9 @@ final class ActionResponse
             $httpResponse = $httpResponse->withHeader('Location', (string)$this->redirectUri);
         }
 
+        foreach ($this->headers as $headerName => $headerValue) {
+            $httpResponse = $httpResponse->withAddedHeader($headerName, implode(', ', $headerValue));
+        }
         foreach ($this->cookies as $cookie) {
             $httpResponse = $httpResponse->withAddedHeader('Set-Cookie', (string)$cookie);
         }
@@ -334,5 +335,166 @@ final class ActionResponse
     private function hasContent(): bool
     {
         return $this->content->getSize() > 0;
+    }
+
+    /**
+     * Sets the respective directive in the Cache-Control header.
+     *
+     * A response flagged as "public" may be cached by any cache, even if it normally
+     * wouldn't be cacheable in a shared cache.
+     */
+    public function setPublic()
+    {
+        $this->headers->setCacheControlDirective('public');
+    }
+
+    /**
+     * Sets the respective directive in the Cache-Control header.
+     *
+     * A response flagged as "private" tells that it is intended for a specific
+     * user and must not be cached by a shared cache.
+     */
+    public function setPrivate()
+    {
+        $this->headers->setCacheControlDirective('private');
+    }
+
+    /**
+     * Sets the Date header.
+     *
+     * The given date must either be an RFC2822 parseable date string or a DateTime
+     * object. The timezone will be converted to GMT internally, but the point in
+     * time remains the same.
+     *
+     * @param string|\DateTime $date
+     */
+    public function setDate($date)
+    {
+        $this->headers->set('Date', $date);
+    }
+
+    /**
+     * Returns the date from the Date header.
+     *
+     * The returned date is configured to be in the GMT timezone.
+     *
+     * @return \DateTime|null The date of this response
+     */
+    public function getDate()
+    {
+        return $this->headers->get('Date');
+    }
+
+    /**
+     * Sets the Last-Modified header.
+     *
+     * The given date must either be an RFC2822 parseable date string or a DateTime
+     * object. The timezone will be converted to GMT internally, but the point in
+     * time remains the same.
+     *
+     * @param string|\DateTime $date
+     */
+    public function setLastModified($date)
+    {
+        $this->headers->set('Last-Modified', $date);
+    }
+
+    /**
+     * Returns the date from the Last-Modified header or NULL if no such header
+     * is present.
+     *
+     * The returned date is configured to be in the GMT timezone.
+     *
+     * @return \DateTime|null The last modification date or NULL
+     */
+    public function getLastModified()
+    {
+        return $this->headers->get('Last-Modified');
+    }
+
+    /**
+     * Sets the Expires header.
+     *
+     * The given date must either be an RFC2822 parseable date string or a DateTime
+     * object. The timezone will be converted to GMT internally, but the point in
+     * time remains the same.
+     *
+     * In order to signal that the response has already expired, the date should
+     * be set to the same date as the Date header (that is, $now). To communicate
+     * an infinite expiration time, the date should be set to one year in the future.
+     *
+     * Expiration times should not be more than one year in the future, according
+     * to RFC 2616 / 14.21
+     *
+     * @param string|\DateTime $date
+     */
+    public function setExpires($date)
+    {
+        $this->headers->set('Expires', $date);
+    }
+
+    /**
+     * Returns the date from the Expires header or NULL if no such header
+     * is present.
+     *
+     * The returned date is configured to be in the GMT timezone.
+     *
+     * @return \DateTime|null The expiration date or NULL
+     */
+    public function getExpires()
+    {
+        return $this->headers->get('Expires');
+    }
+
+    /**
+     * Sets the maximum age in seconds before this response becomes stale.
+     *
+     * This method sets the "max-age" directive in the Cache-Control header.
+     *
+     * @param integer $age The maximum age in seconds
+     */
+    public function setMaximumAge($age)
+    {
+        $this->headers->setCacheControlDirective('max-age', $age);
+    }
+
+    /**
+     * Returns the maximum age in seconds before this response becomes stale.
+     *
+     * This method returns the value from the "max-age" directive in the
+     * Cache-Control header.
+     *
+     * @return integer The maximum age in seconds, or NULL if none has been defined
+     */
+    public function getMaximumAge()
+    {
+        return $this->headers->getCacheControlDirective('max-age');
+    }
+
+    /**
+     * Sets the maximum age in seconds before this response becomes stale in shared
+     * caches, such as proxies.
+     *
+     * This method sets the "s-maxage" directive in the Cache-Control header.
+     *
+     * @param integer $maximumAge The maximum age in seconds
+     */
+    public function setSharedMaximumAge($maximumAge)
+    {
+        $this->headers->setCacheControlDirective('s-maxage', $maximumAge);
+    }
+
+    /**
+     * Returns the maximum age in seconds before this response becomes stale in shared
+     * caches, such as proxies.
+     *
+     * This method returns the value from the "s-maxage" directive in the
+     * Cache-Control header.
+     *
+     * @return integer The maximum age in seconds, or NULL if none has been defined
+     */
+    public function getSharedMaximumAge()
+    {
+        return $this->headers->getCacheControlDirective('s-maxage');
     }
 }
