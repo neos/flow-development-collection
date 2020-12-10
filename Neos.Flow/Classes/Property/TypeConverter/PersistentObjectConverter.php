2cb71b94daf41cedf123c865c2f3c6cce7293808
Alexander Berl <a.berl@outlook.com>
2020-12-10 15:52:30 +0100

    TASK: Add link for 7.0 release notes to exception
    Co-authored-by: Bastian Waidelich <b.waidelich@wwwision.de>

--- Neos.Flow/Classes/Property/TypeConverter/PersistentObjectConverter.php
+++ Neos.Flow/Classes/Property/TypeConverter/PersistentObjectConverter.php
@@ -222,7 +222,7 @@ class PersistentObjectConverter extends ObjectConverter
                 throw new InvalidTargetException($exceptionMessage, 1297935345);
             }
             if ($isCollectionType && $originalValue !== ObjectAccess::getProperty($object, $propertyName, true)) {
-                throw new TypeConverterException(sprintf('You overwrote the collection property "%s" of type "%s". This will lead to data loss. Read more about it and how to prevent this here: ADDLINKHERE', $propertyName, $targetType), 1602324196);
+                throw new TypeConverterException(sprintf('You overwrote the collection property "%s" of type "%s". This will lead to data loss. Read more about it and how to prevent this here: https://www.neos.io/go/flow-7-0-release-notes.html', $propertyName, $targetType), 1602324196);
             }
         }
 
