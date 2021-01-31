c9e099bd68594aba66a7950e5db0e9dcfeb4b7ab
Michael Gerdemann <michael@gerdemann.me>
2021-01-31 12:39:43 +0100

    BUGFIX: Fix ResourceTypeConverter for "data" import
    Fix the ResourceTypeConverter so that you
    can also import resources via base64.

--- Neos.Flow/Classes/ResourceManagement/ResourceTypeConverter.php
+++ Neos.Flow/Classes/ResourceManagement/ResourceTypeConverter.php
@@ -212,7 +212,7 @@ class ResourceTypeConverter extends AbstractTypeConverter
     protected function handleHashAndData(array $source, PropertyMappingConfigurationInterface $configuration = null)
     {
         $hash = null;
-        $resource = false;
+        $resource = null;
         $givenResourceIdentity = null;
         if (isset($source['__identity'])) {
             $givenResourceIdentity = $source['__identity'];
@@ -236,7 +236,7 @@ class ResourceTypeConverter extends AbstractTypeConverter
         }
         if ($resource === null) {
             $collectionName = isset($source['collectionName']) ? $source['collectionName'] : $this->getCollectionName($source, $configuration);
-            if (isset($source['data'])) {
+            if (isset($source['data']) && isset($source['filename'])) {
                 $resource = $this->resourceManager->importResourceFromContent(base64_decode($source['data']), $source['filename'], $collectionName, $givenResourceIdentity);
             } elseif ($hash !== null) {
                 /** @var PersistentResource $resource */
