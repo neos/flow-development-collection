7a31ac0c47c94a68427701504138812f6b42cbe0
Karsten Dambekalns <karsten@dambekalns.de>
2021-02-18 10:29:47 +0100

    BUGFIX: ObjectAccess with direct access on private properties of proxied classes
    With this classes with private properties do react as expected to
    ObjectAccess::getProperty() and ObjectAccess::setProperty() with
    $forceDirectAccess enabled, even when they have been subclassed by the
    proxy building of Flow.
    
    Fixes #2388

--- Neos.Utility.ObjectHandling/Classes/ObjectAccess.php
+++ Neos.Utility.ObjectHandling/Classes/ObjectAccess.php
@@ -11,6 +11,7 @@ namespace Neos\Utility;
  * source code.
  */
 
+use Neos\Flow\ObjectManagement\Proxy\ProxyInterface;
 use Neos\Utility\Exception\PropertyNotAccessibleException;
 
 /**
@@ -116,6 +117,11 @@ abstract class ObjectAccess
         if ($forceDirectAccess === true) {
             if (property_exists($className, $propertyName)) {
                 $propertyReflection = new \ReflectionProperty($className, $propertyName);
+                $propertyReflection->setAccessible(true);
+                return $propertyReflection->getValue($subject);
+            }
+            if ($subject instanceof ProxyInterface && property_exists(get_parent_class($className), $propertyName)) {
+                $propertyReflection = new \ReflectionProperty(get_parent_class($className), $propertyName);
                 $propertyReflection->setAccessible(true);
                 return $propertyReflection->getValue($subject);
             }
@@ -250,6 +256,10 @@ abstract class ObjectAccess
             $className = TypeHandling::getTypeForValue($subject);
             if (property_exists($className, $propertyName)) {
                 $propertyReflection = new \ReflectionProperty($className, $propertyName);
+                $propertyReflection->setAccessible(true);
+                $propertyReflection->setValue($subject, $propertyValue);
+            } elseif ($subject instanceof ProxyInterface && property_exists(get_parent_class($className), $propertyName)) {
+                $propertyReflection = new \ReflectionProperty(get_parent_class($className), $propertyName);
                 $propertyReflection->setAccessible(true);
                 $propertyReflection->setValue($subject, $propertyValue);
             } else {
