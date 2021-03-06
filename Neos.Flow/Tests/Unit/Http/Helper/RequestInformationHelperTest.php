afa3f61418475c279a8ed18dcdb65aa2230040cb
Alexander Berl <a.berl@outlook.com>
2021-03-06 11:28:23 +0100

    TASK: Add test covering undisclosed credentials

---
+++ Neos.Flow/Tests/Unit/Http/Helper/RequestInformationHelperTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Neos\Flow\Tests\Unit\Http\Helper;
+
+use Neos\Flow\Http\Helper\ArgumentsHelper;
+use Neos\Flow\Http\Helper\RequestInformationHelper;
+use Neos\Flow\Http\Request;
+use Neos\Flow\Tests\UnitTestCase;
+
+/**
+ * Tests for the RequestInformationHelper
+ */
+class RequestInformationHelperTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function renderRequestHeadersWillNotDiscloseAuthorizationCredentials()
+    {
+        $request = Request::createFromEnvironment()
+            ->withAddedHeader('Authorization', 'Basic SomeUser:SomePassword')
+            ->withAddedHeader('Authorization', 'Bearer SomeToken');
+
+        $renderedHeaders = RequestInformationHelper::renderRequestHeaders($request);
+        self::assertNotContains('SomePassword', $renderedHeaders);
+        self::assertNotContains('SomeToken', $renderedHeaders);
+    }
+}
