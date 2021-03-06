69901343ae3b9d424d030dce920245c367683dac
Alexander Berl <a.berl@outlook.com>
2021-03-07 00:46:34 +0100

    TASK: Add hasContentType() to ActionResponse

--- Neos.Flow/Classes/Mvc/ActionResponse.php
+++ Neos.Flow/Classes/Mvc/ActionResponse.php
@@ -191,6 +191,11 @@ final class ActionResponse
         return $this->statusCode ?? 200;
     }
 
+    public function hasContentType(): bool
+    {
+        return !empty($this->contentType);
+    }
+
     /**
      * @return string
      */
@@ -209,7 +214,7 @@ final class ActionResponse
             $actionResponse->setContent($this->content);
         }
 
-        if ($this->contentType !== null) {
+        if ($this->hasContentType()) {
             $actionResponse->setContentType($this->contentType);
         }
 
@@ -248,7 +253,7 @@ final class ActionResponse
             $httpResponse = $httpResponse->withBody($this->content);
         }
 
-        if ($this->contentType) {
+        if ($this->hasContentType()) {
             $httpResponse = $httpResponse->withHeader('Content-Type', $this->contentType);
         }
 
@@ -293,7 +298,7 @@ final class ActionResponse
             $httpResponse = $httpResponse->withBody($this->content);
         }
 
-        if ($this->contentType !== null) {
+        if ($this->hasContentType()) {
             $httpResponse = $httpResponse->withHeader('Content-Type', $this->contentType);
         }
 
