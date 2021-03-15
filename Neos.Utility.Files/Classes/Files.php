b61942bc4b7f8116d1041e8d5fb1d79a4826a365
Alexander Berl <a.berl@outlook.com>
2021-03-15 19:32:27 +0100

    TASK: Replace missing double-shrug catching comment

--- Neos.Utility.Files/Classes/Files.php
+++ Neos.Utility.Files/Classes/Files.php
@@ -193,7 +193,7 @@ abstract class Files
                 try {
                     @unlink($path . '/.DS_Store');
                 } catch (\Throwable $e) {
-                    // Double-shrug catching
+                    // PHP 8 apparently throws for unlink even with shutup operator, but we really don't care at this place. It's also the only way to handle this race-condition free.
                 }
             }
             if (@rmdir($path) === false) {
