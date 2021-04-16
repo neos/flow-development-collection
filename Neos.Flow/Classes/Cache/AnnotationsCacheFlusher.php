0756b13b753858e7245c85dd9912511fcbc08e53
Alexander Berl <alexander.berl@xmv.de>
2021-04-16 11:49:16 +0200

    TASK: Fix missing annotation namespace import

--- Neos.Flow/Classes/Cache/AnnotationsCacheFlusher.php
+++ Neos.Flow/Classes/Cache/AnnotationsCacheFlusher.php
@@ -1,8 +1,19 @@
 <?php
 declare(strict_types=1);
 
+/*
+ * This file is part of the Neos.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
 namespace Neos\Flow\Cache;
 
+use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Configuration\ConfigurationManager;
 use Neos\Flow\Reflection\ReflectionService;
 use Neos\Flow\Log\Utility\LogEnvironment;
@@ -71,5 +82,4 @@ final class AnnotationsCacheFlusher
             $this->configurationManager->refreshConfiguration();
         }
     }
-
 }
