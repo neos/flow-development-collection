f40b2947277cc2cff296a6e098aea57ad166ad26
Alexander Berl <a.berl@outlook.com>
2021-04-18 14:33:27 +0200

    TASK: Make SampleAttribute work with PHP 7.3

--- Neos.Flow/Tests/Functional/ObjectManagement/Fixtures/SampleAttribute.php
+++ Neos.Flow/Tests/Functional/ObjectManagement/Fixtures/SampleAttribute.php
@@ -4,9 +4,9 @@ namespace Neos\Flow\Tests\Functional\ObjectManagement\Fixtures;
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
 class SampleAttribute
 {
-    public string $class;
-    public array $options;
-    public string $argWithDefault;
+    public $class;
+    public $options;
+    public $argWithDefault;
     public function __construct(string $class, array $options = [], string $argWithDefault = 'default')
     {
         $this->class = $class;
