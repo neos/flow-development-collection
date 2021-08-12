add3b78597c5a8a090205c4ac8f4352ea9d00d43
Ferdinand Kuhl <f.kuhl@digital-competence.de>
2021-08-12 11:15:33 +0200

    BUGFIX: Do not run before and afterControllerInvocation signals in compile time
    Together with Flow 6.x the Cli commands have been seperated from the Http ActionControllers.
    
    Before these slots have been guaranteed to be executed during runtime only. This patch restores
    that behavior. There will be another commit against master, which introduces new 4 new signals,
    so in future you can even use compile time slots.
    
    Fixes: #2582

--- Neos.Flow/Classes/Cli/Dispatcher.php
+++ Neos.Flow/Classes/Cli/Dispatcher.php
@@ -5,6 +5,7 @@ use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Cli\Exception\InfiniteLoopException;
 use Neos\Flow\Cli\Exception\InvalidCommandControllerException;
 use Neos\Flow\Cli\Exception\StopCommandException;
+use Neos\Flow\ObjectManagement\CompileTimeObjectManager;
 use Neos\Flow\ObjectManagement\ObjectManagerInterface;
 
 /**
@@ -80,6 +81,11 @@ class Dispatcher
      */
     protected function emitBeforeControllerInvocation(Request $request, Response $response, CommandControllerInterface $controller)
     {
+        // Before Flow 5.3 you could rely on this slot only being called during runtime
+        // There will be new slots in 7.x, which will run in compile time, too
+        if ($this->objectManager instanceof CompileTimeObjectManager) {
+            return;
+        }
         $this->signalDispatcher->dispatch(\Neos\Flow\Mvc\Dispatcher::class, 'beforeControllerInvocation', [
             'request' => $request,
             'response' => $response,
@@ -98,6 +104,11 @@ class Dispatcher
      */
     protected function emitAfterControllerInvocation(Request $request, Response $response, CommandControllerInterface $controller)
     {
+        // Before Flow 5.3 you could rely on this slot only being called during runtime
+        // There will be new slots in 7.x, which will run in compile time, too
+        if ($this->objectManager instanceof CompileTimeObjectManager) {
+            return;
+        }
         $this->signalDispatcher->dispatch(\Neos\Flow\Mvc\Dispatcher::class, 'afterControllerInvocation', [
             'request' => $request,
             'response' => $response,
