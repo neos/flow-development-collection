536fdaaa5cfc3c6b6bd7b5b13d8bcb9ef6578f91
Robert Lemke <robert@flownative.com>
2022-02-22 08:46:09 +0100

    Provide functional test for PHP 8.1 enum handling

---
+++ Neos.Flow/Tests/Functional/ObjectManagement/Fixtures/PHP8/BackedEnumWithMethod.php
@@ -0,0 +1,38 @@
+<?php
+namespace Neos\Flow\Tests\Functional\ObjectManagement\Fixtures\PHP8;
+
+/*
+ * This file is part of the Neos.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Annotations\Proxy;
+
+/**
+ * A PHP 8.1 value-backed enum with methods
+ */
+enum BackedEnumWithMethod: string
+{
+    case ESPRESSO = 'esp';
+    case RISTRETTO = 'ris';
+    case FLAT_WHITE = 'flw';
+
+    public function label(): string
+    {
+        return BackedEnumWithMethod::getLabel($this);
+    }
+
+    public static function getLabel(self $value): string
+    {
+        return match ($value) {
+            self::ESPRESSO => 'Espresso',
+            self::RISTRETTO => 'Ristretto',
+            self::FLAT_WHITE => 'Flat White'
+        };
+    }
+}
