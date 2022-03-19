7a8285bfd6b6b2bae981324d6d26408fa11e7c5e
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-03-19 10:16:52 +0100

    TASK: Add legacy fluid error view options migration
    warn if if Neos.Flow.error.exceptionHandler.defaultRenderingOptions was used with legacy fluid view options
    moving these options globally to the 'viewOptions' subkey would lead to that every view (not only fluid) will get them applied and not supported options view errors will occur.

--- Neos.Flow/Migrations/Code/Version20220318174300.php
+++ Neos.Flow/Migrations/Code/Version20220318174300.php
@@ -48,9 +48,21 @@ class Version20220318174300 extends AbstractMigration
 
     /**
      * Adjust legacy fluid view options: Neos.Flow.error.exceptionHandler.renderingGroups.{exampleGroup}.options
+     * show warning if Neos.Flow.error.exceptionHandler.defaultRenderingOptions was used with legacy fluid view options
      */
     public function processErrorViewSettings(array &$configuration): void
     {
+        if (self::arrayValueByPathIsArray($configuration, 'Neos.Flow.error.exceptionHandler.defaultRenderingOptions')) {
+            $defaultRenderingOptions = $configuration['Neos']['Flow']['error']['exceptionHandler']['defaultRenderingOptions'];
+            if (isset($defaultRenderingOptions['templatePathAndFilename'])
+                || isset($defaultRenderingOptions['layoutRootPath'])
+                || isset($defaultRenderingOptions['partialRootPath'])
+                || isset($defaultRenderingOptions['format'])) {
+                // moving these options globally to the 'viewOptions' subkey would lead to that every view (not only fluid) will get them applied and not supported options view errors will occur.
+                $this->showWarning("No automatic migration can be done for the global legacy fluid error view options:\nNeos.Flow.error.exceptionHandler.defaultRenderingOptions");
+            }
+        }
+
         if (self::arrayValueByPathIsArray($configuration, 'Neos.Flow.error.exceptionHandler.renderingGroups') === false) {
             return;
         }
@@ -76,8 +88,15 @@ class Version20220318174300 extends AbstractMigration
             if (isset($options['templatePathAndFilename'])) {
                 $value = $options['templatePathAndFilename'];
                 unset($options['templatePathAndFilename']);
-                $viewOptions['templatePathAndFilename'] = $value;
-                $this->showNote("Moved 'templatePathAndFilename' to 'viewOptions.templatePathAndFilename' subkey:\n $renderingGroupPath.options.templatePathAndFilename = $value");
+                if (is_string($value) && $value !== '') {
+                    $viewOptions['templatePathAndFilename'] = $value;
+                    $this->showNote("Moved 'templatePathAndFilename' to 'viewOptions.templatePathAndFilename' subkey:\n $renderingGroupPath.options.templatePathAndFilename = $value");
+                } else {
+                    // templatePathAndFilename: true
+                    // was a workaround:
+                    // https://github.com/neos/flow-development-collection/issues/1108
+                    $this->showNote("Old 'templatePathAndFilename' workaround removed:\n  $renderingGroupPath.options.templatePathAndFilename = $value");
+                }
             }
 
             //
