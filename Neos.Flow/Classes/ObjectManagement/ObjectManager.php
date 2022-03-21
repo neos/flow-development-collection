d31c08e9a805460a8faee0e026cbf572e39ffcb4
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-03-21 13:53:06 +0100

    TASK: use Psaml Template instead of PHPSTORM_META
    \Neos\Flow\ObjectManagement\ObjectManagerInterface::get
    \Neos\Flow\Core\Bootstrap::getEarlyInstance
    
    make use of the @template tag:
     https://psalm.dev/docs/annotating_code/templated_annotations/

--- Neos.Flow/Classes/ObjectManagement/ObjectManager.php
+++ Neos.Flow/Classes/ObjectManagement/ObjectManager.php
@@ -179,9 +179,10 @@ class ObjectManager implements ObjectManagerInterface
     /**
      * Returns a fresh or existing instance of the object specified by $objectName.
      *
-     * @param string $objectName The name of the object to return an instance of
+     * @template T
+     * @param class-string<T> $objectName The name of the object to return an instance of
      * @param mixed ...$constructorArguments Any number of arguments that should be passed to the constructor of the object
-     * @return object The object instance
+     * @return T The object instance
      * @throws Exception\CannotBuildObjectException
      * @throws Exception\UnknownObjectException if an object with the given name does not exist
      * @throws \InvalidArgumentException
