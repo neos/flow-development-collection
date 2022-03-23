cf27094cc7477c9d050d44f0c8cda043f26d7abf
Marc Henry Schultz <85400359+mhsdesign@users.noreply.github.com>
2022-03-23 23:07:22 +0100

    BUGFIX: Array.push accept `null` for array type casting
    https://github.com/neos/neos-development-collection/pull/3658

--- Neos.Eel/Tests/Unit/Helper/ArrayHelperTest.php
+++ Neos.Eel/Tests/Unit/Helper/ArrayHelperTest.php
@@ -426,6 +426,7 @@ class ArrayHelperTest extends \Neos\Flow\Tests\UnitTestCase
             # expect cast scalar (as arg $array) to array
             'string' => ['a', 'b', 'c', ['a', 'b', 'c']],
             'int' => [123, 'b', 'c', [123, 'b', 'c']],
+            'null' => [null, 'b', 'c', [null, 'b', 'c']],
         ];
     }
 
