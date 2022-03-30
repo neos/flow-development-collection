0d91a793d248ef58734064cddf518afb77b270ee
Karsten Dambekalns <karsten@dambekalns.de>
2022-03-30 16:00:07 +0200

    BUGFIX: Use env var for FLOW_LOCKHOLDINGPAGE
    Fixes #2798

--- Neos.Flow/Classes/Core/LockManager.php
+++ Neos.Flow/Classes/Core/LockManager.php
@@ -54,7 +54,8 @@ class LockManager
         $lockPath = $this->getLockPath();
         $this->lockPathAndFilename = $lockPath . md5(FLOW_PATH_ROOT) . '_Flow.lock';
         $this->lockFlagPathAndFilename = $lockPath . md5(FLOW_PATH_ROOT) . '_FlowIsLocked';
-        $this->lockHoldingPage = defined('FLOW_LOCKHOLDINGPAGE') ? FLOW_PATH_PACKAGES . FLOW_LOCKHOLDINGPAGE : FLOW_PATH_FLOW . 'Resources/Private/Core/LockHoldingStackPage.html';
+        $configuredLockHoldingPage = Bootstrap::getEnvironmentConfigurationSetting('FLOW_LOCKHOLDINGPAGE');
+        $this->lockHoldingPage = $configuredLockHoldingPage ? FLOW_PATH_PACKAGES . $configuredLockHoldingPage : FLOW_PATH_FLOW . 'Resources/Private/Core/LockHoldingStackPage.html';
         $this->removeExpiredLock();
     }
 
