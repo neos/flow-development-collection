4b63f84806ea140e27cc8f9f7b2969f71a9b53f4
Martin Ficzel <ficzel@sitegeist.de>
2022-07-01 15:18:27 +0200

    WIP: Feature introduce NotFound-Middleware
    to render 404 responses instead of throwing exceptions. This allows to intercept and modify the results via the http chain

--- Neos.Flow/Classes/Mvc/DispatchMiddleware.php
+++ Neos.Flow/Classes/Mvc/DispatchMiddleware.php
@@ -14,7 +14,9 @@ namespace Neos\Flow\Mvc;
  */
 
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Http\Middleware\NotFoundMiddleware;
 use Neos\Flow\Http\ServerRequestAttributes;
+use Neos\Flow\Mvc\Controller\Exception\InvalidControllerException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -25,6 +27,12 @@ use Psr\Http\Server\RequestHandlerInterface;
  */
 class DispatchMiddleware implements MiddlewareInterface
 {
+    public const ABSTAIN_STATUS_HEADER = 'X-Flow-Abstain-StatusCode';
+
+    public const ABSTAIN_TEXT = 'X-Flow-Abstain-StatusCode';
+
+    public const ABSTAIN_DETAILS = 'X-Flow-Abstain-StatusCode';
+
     /**
      * @Flow\Inject(lazy=false)
      * @var Dispatcher
@@ -36,13 +44,37 @@ class DispatchMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $next): ResponseInterface
     {
+        /** @var ActionRequest $actionRequest */
         $actionRequest = $request->getAttribute(ServerRequestAttributes::ACTION_REQUEST);
         if ($actionRequest === null) {
-            throw new Exception('No ActionRequest was created before the DispatchMiddleware. Make sure you have the SecurityEntryPointMiddleware configured before dispatch.', 1605091292);
+             return $next->handle(
+                $request
+                    ->withAttribute(NotFoundMiddleware::DETAILS, 'No ActionRequest was created before the DispatchMiddleware. Make sure you have the SecurityEntryPointMiddleware configured before dispatch.')
+                    ->withAttribute(NotFoundMiddleware::REFERENCE_CODE, 1605091292)
+            );
         }
 
-        $actionResponse = new ActionResponse();
-        $this->dispatcher->dispatch($actionRequest, $actionResponse);
-        return $actionResponse->buildHttpResponse();
+        try {
+            $actionResponse = new ActionResponse();
+            $this->dispatcher->dispatch($actionRequest, $actionResponse);
+            $httpResponse = $actionResponse->buildHttpResponse();
+        } catch (InvalidControllerException $e) {
+            return $next->handle(
+                $request
+                    ->withAttribute(NotFoundMiddleware::DETAILS, $e->getMessage())
+                    ->withAttribute(NotFoundMiddleware::REFERENCE_CODE, $e->getReferenceCode())
+            );
+        }
+
+        if ($httpResponse->hasHeader(self::ABSTAIN_STATUS_HEADER)) {
+            return $next->handle(
+                $request
+                    ->withAttribute(NotFoundMiddleware::STATUS_CODE, $httpResponse->getHeaderLine(self::ABSTAIN_STATUS_HEADER))
+                    ->withAttribute(NotFoundMiddleware::DESCRIPTION, $httpResponse->getHeaderLine(self::ABSTAIN_TEXT))
+                    ->withAttribute(NotFoundMiddleware::DETAILS, $httpResponse->getHeaderLine(self::ABSTAIN_DETAILS))
+            );
+        }
+
+        return $httpResponse;
     }
 }
