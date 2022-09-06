5b575b323e84d55c24bd07f9582be9e5faf269d6
Karsten Dambekalns <karsten@dambekalns.de>
2022-09-06 11:11:51 +0200

    TASK: Use strict typing in MultiBackendTest

--- Neos.Cache/Tests/Unit/Backend/MultiBackendTest.php
+++ Neos.Cache/Tests/Unit/Backend/MultiBackendTest.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace Neos\Cache\Tests\Unit\Backend;
 
 include_once(__DIR__ . '/../../BaseTestCase.php');
@@ -76,7 +78,7 @@ class MultiBackendTest extends BaseTestCase
         $this->inject($multiBackend, 'backends', [$firstNullBackendMock, $secondNullBackendMock]);
         $this->inject($multiBackend, 'initialized', true);
 
-        $multiBackend->set('foo', 1);
+        $multiBackend->set('foo', 'data');
     }
 
     /**
