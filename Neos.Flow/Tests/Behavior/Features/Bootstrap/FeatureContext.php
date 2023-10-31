40a4861f0ad2de8038472e504733ff97bdf0a2e0
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-10-31 17:28:52 +0100

    TASK: Use new behat FlowBootstrapTrait
    see https://github.com/neos/behat/pull/35

--- Neos.Flow/Tests/Behavior/Features/Bootstrap/FeatureContext.php
+++ Neos.Flow/Tests/Behavior/Features/Bootstrap/FeatureContext.php
@@ -1,12 +1,11 @@
 <?php
 use Behat\Behat\Context\Context;
-use Neos\Behat\Tests\Behat\FlowContextTrait;
+use Neos\Behat\FlowBootstrapTrait;
 use Neos\Flow\Tests\Behavior\Features\Bootstrap\IsolatedBehatStepsTrait;
 use Neos\Flow\Tests\Behavior\Features\Bootstrap\SecurityOperationsTrait;
 use Neos\Flow\Tests\Functional\Command\BehatTestHelper;
 use Neos\Flow\Utility\Environment;
 
-require_once(__DIR__ . '/../../../../../../Application/Neos.Behat/Tests/Behat/FlowContextTrait.php');
 require_once(__DIR__ . '/IsolatedBehatStepsTrait.php');
 require_once(__DIR__ . '/SecurityOperationsTrait.php');
 
@@ -18,7 +17,7 @@ require_once(__DIR__ . '/SecurityOperationsTrait.php');
  */
 class FeatureContext implements Context
 {
-    use FlowContextTrait;
+    use FlowBootstrapTrait;
     use IsolatedBehatStepsTrait;
     use SecurityOperationsTrait;
 
@@ -32,14 +31,16 @@ class FeatureContext implements Context
      */
     protected $environment;
 
+    /**
+     * @var \Neos\Flow\ObjectManagement\ObjectManagerInterface
+     */
+    protected $objectManager;
+
     public function __construct()
     {
-        if (self::$bootstrap === null) {
-            self::$bootstrap = $this->initializeFlow();
-        }
-        $this->objectManager = self::$bootstrap->getObjectManager();
-
-        $this->environment = $this->objectManager->get(Environment::class);
+        self::bootstrapFlow();
+        $this->objectManager = $this->getObjectManager();
+        $this->environment = $this->getObject(Environment::class);
         $this->setupSecurity();
     }
 }
