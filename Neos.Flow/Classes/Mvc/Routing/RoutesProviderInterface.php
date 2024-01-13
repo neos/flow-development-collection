3aa3fc11d49565ce73479983c8e0e44b008b1255
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-01-13 15:29:43 +0100

    TASK document RoutesProviderInterface

--- Neos.Flow/Classes/Mvc/Routing/RoutesProviderInterface.php
+++ Neos.Flow/Classes/Mvc/Routing/RoutesProviderInterface.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace Neos\Flow\Mvc\Routing;
 
 /**
+ * Supplier for lazily fetching the routes for the router.
+ *
+ * This layer of abstraction avoids having to parse the routes for every request.
+ * The router will only request the routes if it comes across a route it hasn't seen (i.e. cached) before.
+ *
  * @internal
  */
 interface RoutesProviderInterface
