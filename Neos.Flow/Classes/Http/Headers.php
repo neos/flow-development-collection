8d9bcfcff74fad681356b04a809e736dbd8aedb1
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-01-29 21:33:19 +0100

    TASK: Remove unused `Headers::createFromServer`

--- Neos.Flow/Classes/Http/Headers.php
+++ Neos.Flow/Classes/Http/Headers.php
@@ -51,33 +51,6 @@ class Headers implements \Iterator
     }
 
     /**
-     * Creates a new Headers instance from the given $_SERVER-superglobal-like array.
-     *
-     * @param array $server An array similar or equal to $_SERVER, containing headers in the form of "HTTP_FOO_BAR"
-     * @return Headers
-     */
-    public static function createFromServer(array $server)
-    {
-        $headerFields = [];
-        if (isset($server['PHP_AUTH_USER'], $server['PHP_AUTH_PW'])) {
-            $headerFields['Authorization'] = 'Basic ' . base64_encode($server['PHP_AUTH_USER'] . ':' . $server['PHP_AUTH_PW']);
-        }
-
-        foreach ($server as $name => $value) {
-            if (strpos($name, 'HTTP_') === 0) {
-                $name = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))));
-                $headerFields[$name] = $value;
-            } elseif ($name === 'REDIRECT_REMOTE_AUTHORIZATION' && !isset($headerFields['Authorization'])) {
-                $headerFields['Authorization'] = $value;
-            } elseif (in_array($name, ['CONTENT_TYPE', 'CONTENT_LENGTH'])) {
-                $name = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', $name))));
-                $headerFields[$name] = $value;
-            }
-        }
-        return new self($headerFields);
-    }
-
-    /**
      * Sets the specified HTTP header
      *
      * DateTime objects will be converted to a string representation internally but
