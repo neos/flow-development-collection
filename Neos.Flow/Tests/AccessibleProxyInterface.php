98a5829f9701a1cd489aeaf2ef2e3dda07452810
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-02-05 21:56:59 +0100

    TASK: Deprecate BaseTestCase functionalities

--- Neos.Flow/Tests/AccessibleProxyInterface.php
+++ Neos.Flow/Tests/AccessibleProxyInterface.php
@@ -12,7 +12,8 @@ use Neos\Utility\ObjectAccess;
  *
  * Note that this interface is not actually implemented by the accessible proxy, but only provides IDE support.
  *
- * @deprecated you should not use this for unit testing
+ * @deprecated you should not use this for testing. As it will couple the tests to highly to the internal implementation
+ * and makes refactorings without rewriting major tests impossible.
  */
 interface AccessibleProxyInterface
 {
@@ -20,10 +21,10 @@ interface AccessibleProxyInterface
     public function _call(string $methodName, mixed ...$arguments): mixed;
     /** @deprecated */
     public function _callRef(string $methodName, mixed ...$argumentsPassedByReference): mixed;
-    /** @deprecated please use {@see BaseTestCase::inject()} instead */
+    /** @deprecated please specify properties via constructor call the injector manually or use - if you must - {@see ObjectAccess::setProperty()} instead. */
     public function _set(string $propertyName, mixed $value): void;
     /** @deprecated */
     public function _setRef(string $propertyName, mixed &$value): void;
-    /** @deprecated please use {@see ObjectAccess::getProperty()} instead */
+    /** @deprecated please use - if you must - {@see ObjectAccess::setProperty()} instead. */
     public function _get(string $propertyName): mixed;
 }
