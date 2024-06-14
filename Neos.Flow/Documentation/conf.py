2c452c7aa3c5c39f94eff77af70cbaaec7a39600
Robert Lemke <robert@flownative.com>
2024-06-14 12:53:28 +0200

    TASK: Update Flow version in docs rendering config

--- Neos.Flow/Documentation/conf.py
+++ Neos.Flow/Documentation/conf.py
@@ -27,9 +27,9 @@ copyright = '2006 and onwards by the authors'
 author = 'Neos Team and Contributors'
 
 # The short X.Y version.
-version = '9.0'
+version = '8.4'
 # The full version, including alpha/beta/rc tags.
-release = '9.0.x'
+release = '8.4.x'
 
 # -- General configuration ---------------------------------------------------
 
