dc8fd6eca1f6247ed51a566d16b5791b2f4d915c
Christian Müller <christian@flownative.com>
2024-06-26 09:26:57 +0200

    Adaptions for DBAL update
    The CacheAdapter is no longer needed as the custom interface is deprecated in doctrine
    and we can provide a PSR cache interface instead.

--- Neos.Flow/Classes/Persistence/Doctrine/DataTypes/JsonArrayType.php
+++ Neos.Flow/Classes/Persistence/Doctrine/DataTypes/JsonArrayType.php
@@ -5,7 +5,6 @@ namespace Neos\Flow\Persistence\Doctrine\DataTypes;
 
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Platforms\PostgreSQL94Platform;
 use Doctrine\DBAL\Types\ConversionException;
 use Doctrine\DBAL\Types\JsonType as DoctrineJsonType;
 use Doctrine\ORM\Mapping\Entity as ORMEntity;
@@ -44,11 +43,6 @@ class JsonArrayType extends DoctrineJsonType
      */
     public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
     {
-        if ($platform instanceof PostgreSQL94Platform) {
-            // FIXME: Is this special case still needed as of Doctrine/DBAL 3.3?
-            return 'jsonb';
-        }
-
         return $platform->getJsonTypeDeclarationSQL($column);
     }
 
