a2a0cb5c1557f9a67a8e84b789cf4def3f08ed79
Bernhard Schmitt <schmitt@sitegeist.de>
2025-04-04 23:30:27 +0200

    Adjust Neos.Utility.Arrays

--- Neos.Utility.Arrays/Classes/Arrays.php
+++ Neos.Utility.Arrays/Classes/Arrays.php
@@ -22,9 +22,9 @@ abstract class Arrays
      * Explodes a $string delimited by $delimiter and passes each item in the array through intval().
      * Corresponds to explode(), but with conversion to integers for all values.
      *
-     * @param string $delimiter Delimiter string to explode with
+     * @param non-empty-string $delimiter Delimiter string to explode with
      * @param string $string The string to explode
-     * @return array Exploded values, all converted to integers
+     * @return array<mixed> Exploded values, all converted to integers
      */
     public static function integerExplode(string $delimiter, string $string): array
     {
@@ -41,10 +41,10 @@ abstract class Arrays
      * Explodes a string and trims all values for whitespace in the ends.
      * If $onlyNonEmptyValues is set, then all blank ('') values are removed.
      *
-     * @param string $delimiter Delimiter string to explode with
+     * @param non-empty-string $delimiter Delimiter string to explode with
      * @param string $string The string to explode
      * @param boolean $onlyNonEmptyValues If disabled, even empty values (='') will be set in output
-     * @return array Exploded values
+     * @return array<mixed> Exploded values
      */
     public static function trimExplode(string $delimiter, string $string, bool $onlyNonEmptyValues = true): array
     {
@@ -64,11 +64,11 @@ abstract class Arrays
      * in the first array ($firstArray) with the values of the second array ($secondArray) in case of identical keys,
      * ie. keeping the values of the second.
      *
-     * @param array $firstArray First array
-     * @param array $secondArray Second array, overruling the first array
+     * @param array<mixed> $firstArray First array
+     * @param array<mixed> $secondArray Second array, overruling the first array
      * @param boolean $dontAddNewKeys If set, keys that are NOT found in $firstArray (first array) will not be set. Thus only existing value can/will be overruled from second array.
      * @param boolean $emptyValuesOverride If set (which is the default), values from $secondArray will overrule if they are empty (according to PHP's empty() function)
-     * @return array Resulting array where $secondArray values has overruled $firstArray values
+     * @return array<mixed> Resulting array where $secondArray values has overruled $firstArray values
      */
     public static function arrayMergeRecursiveOverrule(array $firstArray, array $secondArray, bool $dontAddNewKeys = false, bool $emptyValuesOverride = true): array
     {
@@ -109,13 +109,13 @@ abstract class Arrays
      * Merges two arrays recursively and "binary safe" (integer keys are overridden as well), overruling similar values in the first array ($firstArray) with the values of the second array ($secondArray)
      * In case of identical keys, ie. keeping the values of the second. The given $toArray closure will be used if one of the two array keys contains an array and the other not. It should return an array.
      *
-     * @param array $firstArray First array
-     * @param array $secondArray Second array, overruling the first array
+     * @param array<mixed> $firstArray First array
+     * @param array<mixed> $secondArray Second array, overruling the first array
      * @param \Closure $toArray The given callable will get a value that is not an array and has to return an array.
      *                          This is to allow custom merging of simple types with (sub) arrays
      * @param \Closure|null $overrideFirst The given callable will determine whether the value of the first array should be overridden.
      *                                     It should have the following signature $callable($key, ?array $firstValue = null, ?array $secondValue = null): bool
-     * @return array Resulting array where $secondArray values has overruled $firstArray values
+     * @return array<mixed> Resulting array where $secondArray values has overruled $firstArray values
      */
     public static function arrayMergeRecursiveOverruleWithCallback(array $firstArray, array $secondArray, \Closure $toArray, ?\Closure $overrideFirst = null): array
     {
@@ -157,8 +157,7 @@ abstract class Arrays
     /**
      * Returns true if the given array contains elements of varying types
      *
-     * @param array $array
-     * @return boolean
+     * @param array<mixed> $array
      */
     public static function containsMultipleTypes(array $array): bool
     {
@@ -179,12 +178,12 @@ abstract class Arrays
      * Replacement for array_reduce that allows any type for $initial (instead
      * of only integer)
      *
-     * @param array $array the array to reduce
-     * @param string $function the reduce function with the same order of parameters as in the native array_reduce (i.e. accumulator first, then current array element)
+     * @param array<mixed> $array the array to reduce
+     * @param callable $function the reduce function with the same order of parameters as in the native array_reduce (i.e. accumulator first, then current array element)
      * @param mixed $initial the initial accumulator value
      * @return mixed
      */
-    public static function array_reduce(array $array, string $function, $initial = null)
+    public static function array_reduce(array $array, callable $function, $initial = null)
     {
         $accumulator = $initial;
         foreach ($array as $value) {
@@ -196,8 +195,8 @@ abstract class Arrays
     /**
      * Returns the value of a nested array by following the specifed path.
      *
-     * @param array $array The array to traverse
-     * @param array|string $path The path to follow. Either a simple array of keys or a string in the format 'foo.bar.baz'
+     * @param array<mixed> $array The array to traverse
+     * @param array<mixed>|string $path The path to follow. Either a simple array of keys or a string in the format 'foo.bar.baz'
      * @return mixed The value found, NULL if the path didn't exist (note there is no way to distinguish between a found NULL value and "path not found")
      */
     public static function getValueByPath(array $array, array|string $path): mixed
@@ -222,8 +221,8 @@ abstract class Arrays
      * See {@see ValueAccessor}
      *
      * @internal experimental feature, not stable API
-     * @param array $array The array to traverse
-     * @param array|string $path The path to follow. Either a simple array of keys or a string in the format 'foo.bar.baz'
+     * @param array<mixed> $array The array to traverse
+     * @param array<mixed>|string $path The path to follow. Either a simple array of keys or a string in the format 'foo.bar.baz'
      * @return ValueAccessor
      */
     public static function getAccessorByPath(array $array, array|string $path): ValueAccessor
@@ -235,21 +234,16 @@ abstract class Arrays
     /**
      * Sets the given value in a nested array or object by following the specified path.
      *
-     * @param array|\ArrayAccess $subject The array or ArrayAccess instance to work on
-     * @param array|string $path The path to follow. Either a simple array of keys or a string in the format 'foo.bar.baz'
+     * @param array<mixed>|\ArrayAccess<int|string, mixed> $subject The array or ArrayAccess instance to work on
+     * @param array<mixed>|string $path The path to follow. Either a simple array of keys or a string in the format 'foo.bar.baz'
      * @param mixed $value The value to set
-     * @return array|\ArrayAccess The modified array or object
+     * @return array<mixed>|\ArrayAccess<int|string, mixed> The modified array or object
      * @throws \InvalidArgumentException
      */
-    public static function setValueByPath($subject, $path, $value)
+    public static function setValueByPath(array|\ArrayAccess $subject, array|string $path, $value)
     {
-        if (!is_array($subject) && !($subject instanceof \ArrayAccess)) {
-            throw new \InvalidArgumentException('setValueByPath() expects $subject to be array or an object implementing \ArrayAccess, "' . (is_object($subject) ? get_class($subject) : gettype($subject)) . '" given.', 1306424308);
-        }
         if (is_string($path)) {
             $path = explode('.', $path);
-        } elseif (!is_array($path)) {
-            throw new \InvalidArgumentException('setValueByPath() expects $path to be string or array, "' . gettype($path) . '" given.', 1305111499);
         }
         $key = array_shift($path);
         if (count($path) === 0) {
@@ -266,17 +260,15 @@ abstract class Arrays
     /**
      * Unsets an element/part of a nested array by following the specified path.
      *
-     * @param array $array The array
-     * @param array|string $path The path to follow. Either a simple array of keys or a string in the format 'foo.bar.baz'
-     * @return array The modified array
+     * @param array<mixed> $array The array
+     * @param array<mixed>|string $path The path to follow. Either a simple array of keys or a string in the format 'foo.bar.baz'
+     * @return array<mixed> The modified array
      * @throws \InvalidArgumentException
      */
-    public static function unsetValueByPath(array $array, $path): array
+    public static function unsetValueByPath(array $array, array|string $path): array
     {
         if (is_string($path)) {
             $path = explode('.', $path);
-        } elseif (!is_array($path)) {
-            throw new \InvalidArgumentException('unsetValueByPath() expects $path to be string or array, "' . gettype($path) . '" given.', 1305111513);
         }
         $key = array_shift($path);
         if (count($path) === 0) {
@@ -293,7 +285,7 @@ abstract class Arrays
     /**
      * Sorts multidimensional arrays by recursively calling ksort on its elements.
      *
-     * @param array $array the array to sort
+     * @param array<mixed> $array the array to sort
      * @param integer $sortFlags may be used to modify the sorting behavior using these values (see http://www.php.net/manual/en/function.sort.php)
      * @return boolean true on success, false on failure
      * @see asort()
@@ -314,7 +306,7 @@ abstract class Arrays
      * Recursively convert an object hierarchy into an associative array.
      *
      * @param mixed $subject An object or array of objects
-     * @return array The subject represented as an array
+     * @return array<mixed> The subject represented as an array
      * @throws \InvalidArgumentException
      */
     public static function convertObjectToArray($subject): array
@@ -336,8 +328,8 @@ abstract class Arrays
     /**
      * Recursively removes empty array elements.
      *
-     * @param array $array
-     * @return array the modified array
+     * @param array<mixed> $array
+     * @return array<mixed> the modified array
      */
     public static function removeEmptyElementsRecursively(array $array): array
     {
